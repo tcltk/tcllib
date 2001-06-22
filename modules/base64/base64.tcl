d725c6c8a545d17691bebcbd4f5c5548fd706350
Andreas Kupries <andreas.kupries@gmail.com>
2001-06-22 14:33:07 +0000

    Committed patches to speed up MD5 and Base64 (M. Sofer). Added SHA1.

--- modules/base64/base64.tcl
+++ modules/base64/base64.tcl
@@ -8,167 +8,294 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: base64.tcl,v 1.8 2000/10/11 20:04:11 welch Exp $
+# RCS: @(#) $Id: base64.tcl,v 1.9 2001/06/22 14:33:07 andreas_kupries Exp $
 
 # Version 1.0 implemented Base64_Encode, Bae64_Decode
 # Version 2.0 uses the base64 namespace
 # Version 2.1 fixes various decode bugs and adds options to encode
+# Version 2.2 is much faster, Tcl8.0 compatible
 
 namespace eval base64 {
-    variable i 0
-    variable char
-    variable base64
-    variable base64_en
-    foreach char {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
-	      a b c d e f g h i j k l m n o p q r s t u v w x y z \
-	      0 1 2 3 4 5 6 7 8 9 + /} {
-	set base64($char) $i
-	set base64_en($i) $char
-	incr i
-    }
-
-    namespace export *
 }
 
-# base64::encode --
-#
-#	Base64 encode a given string.
-#
-# Arguments:
-#	args	?-maxlen maxlen? ?-wrapchar wrapchar? string
-#	
-#		If maxlen is 0, the output is not wrapped.
-#
-# Results:
-#	A Base64 encoded version of $string, wrapped at $maxlen characters
-#	by $wrapchar.
+if {![catch {package require Trf 2.0}]} {
+    # Trf is available, so implement the functionality provided here
+    # in terms of calls to Trf for speed.
 
-proc base64::encode {args} {
-    variable base64_en
+    # base64::encode --
+    #
+    #	Base64 encode a given string.
+    #
+    # Arguments:
+    #	args	?-maxlen maxlen? ?-wrapchar wrapchar? string
+    #	
+    #		If maxlen is 0, the output is not wrapped.
+    #
+    # Results:
+    #	A Base64 encoded version of $string, wrapped at $maxlen characters
+    #	by $wrapchar.
     
-    # Set the default wrapchar and maximum line length to match the output
-    # of GNU uuencode 4.2.  Various RFC's allow for different wrapping 
-    # characters and wraplengths, so these may be overridden by command line
-    # options.
-    set wrapchar "\n"
-    set maxlen 60
+    proc base64::encode {args} {
+	# Set the default wrapchar and maximum line length to match the output
+	# of GNU uuencode 4.2.  Various RFC's allow for different wrapping 
+	# characters and wraplengths, so these may be overridden by command line
+	# options.
+	set wrapchar "\n"
+	set maxlen 60
 
-    if { [llength $args] == 0 } {
-	error "wrong # args: should be \"[lindex [info level 0] 0]\
-		?-maxlen maxlen? ?-wrapchar wrapchar? string\""
-    }
+	if { [llength $args] == 0 } {
+	    error "wrong # args: should be \"[lindex [info level 0] 0]\
+		    ?-maxlen maxlen? ?-wrapchar wrapchar? string\""
+	}
 
-    set optionStrings [list "-maxlen" "-wrapchar"]
-    for {set i 0} {$i < [llength $args] - 1} {incr i} {
-	set arg [lindex $args $i]
-	set index [lsearch -glob $optionStrings "${arg}*"]
-	if { $index == -1 } {
-	    error "unknown option \"$arg\": must be -maxlen or -wrapchar"
+	set optionStrings [list "-maxlen" "-wrapchar"]
+	for {set i 0} {$i < [llength $args] - 1} {incr i} {
+	    set arg [lindex $args $i]
+	    set index [lsearch -glob $optionStrings "${arg}*"]
+	    if { $index == -1 } {
+		error "unknown option \"$arg\": must be -maxlen or -wrapchar"
+	    }
+	    incr i
+	    if { $i >= [llength $args] - 1 } {
+		error "value for \"$arg\" missing"
+	    }
+	    set val [lindex $args $i]
+	    set [string range [lindex $optionStrings $index] 1 end] $val
 	}
-	incr i
-	if { $i >= [llength $args] - 1 } {
-	    error "value for \"$arg\" missing"
-	}
-	set val [lindex $args $i]
-	set [string range [lindex $optionStrings $index] 1 end] $val
-    }
     
-    if { ![string is integer -strict $maxlen] } {
-	error "expected integer but got \"$maxlen\""
-    }
-
-    set string [lindex $args end]
-
-    set result {}
-    set state 0
-    set length 0
-    foreach {c} [split $string {}] {
-	# Do the line length check before appending so that we don't get an
-	# extra newline if the output is a multiple of $maxlen chars long.
-	if {$maxlen && $length >= $maxlen} {
-	    append result $wrapchar
-	    set length 0
+	# [string is] requires Tcl8.2; this works with 8.0 too
+	if {[catch {expr {$maxlen % 2}}]} {
+	    error "expected integer but got \"$maxlen\""
 	}
-	scan $c %c x
-	switch [incr state] {
-	    1 {	append result $base64_en([expr {($x >>2) & 0x3F}]) }
-	    2 { append result \
-		$base64_en([expr {(($old << 4) & 0x30) | (($x >> 4) & 0xF)}]) }
-	    3 { append result \
-		$base64_en([expr {(($old << 2) & 0x3C) | (($x >> 6) & 0x3)}])
-		append result $base64_en([expr {($x & 0x3F)}])
-		incr length
-		set state 0}
+
+	set string [lindex $args end]
+	set result [::base64 -mode encode -- $string]
+	regsub -all \n $result {} result
+
+	if {$maxlen > 0} {
+	    set res ""
+	    set edge [expr {$maxlen - 1}]
+	    while {[string length $result] > $maxlen} {
+		append res [string range $result 0 $edge]$wrapchar
+		set result [string range $result $maxlen end]
+	    }
+	    if {[string length $result] > 0} {
+		append res $result
+	    }
+	    set result $res
 	}
-	set old $x
-	incr length
+
+	return $result
     }
-    set x 0
-    switch $state {
-	0 { # OK }
-	1 { append result $base64_en([expr {(($old << 4) & 0x30)}])== }
-	2 { append result $base64_en([expr {(($old << 2) & 0x3C)}])=  }
+
+    # base64::decode --
+    #
+    #	Base64 decode a given string.
+    #
+    # Arguments:
+    #	string	The string to decode.  Characters not in the base64
+    #		alphabet are ignored (e.g., newlines)
+    #
+    # Results:
+    #	The decoded value.
+
+    proc base64::decode {string} {
+	::base64 -mode decode -- $string
     }
-    return $result
-}
 
-# base64::decode --
-#
-#	Base64 decode a given string.
-#
-# Arguments:
-#	string	The string to decode.  Characters not in the base64
-#		alphabet are ignored (e.g., newlines)
-#
-# Results:
-#	The decoded value.
+} else {
+    # Without Trf use a pure tcl implementation
 
-proc base64::decode {string} {
-    variable base64
+    namespace eval base64 {
+	variable base64 {}
+	variable base64_en {}
 
-    set output {}
-    set group 0
-    set j 18
-    foreach char [split $string {}] {
-	if {[string compare $char "="]} {
-	    # RFC 2045 says that line breaks and other characters not part
-	    # of the Base64 alphabet must be ignored, and that the decoder
-	    # can optionally emit a warning or reject the message.  We opt
-	    # not to do so, but to just ignore the character.
+	# We create the auxiliary array base64_tmp, it will be unset later.
 
-	    if { ![info exists base64($char)] } {
+	set i 0
+	foreach char {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
+		a b c d e f g h i j k l m n o p q r s t u v w x y z \
+		0 1 2 3 4 5 6 7 8 9 + /} {
+	    set base64_tmp($char) $i
+	    lappend base64_en $char
+	    incr i
+	}
+
+	#
+	# Create base64 as list: to code for instance C<->3, specify
+	# that [lindex $base64 67] be 3 (C is 67 in ascii); non-coded
+	# ascii chars get a {}. we later use the fact that lindex on a
+	# non-existing index returns {}, and that [expr {} < 0] is true
+	#
+
+	# the last ascii char is 'z'
+	scan z %c len
+	for {set i 0} {$i <= $len} {incr i} {
+	    set char [format %c $i]
+	    set val {}
+	    if {[info exists base64_tmp($char)]} {
+		set val $base64_tmp($char)
+	    } else {
+		set val {}
+	    }
+	    lappend base64 $val
+	}
+
+	# code the character "=" as -1; used to signal end of message
+	scan = %c i
+	set base64 [lreplace $base64 $i $i -1]
+
+	# remove unneeded variables
+	unset base64_tmp i char len val
+
+	namespace export *
+    }
+
+    # base64::encode --
+    #
+    #	Base64 encode a given string.
+    #
+    # Arguments:
+    #	args	?-maxlen maxlen? ?-wrapchar wrapchar? string
+    #	
+    #		If maxlen is 0, the output is not wrapped.
+    #
+    # Results:
+    #	A Base64 encoded version of $string, wrapped at $maxlen characters
+    #	by $wrapchar.
+    
+    proc base64::encode {args} {
+	set base64_en $::base64::base64_en
+	
+	# Set the default wrapchar and maximum line length to match the output
+	# of GNU uuencode 4.2.  Various RFC's allow for different wrapping 
+	# characters and wraplengths, so these may be overridden by command line
+	# options.
+	set wrapchar "\n"
+	set maxlen 60
+
+	if { [llength $args] == 0 } {
+	    error "wrong # args: should be \"[lindex [info level 0] 0]\
+		    ?-maxlen maxlen? ?-wrapchar wrapchar? string\""
+	}
+
+	set optionStrings [list "-maxlen" "-wrapchar"]
+	for {set i 0} {$i < [llength $args] - 1} {incr i} {
+	    set arg [lindex $args $i]
+	    set index [lsearch -glob $optionStrings "${arg}*"]
+	    if { $index == -1 } {
+		error "unknown option \"$arg\": must be -maxlen or -wrapchar"
+	    }
+	    incr i
+	    if { $i >= [llength $args] - 1 } {
+		error "value for \"$arg\" missing"
+	    }
+	    set val [lindex $args $i]
+	    set [string range [lindex $optionStrings $index] 1 end] $val
+	}
+    
+	# [string is] requires Tcl8.2; this works with 8.0 too
+	if {[catch {expr {$maxlen % 2}}]} {
+	    error "expected integer but got \"$maxlen\""
+	}
+
+	set string [lindex $args end]
+
+	set result {}
+	set state 0
+	set length 0
+
+
+	# Process the input bytes 3-by-3
+
+	binary scan $string c* X
+	foreach {x y z} $X {
+	    # Do the line length check before appending so that we don't get an
+	    # extra newline if the output is a multiple of $maxlen chars long.
+	    if {$maxlen && $length >= $maxlen} {
+		append result $wrapchar
+		set length 0
+	    }
+	
+	    append result [lindex $base64_en [expr {($x >>2) & 0x3F}]] 
+	    if {$y != {}} {
+		append result [lindex $base64_en [expr {(($x << 4) & 0x30) | (($y >> 4) & 0xF)}]] 
+		if {$z != {}} {
+		    append result \
+			    [lindex $base64_en [expr {(($y << 2) & 0x3C) | (($z >> 6) & 0x3)}]]
+		    append result [lindex $base64_en [expr {($z & 0x3F)}]]
+		} else {
+		    set state 2
+		    break
+		}
+	    } else {
+		set state 1
+		break
+	    }
+	    incr length 4
+	}
+	if {$state == 1} {
+	    append result [lindex $base64_en [expr {(($x << 4) & 0x30)}]]== 
+	} elseif {$state == 2} {
+	    append result [lindex $base64_en [expr {(($y << 2) & 0x3C)}]]=  
+	}
+	return $result
+    }
+
+    # base64::decode --
+    #
+    #	Base64 decode a given string.
+    #
+    # Arguments:
+    #	string	The string to decode.  Characters not in the base64
+    #		alphabet are ignored (e.g., newlines)
+    #
+    # Results:
+    #	The decoded value.
+
+    proc base64::decode {string} {
+	set base64 $::base64::base64
+
+	binary scan $string c* X
+	foreach x $X {
+	    set bits [lindex $base64 $x]
+	    if {$bits >= 0} {
+		if {[llength [lappend nums $bits]] == 4} {
+		    foreach {v w x y} $nums break
+		    set a [expr {($v << 2) | ($w >> 4)}]
+		    set b [expr {(($w & 0xF) << 4) | ($x >> 2)}]
+		    set c [expr {(($x & 0x3) << 6) | $y}]
+		    append output [binary format ccc $a $b $c]
+		    set nums {}
+		}		
+	    } elseif {$bits == -1} {
+		# = indicates end of data.  Output whatever chars are left.
+		# The encoding algorithm dictates that we can only have 1 or 2
+		# padding characters.  If x=={}, we have 12 bits of input 
+		# (enough for 1 8-bit output).  If x!={}, we have 18 bits of
+		# input (enough for 2 8-bit outputs).
+		
+		foreach {v w x} $nums break
+		set a [expr {($v << 2) | (($w & 0x30) >> 4)}]
+		
+		if {$x == {}} {
+		    append output [binary format c $a ]
+		} else {
+		    set b [expr {(($w & 0xF) << 4) | (($x & 0x3C) >> 2)}]
+		    append output [binary format cc $a $b]
+		}		
+		break
+	    } else {
+		# RFC 2045 says that line breaks and other characters not part
+		# of the Base64 alphabet must be ignored, and that the decoder
+		# can optionally emit a warning or reject the message.  We opt
+		# not to do so, but to just ignore the character. 
 		continue
 	    }
-	    set bits $base64($char)
-	    set group [expr {$group | ($bits << $j)}]
-	    if {[incr j -6] < 0} {
-		scan [format %06x $group] %2x%2x%2x a b c
-		append output [format %c%c%c $a $b $c]
-		set group 0
-		set j 18
-	    }
-	} else {
-	    # = indicates end of data.  Output whatever chars are left.
-	    # The encoding algorithm dictates that we can only have 1 or 2
-	    # padding characters.  If j is 6, we have 12 bits of input 
-	    # (enough for 1 8-bit output).  If j is 0, we have 18 bits of
-	    # input (enough for 2 8-bit outputs).
-	    # It is crucial to scan three hex digits even though we
-	    # discard c - older code used %04x and scanned 2 hex digits
-	    # but really ended up generating 5 or 6 (not 4!) digits and
-	    # resulted in alignment errors.
-
-	    scan [format %06x $group] %2x%2x%2x a b c
-	    if {$j == 6} {
-		append output [format %c $a]
-	    } elseif {$j == 0} {
-		append output [format %c%c $a $b]
-	    }
-	    break
 	}
+	return $output
     }
-    return $output
 }
 
-package provide base64 2.1
+package provide base64 2.2
 
