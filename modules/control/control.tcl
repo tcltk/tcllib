aedaa31faa3802098c333598c4bd44858895e2ea
dgp <dgp>
2001-11-07 21:59:24 +0000

    * do.tcl: updated to use [BodyAsCaller] and [CommandAsCaller]         so that proper ::errorInfo management is achieved.
            * rswitch.tcl:
            * ascaller.tcl (new-file): factored out utility procs from
            rswitch.tcl so they can be used by other conotrol commands.
            * tclIndex: generated

--- modules/control/control.tcl
+++ modules/control/control.tcl
@@ -4,13 +4,13 @@
 #	"control".  It provides commands that govern the flow of
 #	control of a program.
 #
-# RCS: @(#) $Id: control.tcl,v 1.6 2001/11/07 10:40:01 rmax Exp $
+# RCS: @(#) $Id: control.tcl,v 1.7 2001/11/07 21:59:24 dgp Exp $
 
 package require Tcl 8.2
 
 namespace eval ::control {
     variable version 0.1
-    namespace export assert control no-op rswitch do
+    namespace export assert control do no-op rswitch
 
     proc control {command args} {
 	# Need to add error handling here
