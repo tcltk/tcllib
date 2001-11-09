b4308e163e6d7285f40c9a84f54756247d3f309a
dgp <dgp@users.sourceforge.net>
2001-11-09 04:59:45 +0000

    * ascaller.tcl (ErrorInfoAsCaller): new utility proc that provides         only ::errorInfo management, leaving return code matters to the caller.         * do.tcl: At the prompting of Reinhard Max, replaced use of the         [BodyAsCaller] and [CommandAsCaller] routines with the simpler         [ErrorInfoAsCaller] with big performance improvement.         * do.test: New tests from Reinhard Max for testing ::errorInfo.         * tclIndex: generated

--- modules/control/do.tcl
+++ modules/control/do.tcl
@@ -10,7 +10,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: do.tcl,v 1.2 2001/11/07 21:59:24 dgp Exp $
+# RCS: @(#) $Id: do.tcl,v 1.3 2001/11/09 04:59:45 dgp Exp $
 #
 namespace eval ::control {
 
@@ -32,31 +32,40 @@ namespace eval ::control {
 		    "bad option \"$whileOrUntil\": must be until, or while"
 	    }
 	}
+
 	# the first invocation of the body
-	#set code [catch { uplevel $body } result]
-	eval [BodyAsCaller body result code]
+	set code [catch { uplevel 1 $body } result]
 
 	# decide what to do upon the return code:
 	#
 	#               0 - the body executed successfully
+	#               1 - the body raised an error
+	#               2 - the body invoked [return]
 	#               3 - the body invoked [break]
 	#               4 - the body invoked [continue]
 	# everything else - return and pass on the results
 	#
-	# Note: return code 1, error, is handled directly by
-	# [BodyAsCaller] above.
-	switch $code {
+	switch -exact -- $code {
 	    0 {}
+	    1 {
+		return -errorinfo [ErrorInfoAsCaller uplevel do]  \
+		    -errorcode $::errorCode -code error $result
+	    }
 	    3 return
 	    4 {}
-	    default { return -code $code $result }
+	    default {
+		return -code $code $result
+	    }
 	}
+
 	# the rest of the loop
-	#set code [catch {uplevel 1 [list while $test $body] } result]
-	set cmd [list while $test $body]
-	eval [CommandAsCaller cmd result]
-	#return -code $code $result
-	# [CommandAsCaller takes care of all non-TCL_OK return codes
-	return $result
+	set code [catch {uplevel 1 [list while $test $body]} result]
+	if {$code == 1} {
+	    return -errorinfo [ErrorInfoAsCaller while do] \
+		-errorcode $::errorCode -code error $result
+	}
+	return -code $code $result
+	
     }
+
 }
