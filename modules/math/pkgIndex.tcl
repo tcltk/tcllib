9d5278101507d7cf9bad02c2b6c625af7b1c085e
chdamm <chdamm>
2001-12-12 18:42:15 +0000

    implemented more functions in math::geometry

--- modules/math/pkgIndex.tcl
+++ modules/math/pkgIndex.tcl
@@ -10,3 +10,5 @@
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded math 1.1 [list source [file join $dir math.tcl]]
+package ifneeded math::geometry 1.0 [list source [file join $dir geometry.tcl]]
+
