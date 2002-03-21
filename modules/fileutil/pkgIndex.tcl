67e68c294e69369cea82dc1979d32abdd60f6f29
Eric Melski <ericm@users.sf.net>
2002-03-21 07:11:50 +0000

    Added fileType function (with minor modification) as posted to comp.lang.tcl by Phil Ehrens.

--- modules/fileutil/pkgIndex.tcl
+++ modules/fileutil/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if {![package vsatisfies [package provide Tcl] 8]} {return}
-package ifneeded fileutil 1.3 [list source [file join $dir fileutil.tcl]]
+package ifneeded fileutil 1.4 [list source [file join $dir fileutil.tcl]]
