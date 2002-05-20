8562a0b8d4a1cb0601026da5aa5996d9653a1417
jfontain <jfontain>
2002-05-20 21:16:43 +0000

    stooop package version 4.4.

--- modules/stooop/pkgIndex.tcl
+++ modules/stooop/pkgIndex.tcl
@@ -1,14 +1,14 @@
 # Copyright (c) 2001 by Jean-Luc Fontaine <jfontain@free.fr>.
 # This code may be distributed under the same terms as Tcl.
 #
-# $Id: pkgIndex.tcl,v 1.3 2001/12/19 11:59:23 jfontain Exp $
+# $Id: pkgIndex.tcl,v 1.4 2002/05/20 21:16:43 jfontain Exp $
 
 # Since stooop redefines the proc command and the default package facility will
 # only load the stooop package at the first unknown command, proc being
 # obviously known by default, forcing the loading of stooop is mandatory prior
 # to the first proc declaration.
 
-package ifneeded stooop 4.3 [list source [file join $dir stooop.tcl]]
+package ifneeded stooop 4.4 [list source [file join $dir stooop.tcl]]
 
 # the following package index instruction was generated using:
 #   "tclsh mkpkgidx.tcl switched switched.tcl"
