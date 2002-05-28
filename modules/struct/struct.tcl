5651c87f669631879f5f5a2210cdfc1a9e53de5d
Andreas Kupries <akupries@shaw.ca>
2002-05-28 06:29:31 +0000

    * matrix.man: Fixed typo (graph -> matrix).
    	* struct.tcl: Added pool files to list of files to source.
    
    	* pool.man: New documentation for pool based upon the original
    	  HTML manpage.
    
    	* pool.html:
    	* pooltest.tcl:
    	* pool.tcl: New data structure, pool, by Erik Leunissen
    	  <e.leunissen@hccnet.nl>. Modified code to be a sub-namespace of
    	  ::struct, made it a part of the struct package. No regular
    	  testsuite yet (see pooltest.tcl for the irregular testsuite).

--- modules/struct/struct.tcl
+++ modules/struct/struct.tcl
@@ -5,11 +5,13 @@ source [file join [file dirname [info script]] queue.tcl]
 source [file join [file dirname [info script]] stack.tcl]
 source [file join [file dirname [info script]] tree.tcl]
 source [file join [file dirname [info script]] matrix.tcl]
+source [file join [file dirname [info script]] pool.tcl]
 namespace eval struct {
 	namespace import -force graph::*
 	namespace import -force queue::*
 	namespace import -force stack::*
 	namespace import -force tree::*
 	namespace import -force matrix::*
+	namespace import -force pool::*
 	namespace export *
 }
