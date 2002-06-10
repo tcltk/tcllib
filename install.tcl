a63f564e6843915774c7a90621d007a70aa29537
Andreas Kupries <akupries@shaw.ca>
2002-06-10 22:29:51 +0000

    Last minute changes before release. Tagging will move.

--- install.tcl
+++ install.tcl
@@ -1,5 +1,7 @@
-# -*- tcl -*-
-#
+#!/bin/sh
+# -*- tcl -*- \
+exec tclsh "$0" ${1+"$@"}
+
 # Install tcllib in the lib directory of the tclsh used to execute
 # this script.
 
@@ -20,16 +22,24 @@ proc xcopy {src dest} {
 
 set installdir [file join [file dirname [info library]] tcllib1.3]
 
-#puts $installdir
-#exit
+puts "[info nameofexecutable] ..."
+puts "Tcl script library at      [info library]"
+puts "Installing Tcllib 1.3 into $installdir"
 
 file mkdir $installdir
 file copy pkgIndex.tcl $installdir
+
+puts "\tCopying HTML documentation ..."
 
 xcopy [file join doc html] [file join $installdir doc]
 
 cd modules
 
 foreach module [glob -nocomplain *] {
+    puts "\tCopying module $module ..."
+
     xcopy $module [file join $installdir $module]
 }
+
+puts Done
+exit
