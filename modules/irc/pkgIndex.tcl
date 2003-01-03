f090b277ab7eb9c741f917d1b806dfbd6a81535d
davidw <davidw>
2003-01-03 02:52:16 +0000

    * irc.tcl: Use 'logger' package for error/debug reporting.  Cleanups   with regards to possible 'bgerror' situations (network   input/output).  Bumped version number to 0.3.

--- modules/irc/pkgIndex.tcl
+++ modules/irc/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded irc 0.2 [list source [file join $dir irc.tcl]]
+package ifneeded irc 0.3 [list source [file join $dir irc.tcl]]
