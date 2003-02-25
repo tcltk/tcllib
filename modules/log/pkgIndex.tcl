0f1e2252a82c21875abe8c2d28f2d8e13309b147
dgp <dgp>
2003-02-25 18:07:36 +0000

    prevent loading of logger 0.1 in pre-Tcl-8.2 interps. Would be much better to separate the index scripts of the logger and log packages.

--- modules/log/pkgIndex.tcl
+++ modules/log/pkgIndex.tcl
@@ -10,4 +10,5 @@
 
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 package ifneeded log 1.0.1 [list source [file join $dir log.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded logger 0.1 [list source [file join $dir logger.tcl]]
