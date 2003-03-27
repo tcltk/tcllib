59c108d0434336954c73eec0d4867810497252e6
Andreas Kupries <akupries@shaw.ca>
2003-03-27 22:13:50 +0000

    * textutil.man: 	* adjust.tcl: 	* trim.tcl: 	* textutil.tcl: New commands: blank, chop, tail, cap, uncap, 	  indent, trimPrefix, and trimEmptyHeading. Imported from my own 	  Pool library. Also fixed some typos in the manpage: Superfluous 	  closing brackets.

--- modules/textutil/textutil.tcl
+++ modules/textutil/textutil.tcl
@@ -30,6 +30,11 @@ if {$::textutil::HaveStrRepeat} {
     proc ::textutil::strRepeat {char num} {
 	return [string repeat $char $num]
     }
+
+    proc ::textutil::blank {n} {
+	return [string repeat " " $num]
+    }
+
 } else {
     proc ::textutil::strRepeat {char num} {
 	if {$num <= 0} {
@@ -50,8 +55,67 @@ if {$::textutil::HaveStrRepeat} {
 	    return "$char[strRepeat $char [incr num -1]]"
 	}
     }
+
+    proc ::textutil::blank {n} {
+	return [strRepeat " " $n]
+    }
 }
 
+
+# @c Removes the last character from the given <a string>.
+#
+# @a string: The string to manipulate.
+#
+# @r The <a string> without its last character.
+#
+# @i chopping
+
+proc ::textutil::chop {string} {
+    return [string range $string 0 [expr {[string length $string]-2}]]
+}
+
+
+
+# @c Removes the first character from the given <a string>.
+# @c Convenience procedure.
+#
+# @a string: string to manipulate.
+#
+# @r The <a string> without its first character.
+#
+# @i tail
+
+proc ::textutil::tail {string} {
+    return [string range $string 1 end]
+}
+
+
+
+# @c Capitalizes first character of the given <a string>.
+# @c Complementary procedure to <p ::textutil::uncap>.
+#
+# @a string: string to manipulate.
+#
+# @r The <a string> with its first character capitalized.
+#
+# @i capitalize
+
+proc ::textutil::cap {string} {
+    return [string toupper [string index $string 0]][string range $string 1 end]
+}
+
+# @c unCapitalizes first character of the given <a string>.
+# @c Complementary procedure to <p ::textutil::cap>.
+#
+# @a string: string to manipulate.
+#
+# @r The <a string> with its first character uncapitalized.
+#
+# @i uncapitalize
+
+proc ::textutil::uncap {string} {
+    return [string tolower [string index $string 0]][string range $string 1 end]
+}
 
 
 source [ file join [ file dirname [ info script ] ] adjust.tcl ]
--- modules/textutil/trim.tcl
+++ modules/textutil/trim.tcl
@@ -6,7 +6,8 @@ namespace eval ::textutil {
 	variable StrR "(${StrU})\$"
 	variable StrL "^(${StrU})"
 
-	namespace export trim trimright trimleft
+	namespace export trim trimright trimleft \
+		trimPrefix trimEmpyHeading
 
 	# This will be redefined later. We need it just to let
 	# a chance for the next import subcommand to work
@@ -15,37 +16,28 @@ namespace eval ::textutil {
 	proc trimright { text { trim "[ \t]+" } } { }
 	proc trim      { text { trim "[ \t]+" } } { }
 
+	proc trimPrefix {text prefix} {}
+	proc trimEmptyHeading {text} {}
     }
 
-    namespace import -force trim::trim trim::trimleft trim::trimright
-    namespace export trim trimleft trimright
-
+    namespace import -force trim::trim trim::trimleft trim::trimright trim::trimPrefix trim::trimEmpyHeading
+    namespace export trim trimleft trimright trimPrefix trimEmpyHeading
 }
 
-proc ::textutil::trim::trimleft { text { trim "[ \t]+" } } {
-    
-    set trim "[ MakeStr $trim left ]"
-    regsub -line -all -- $trim $text {} text
 
+proc ::textutil::trim::trimleft {text {trim "[ \t]+"}} {
+    regsub -line -all -- [MakeStr $trim left] $text {} text
     return $text
 }
 
-proc ::textutil::trim::trimright { text { trim "[ \t]+" } } {
-    
-    set trim "[ MakeStr $trim right ]"
-    regsub -line -all -- $trim $text {} text
-
-    
+proc ::textutil::trim::trimright {text {trim "[ \t]+"}} {
+    regsub -line -all -- [MakeStr $trim right] $text {} text
     return $text
 }
 
-proc ::textutil::trim::trim { text { trim "[ \t]+" } } {
-    
-    set triml "[ MakeStr $trim left ]"
-    regsub -line -all -- $triml $text {} text
-    set trimr "[ MakeStr $trim right ]"
-    regsub -line -all -- $trimr $text {} text
-    
+proc ::textutil::trim::trim {text {trim "[ \t]+"}} {
+    regsub -line -all -- [MakeStr $trim left]  $text {} text
+    regsub -line -all -- [MakeStr $trim right] $text {} text
     return $text
 }
 
@@ -54,19 +46,49 @@ proc ::textutil::trim::MakeStr { string pos }  {
     variable StrR
     variable StrL
 
-    if { "$string" != "$StrU" } then {
+    if { "$string" != "$StrU" } {
         set StrU $string
         set StrR "(${StrU})\$"
         set StrL "^(${StrU})"
     }
-
-    if { "$pos" == "left" } then {
+    if { "$pos" == "left" } {
         return $StrL
     }
-
-    if { "$pos" == "right" } then {
+    if { "$pos" == "right" } {
         return $StrR
     }
 
-    error "Panic, illegal position key \"$pos\""
+    return -code error "Panic, illegal position key \"$pos\""
+}
+
+
+# @c Strips <a prefix> from <a text>, if found at its start.
+#
+# @a text: The string to check for <a prefix>.
+# @a prefix: The string to remove from <a text>.
+#
+# @r The <a text>, but without <a prefix>.
+#
+# @i remove, prefix
+
+proc ::textutil::trim::trimPrefix {text prefix} {
+    if {[string first $prefix $text] == 0} {
+	return [string range $text [string length $prefix] end]
+    } else {
+	return $text
+    }
+}
+
+
+# @c Removes the Heading Empty Lines of <a text>.
+#
+# @a text: The text block to manipulate.
+#
+# @r The <a text>, but without heading empty lines.
+#
+# @i remove, empty lines
+
+proc ::textutil::trim::trimEmptyHeading {text} {
+    regsub -- "^(\[ \t\]*\n)*" $text {} text
+    return $text
 }
