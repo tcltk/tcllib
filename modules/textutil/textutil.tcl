c78bc4d88c37341c8e4aee0fe5df727a77adc41b
Andreas Kupries <akupries@shaw.ca>
2003-03-28 19:57:04 +0000

    * adjust.test: 	* textutil.test: Added testsuite for new commands. Fixed typo bug 	  in yesterday's 'blank' and 'indent'.
    	* adjust.tcl: New command 'undent'.
    	* textutil.tcl: New command 'longestCommonPrefix'.
    
    	* textutil.man: documented the new commands.

--- modules/textutil/textutil.tcl
+++ modules/textutil/textutil.tcl
@@ -32,7 +32,7 @@ if {$::textutil::HaveStrRepeat} {
     }
 
     proc ::textutil::blank {n} {
-	return [string repeat " " $num]
+	return [string repeat " " $n]
     }
 
 } else {
@@ -116,6 +116,52 @@ proc ::textutil::cap {string} {
 proc ::textutil::uncap {string} {
     return [string tolower [string index $string 0]][string range $string 1 end]
 }
+
+
+# Compute the longest string which is common to all strings given to
+# the command, and at the beginning of said strings, i.e. a prefix. If
+# only one argument is specified it is treated as a list of the
+# strings to look at. If more than one argument is specified these
+# arguments are the strings to be looked at. If only one string is
+# given, in either form, the string is returned, as it is its own
+# longest common prefix.
+
+proc ::textutil::longestCommonPrefix {args} {
+    return [longestCommonPrefixList $args]
+}
+
+proc ::textutil::longestCommonPrefixList {list} {
+    if {[llength $list] == 0} {
+	return ""
+    } elseif {[llength $list] == 1} {
+	return [lindex $list 0]
+    }
+
+    set list [lsort  $list]
+    set min  [lindex $list 0]
+    set max  [lindex $list end]
+
+    # Min and max are the two strings which are most different. If
+    # they have a common prefix, it will also be the common prefix for
+    # all of them.
+
+    # Fast bailouts for common cases.
+
+    set n [string length $min]
+    if {$n == 0}                         {return ""}
+    if {0 == [string compare $min $max]} {return $min}
+
+    set prefix ""
+    for {set i 0} {$i < $n} {incr i} {
+	if {0 == [string compare [set x [string range $min 0 $i]] [string range $max 0 $i]]} {
+	    set prefix $x
+	    continue
+	}
+	break
+    }
+    return $prefix
+}
+
 
 
 source [ file join [ file dirname [ info script ] ] adjust.tcl ]
