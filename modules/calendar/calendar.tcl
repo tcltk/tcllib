2340b874afbc418fd4242b117ccbd075d4baebe3
Andreas Kupries <akupries@shaw.ca>
2003-04-11 19:10:42 +0000

    * pkgIndex.tcl: 	* gregorian.tcl: Fixed bug #614591. Set version of the package to 	  0.2

--- modules/calendar/calendar.tcl
+++ modules/calendar/calendar.tcl
@@ -6,13 +6,13 @@
 #   	'calendar' package.  The package provides various commands for
 #	manipulating dates and times.
 #
-# RCS:$(@) $Id: calendar.tcl,v 1.1 2002/01/11 23:48:24 kennykb Exp $
+# RCS:$(@) $Id: calendar.tcl,v 1.2 2003/04/11 19:10:42 andreas_kupries Exp $
 
 package require Tcl 8.2
 
 namespace eval ::calendar {
 
-    variable version 0.1
+    variable version 0.2
 
     variable home [file join [pwd] [file dirname [info script]]]
     if { [lsearch -exact $::auto_path $home] == -1 } {
