73530b146dfb3b94c5fac770c78b3c796c3584f6
Andreas Kupries <andreas.kupries@gmail.com>
2003-04-11 20:22:59 +0000

    * struct.tcl: 	* list.man: 	* matrix.man: 	* pool.man: 	* queue.man: 	* record.man: 	* stack.man: 	* tree.man: 	* pkgIndex.tcl:  Set version of the package to 1.3.

--- modules/struct/pkgIndex.tcl
+++ modules/struct/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded struct 1.2.2 [list source [file join $dir struct.tcl]]
+package ifneeded struct 1.3 [list source [file join $dir struct.tcl]]
