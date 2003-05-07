49e79d5d93a5c173ea4ec85713755588164a2b32
patthoyts <patthoyts@users.sourceforge.net>
2003-05-07 22:30:51 +0000

    * md4.tcl, md4.man, pkgIndex.tcl: Hiked version to 1.0.1

--- modules/md4/md4.tcl
+++ modules/md4/md4.tcl
@@ -8,14 +8,14 @@
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # -------------------------------------------------------------------------
 #
-# $Id: md4.tcl,v 1.6 2003/05/06 23:16:33 patthoyts Exp $
+# $Id: md4.tcl,v 1.7 2003/05/07 22:30:51 patthoyts Exp $
 
 package require Tcl 8.2;                # tcl minimum version
 catch {package require md4c 1.0};       # tcllib critcl alternative
 
 namespace eval ::md4 {
-    variable version 1.0.0
-    variable rcsid {$Id: md4.tcl,v 1.6 2003/05/06 23:16:33 patthoyts Exp $}
+    variable version 1.0.1
+    variable rcsid {$Id: md4.tcl,v 1.7 2003/05/07 22:30:51 patthoyts Exp $}
 
     namespace export md4 hmac MD4Init MD4Update MD4Final
 
