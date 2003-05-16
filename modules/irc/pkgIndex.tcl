1f8e16990022873eaf1845b041dbb443a3995fc5
davidw <davidw>
2003-05-16 22:01:12 +0000

    * irc.man: Added Aaron's updated documentation, including the new   commands.
    * irc.tcl: Lots of improvements by Aaron Faupell, including: new
      commands, and a new method of dispatching events.  Server PINGs (as
      opposed to CTCP PINGS) automatically generate a reply, as a
      convenience.  Version number 0.4.

--- modules/irc/pkgIndex.tcl
+++ modules/irc/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if { ![package vsatisfies [package provide Tcl] 8.3] } { return }
-package ifneeded irc 0.3 [list source [file join $dir irc.tcl]]
+package ifneeded irc 0.4 [list source [file join $dir irc.tcl]]
