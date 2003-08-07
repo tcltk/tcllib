bfc2459b9f5e6486d93246847ada30cb0b71a44b
Andreas Kupries <akupries@shaw.ca>
2003-08-07 18:49:58 +0000

    * tree.tcl: Fixed bugs in tree serialization code found when 	  hitting them during testing the graph.
    	* graph.man:  Completed the implementation of graph serialization.
    	* graph.tcl:  Updated testsuite, documentation.
    	* graph.test:
    
    	* tree.tcl:  Created ldelete and lset (emulation pre 8.4)
    	* graph.tcl: and replaced as much 'lreplace's as possible. Using
    	             the K operator for speed, encapsulated in the two l
    	             commands.
    
    	* graph.man: Implemented the renaming of nodes and arcs.
    	* graph.tcl:
    	* graph.test:
    
    	* graph.tcl:
    	* graph.test:
    	* graph.man: Same changes in attribute handling as for
    	  'tree'. Noted that the graph attributes had neither 'append' nor
    	  'lappend' methods. Added. Documentation and testsuite updated.
    
    	* pkgIndex.tcl:
    	* struct_tree.man:
    	* tree.test:
    	* tree.tcl: More rework. The attribute APIs are now backward
    	  incompatible, the default attribute 'data' has been dropped. The
    	  whole module 'struct' has been bumped to version 2.0 because of
    	  this. Reworked the testsuite for the changed APIs. Reworked the
    	  (de)serialization stuff a bit and added tests for them. Added an
    	  API to rename nodes, and an API to query the name of the
    	  rootnode. The APIs 'getall' and 'keys' now allow usage of glob
    	  patterns to restrict their results. Documentation is now
    	  uptodate. Added API to compute the 'height' of a node (=
    	  distance to its deepest child).
    
    	* tree.test:
    	* tree.tcl: Reworked node attribute storage. Name of array to
    	  store the information is now dissociated from the name of the
    	  node. This enables the use of arbitrary node names, i.e. ':' in
    	  node names. The second benefit is that nodes without attribute
    	  data (normal) require less memory than before. Removed the now
    	  irrelevant validation of node names and updated the testsuite.
    
    	* tree.test:
    	* tree.tcl: Changed way of mapping from tree object commands to
    	  associoated namespaces. The object namespace now has the same
    	  name and location of the object command. Adapted all tests to
    	  account for this change.
    
    	* tree.test:
    	* tree.tcl: Changed dispatcher to auto-generate the list of tree
    	  commands when a wrong one is given. Updated tests to account for
    	  the now correct sort order. Changed dispatcher to uplevel 1 the
    	  method execution, updated walking system to reflect this change.

--- modules/struct/struct.tcl
+++ modules/struct/struct.tcl
@@ -1,5 +1,5 @@
 package require Tcl 8.2
-package provide struct 1.4
+package provide struct 2.0
 
 source [file join [file dirname [info script]] graph.tcl]
 source [file join [file dirname [info script]] queue.tcl]
