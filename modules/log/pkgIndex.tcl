238d816e0a63fd7975a14422c386e48e89638e0e
davidw <davidw>
2003-12-16 15:46:16 +0000

    * logger.test (2.1): Test deletion callbacks.  (2.2): Test deletion   callbacks.
    * logger.tcl: Bump logger version number because of API change.
      (::logger::walk): Do eval after walk on children.  (delproc): New
      procedure to set a deletion-time callback that is called for each of
      the instance's children.  Thanks to Michael Schlenker for the
      suggestion [ 856280 ].

--- modules/log/pkgIndex.tcl
+++ modules/log/pkgIndex.tcl
@@ -11,4 +11,4 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 package ifneeded log 1.0.2 [list source [file join $dir log.tcl]]
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded logger 0.1 [list source [file join $dir logger.tcl]]
+package ifneeded logger 0.2 [list source [file join $dir logger.tcl]]
