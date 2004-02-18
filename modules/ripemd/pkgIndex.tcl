808c02ad7775106b962e2d09de33c8334c18c81d
patthoyts <patthoyts>
2004-02-18 14:32:04 +0000

    * ripemd160.man:  Initial implementation of RIPEMD-160 in 	* ripemd160.tcl:  pure-Tcl as per ripemd128. 	* ripemd160.test: 	* pkgIndex.tcl:

--- modules/ripemd/pkgIndex.tcl
+++ modules/ripemd/pkgIndex.tcl
@@ -4,7 +4,8 @@
 #
 # This package has been tested with tcl 8.2.3 and above.
 #
-# $Id: pkgIndex.tcl,v 1.1 2004/02/16 22:50:40 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.2 2004/02/18 14:32:04 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded ripemd128 1.0.0 [list source [file join $dir ripemd128.tcl]]
+package ifneeded ripemd160 1.0.0 [list source [file join $dir ripemd160.tcl]]
