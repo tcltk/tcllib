0aa7864414dde03603f27829dff05b503112964a
Andreas Kupries <akupries@shaw.ca>
2004-05-21 03:11:56 +0000

    Import of Aaron's changes.
    Import of Aaron's changes.
    
    Added commands preventing tests from interfering with each
    other through variables.
    
    Fixed package names in require decls of the documentation.
    
    Struct v1 now available through the general index.
    
    Folded non-duplicate v1 structures back into the struct
    directory, as separate packages.
    
    Deleted all duplicate files.

--- modules/struct/pkgIndex.tcl
+++ modules/struct/pkgIndex.tcl
@@ -10,6 +10,7 @@
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded struct            2.1   [list source [file join $dir struct.tcl]]
+package ifneeded struct            1.4   [list source [file join $dir struct1.tcl]]
 
 package ifneeded struct::graph     2.0   [list source [file join $dir graph.tcl]]
 package ifneeded struct::queue     1.2.1 [list source [file join $dir queue.tcl]]
@@ -22,3 +23,7 @@ package ifneeded struct::list      1.4   [list source [file join $dir list.tcl]]
 package ifneeded struct::set       2.1   [list source [file join $dir sets.tcl]]
 package ifneeded struct::prioqueue 1.3   [list source [file join $dir prioqueue.tcl]]
 package ifneeded struct::skiplist  1.3   [list source [file join $dir skiplist.tcl]]
+
+package ifneeded struct::graph     1.2.1 [list source [file join $dir graph1.tcl]]
+package ifneeded struct::tree      1.2.1 [list source [file join $dir tree1.tcl]]
+package ifneeded struct::matrix    1.2.1 [list source [file join $dir matrix1.tcl]]
