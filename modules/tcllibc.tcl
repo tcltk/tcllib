450bbc03ce7ffe13e6119c25e56ec140d7dc074a
patthoyts <patthoyts>
2004-07-04 01:22:46 +0000

    Added critcl implementation of RC4.

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -1,6 +1,6 @@
 package require critcl
-package provide tcllibc 0.1.0
+package provide tcllibc 0.2.0
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.2 2004/01/15 06:36:12 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.3 2004/07/04 01:22:46 patthoyts Exp $}
 }
