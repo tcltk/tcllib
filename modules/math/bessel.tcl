62ed00c9e3f76e2cfa671ef23a54a17e7cfdeac0
Arjen Markus <arjen.markus@deltares.nl>
2004-07-07 12:05:09 +0000

    Indentation inline with Tcllib's conventions; documentation on Romberg integration adjusted

--- modules/math/bessel.tcl
+++ modules/math/bessel.tcl
@@ -6,15 +6,15 @@
 #    Create a convenient namespace for the "special" mathematical functions
 #
 namespace eval ::math::special {
-   #
-   # Define a number of common mathematical constants
-   #
-   variable pi 3.1415926
+    #
+    # Define a number of common mathematical constants
+    #
+    ::math::constants pi
 
-   #
-   # Export the functions
-   #
-   namespace export J0 J1 J1/2 I_n
+    #
+    # Export the functions
+    #
+    namespace export J0 J1 J1/2 I_n
 }
 
 # J0 --
@@ -26,25 +26,25 @@ namespace eval ::math::special {
 #    Value of J0(x)
 #
 proc ::math::special::J0 {x} {
-   variable pi
-   #
-   # Determine the number of components (very crude heuristic)
-   # (We use a quadrature formula here and use the symmetry of
-   # the cos function to reduce the number of actually computed
-   # components by a gfactor 2.
-   #
+    variable pi
+    #
+    # Determine the number of components (very crude heuristic)
+    # (We use a quadrature formula here and use the symmetry of
+    # the cos function to reduce the number of actually computed
+    # components by a gfactor 2.
+    #
 
-   set ncomps [expr {2*int($x/2.0+0.5)}]
-   if { $ncomps < 6 } {
-      set ncomps 6
-   }
+    set ncomps [expr {2*int($x/2.0+0.5)}]
+    if { $ncomps < 6 } {
+        set ncomps 6
+    }
 
-   set result 0.0
-   for { set i 1 } { $i < $ncomps } { incr i 2 } {
-      set result [expr {$result + cos( $x * cos((2*$i-1)*$pi/(2.0*$ncomps)) ) }]
-   }
+    set result 0.0
+    for { set i 1 } { $i < $ncomps } { incr i 2 } {
+        set result [expr {$result + cos( $x * cos((2*$i-1)*$pi/(2.0*$ncomps)) ) }]
+    }
 
-   expr {2.0*$result/$ncomps}
+    expr {2.0*$result/$ncomps}
 }
 
 # J1 --
@@ -56,26 +56,26 @@ proc ::math::special::J0 {x} {
 #    Value of J1(x)
 #
 proc ::math::special::J1 {x} {
-   variable pi
-   #
-   # Determine the number of components (very crude heuristic)
-   # (We use a quadrature formula here and use the symmetry of
-   # the cos function to reduce the number of actually computed
-   # components by a gfactor 2.
-   #
+    variable pi
+    #
+    # Determine the number of components (very crude heuristic)
+    # (We use a quadrature formula here and use the symmetry of
+    # the cos function to reduce the number of actually computed
+    # components by a gfactor 2.
+    #
 
-   set ncomps [expr {2*int($x/2.0+0.5)}]
-   if { $ncomps < 6 } {
-      set ncomps 6
-   }
+    set ncomps [expr {2*int($x/2.0+0.5)}]
+    if { $ncomps < 6 } {
+        set ncomps 6
+    }
 
-   set result 0.0
-   for { set i 1 } { $i < $ncomps } { incr i 2 } {
-      set factor [expr {cos((2*$i-1)*$pi/(2.0*$ncomps))}]
-      set result [expr {$result + $factor * sin( $x * $factor) }]
-   }
+    set result 0.0
+    for { set i 1 } { $i < $ncomps } { incr i 2 } {
+        set factor [expr {cos((2*$i-1)*$pi/(2.0*$ncomps))}]
+        set result [expr {$result + $factor * sin( $x * $factor) }]
+    }
 
-   expr {2.0*$result/$ncomps}
+    expr {2.0*$result/$ncomps}
 }
 
 # J1/2 --
@@ -87,16 +87,16 @@ proc ::math::special::J1 {x} {
 #    Value of J1/2(x)
 #
 proc ::math::special::J1/2 {x} {
-   variable pi
-   #
-   # This Bessel function can be expressed in terms of elementary
-   # functions. Therefore use the explicit formula
-   #
-   if { $x != 0.0 } {
-      expr {sqrt(2.0/$pi/$x)*sin($x)}
-   } else {
-      return 0
-   }
+    variable pi
+    #
+    # This Bessel function can be expressed in terms of elementary
+    # functions. Therefore use the explicit formula
+    #
+    if { $x != 0.0 } {
+        expr {sqrt(2.0/$pi/$x)*sin($x)}
+    } else {
+        return 0.0
+    }
 }
 
 # I_n --
@@ -113,30 +113,30 @@ proc ::math::special::J1/2 {x} {
 namespace eval ::math::special {}
 
 proc ::math::special::I_n {n x} {
-   if { ! [string is integer $n] || $n < 0 } {
-      error "Wrong order: must be positive integer or zero"
-   }
+    if { ! [string is integer $n] || $n < 0 } {
+        error "Wrong order: must be positive integer or zero"
+    }
 
-   set n2 [expr {$n+8}]  ;# Note: just a guess that this will be enough
+    set n2 [expr {$n+8}]  ;# Note: just a guess that this will be enough
 
-   set ynp1 0.0
-   set yn   1.0
-   set sum  1.0
+    set ynp1 0.0
+    set yn   1.0
+    set sum  1.0
 
-   while { $n2 > 0 } {
-      set ynm1 [expr {$ynp1+2.0*$n2*$yn/$x}]
-      set sum  [expr {$sum+$ynm1}]
-      if { $n2 == $n+1 } {
-         set result $ynm1
-      }
-      set ynp1 $yn
-      set yn   $ynm1
-      incr n2 -1
-   }
+    while { $n2 > 0 } {
+        set ynm1 [expr {$ynp1+2.0*$n2*$yn/$x}]
+        set sum  [expr {$sum+$ynm1}]
+        if { $n2 == $n+1 } {
+           set result $ynm1
+        }
+        set ynp1 $yn
+        set yn   $ynm1
+        incr n2 -1
+    }
 
-   set quotient [expr {(2.0*$sum-$ynm1)/exp($x)}]
+    set quotient [expr {(2.0*$sum-$ynm1)/exp($x)}]
 
-   expr {$result/$quotient}
+    expr {$result/$quotient}
 }
 
 
@@ -153,10 +153,10 @@ package provide math::special 0.1
 if { 0 } {
 set tcl_precision 17
 foreach x {0.0 2.0 4.4 6.0 10.0 11.0 12.0 13.0 14.0} {
-   puts "J0($x) = [::math::special::J0 $x] - J1($x) = [::math::special::J1 $x] \
+    puts "J0($x) = [::math::special::J0 $x] - J1($x) = [::math::special::J1 $x] \
 - J1/2($x) = [::math::special::J1/2 $x]"
 }
 foreach n {0 1 2 3 4 5} {
-   puts [::math::special::I_n $n 1.0]
+    puts [::math::special::I_n $n 1.0]
 }
 }
