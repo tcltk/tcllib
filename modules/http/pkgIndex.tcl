0ff8b2bae617cffc23811269c2c543a8fc4bb728
patthoyts <patthoyts@users.sourceforge.net>
2004-07-19 13:40:18 +0000

    * autoproxy.tcl: Removed the stuff for Digest and NTLM until it 	is working properly. Added an authProc configuration option to 	permit application code to specify a procedure to get 	authentication information from the user.

--- modules/http/pkgIndex.tcl
+++ modules/http/pkgIndex.tcl
@@ -1,6 +1,6 @@
 # pkgIndex.tcl for the tcllib http module.
 #
-# $Id: pkgIndex.tcl,v 1.1 2004/07/17 21:36:05 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.2 2004/07/19 13:40:18 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded autoproxy 1.1.0 [list source [file join $dir autoproxy.tcl]]
+package ifneeded autoproxy 1.2.0 [list source [file join $dir autoproxy.tcl]]
