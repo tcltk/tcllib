7ca109bd3aef009500c29baa0878e51a7fe2c188
patthoyts <patthoyts@users.sourceforge.net>
2004-07-30 21:46:19 +0000

    * spf.tcl:       Updated to draft-ietf-marid-protocol-00 document. * spf.test:      Fully implements section 7 macro expansion.

--- modules/dns/spf.tcl
+++ modules/dns/spf.tcl
@@ -2,35 +2,42 @@
 #
 #                         Sender Policy Framework
 #
+#    http://www.ietf.org/internet-drafts/draft-ietf-marid-protocol-00.txt
 #    http://spf.pobox.com/
-#    http://www.ietf.org/internet-drafts/draft-mengwong-spf-01.txt
 #
 # Domains using SPF:
-#   pobox.org      - mx, a, ptr
-#   oxford.ac.uk   - include
-#   gnu.org        - ip4
-#   aol.com        - ip4, ptr
-#   altavista.com  - exists,  multiple TXT replies.
-#   oreilly.com    - mx, ptr, include (invalid domain)
-#   motleyfool.com - include (looping includes)
+#   pobox.org       - mx, a, ptr
+#   oxford.ac.uk    - include
+#   gnu.org         - ip4
+#   aol.com         - ip4, ptr
+#   sourceforge.net - mx, a
+#   altavista.com   - exists,  multiple TXT replies.
+#   oreilly.com     - mx, ptr, include (invalid domain)
+#   motleyfool.com  - include (looping includes)
 #
 # -------------------------------------------------------------------------
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # -------------------------------------------------------------------------
 #
-# $Id: spf.tcl,v 1.2 2004/07/23 21:44:04 patthoyts Exp $
+# $Id: spf.tcl,v 1.3 2004/07/30 21:46:19 patthoyts Exp $
 
 package require Tcl 8.2;                # tcl minimum version
 package require dns;                    # tcllib 1.3
 package require logger;                 # tcllib 1.3
 package require ip;                     # tcllib 1.7
+package require mime;                   # tcllib 1.3
+package require struct::list;           # tcllib 1.7
+package require uri::urn;               # tcllib 1.3
 
 namespace eval spf {
     variable version 1.1.0
-    variable rcsid {$Id: spf.tcl,v 1.2 2004/07/23 21:44:04 patthoyts Exp $}
+    variable rcsid {$Id: spf.tcl,v 1.3 2004/07/30 21:46:19 patthoyts Exp $}
 
     namespace export spf
+
+    variable uid
+    if {![info exists uid]} {set uid 0}
 
     variable log
     if {![info exists log]} { 
@@ -45,22 +52,36 @@ namespace eval spf {
 }
 
 # -------------------------------------------------------------------------
-# sender : ip address of sender
-# target : domain name to test against.
+# ip     : ip address of the connecting host
+# domain : the domain to match
+# sender : full sender email address
 #
-proc ::spf::spf {sender target} {
+proc ::spf::spf {ip domain sender} {
     variable log
 
-    if {[catch {SPF $target} spf]} {
-        ${log}::debug "error fetching SPF record: $spf"
-        return none
+    # 3.3: Initial processing
+    # If the sender address has no local part, set it to postmaster
+    if {[catch {eval array set addr [mime::parseaddress $sender]}]} {
+        ${log}::debug "error parsing sender \"$sender\""
+        return -code error -errorcode permanent "invalid sender address"
     }
-    
-    return [Spf $sender $target $spf]
+    if {[string length $addr(local)] < 1} {
+        set sender "postmaster@$addr(domain)"
+    }
+
+    # 3.4: Record lookup
+    set spf [SPF $domain]
+    if {[string equal $spf none]} {
+        return $spf
+    }
+
+    return [Spf $ip $domain $sender $spf]
 }
 
-proc ::spf::Spf {sender target spf} {
+proc ::spf::Spf {ip domain sender spf} {
     variable log
+
+    # 3.4.1: Matching Version
     if {![regexp {^v=spf(\d)\s+} $spf -> version]} {
         return none
     }
@@ -68,17 +89,13 @@ proc ::spf::Spf {sender target spf} {
     ${log}::debug "$spf"
 
     if {$version != 1} {
-        return -code error "version mismatch: we only understand SPF 1\
+        return -code error -errorcode permanent \
+            "version mismatch: we only understand SPF 1\
             this domain has provided version \"$version\""
     }
 
-    if {![ip::is ipv4 $sender]} {
-        set ips [A $sender]
-        set sender [lindex $ips 0]
-    }
-    
     set result ?
-    set seen_domains $target
+    set seen_domains $domain
     set explanation {denied}
 
     set directives [lrange [split $spf { }] 1 end]
@@ -104,7 +121,9 @@ proc ::spf::Spf {sender target spf} {
                 break
             }
         } else {
-            if {[catch {::spf::_$cmd $sender $target $param} res]} {
+            set r [catch {::spf::_$cmd $ip $domain $sender $param} res]
+            if {$r} {
+                if {$r == 2} {return $res};# deal with return -code return
                 if {[string equal $res "none"] 
                     || [string equal $res "error"]
                     || [string equal $res "unknown"]} {
@@ -143,16 +162,16 @@ proc ::spf::create_token {} {
 #
 # -------------------------------------------------------------------------
 
-# 4.1: The "all" mechanism is a test that always matches.  It is used as the
-#      rightmost mechanism in an SPF record to provide an explicit default
+# 4.1:	The "all" mechanism is a test that always matches.  It is used as the
+#	rightmost mechanism in an SPF record to provide an explicit default
 #
-proc ::spf::_all {sender target param} {
+proc ::spf::_all {ip domain sender param} {
     return 1
 }
 
-# 4.2: The "include" mechanism triggers a recursive SPF query.
-#
-proc ::spf::_include {sender target param} {
+# 4.2:	The "include" mechanism triggers a recursive SPF query.
+#	The domain-spec is expanded as per section 8.
+proc ::spf::_include {ip domain sender param} {
     variable log
     upvar seen_domains Seen
 
@@ -160,32 +179,35 @@ proc ::spf::_include {sender target param} {
         return -code error "dubious parameters for \"include\""
     }
     set r ?
-    set domain [string range $param 1 end]
-    if {[lsearch $Seen $domain] == -1} {
-        lappend Seen $domain
-        if {[catch {set r [spf $sender $domain]}]} {
-            return -code error error
-        }
-        if {[string equal $r "none"] || [string equal $r "unknown"]} {
-            return -code error $r
-        }
+    set new_domain [Expand [string range $param 1 end] $ip $domain $sender]
+    if {[lsearch $Seen $new_domain] == -1} {
+        lappend Seen $new_domain
+        set spf [SPF $new_domain]
+        if {[string equal $spf none]} {
+            return $spf
+        } 
+        set r [Spf $ip $new_domain $sender $spf]
     }
     return [string equal $r "+"]
 }
 
-# 4.4: This mechanism matches if the <sending-host> is one of the
-#      <target-name>'s IP addresses.
+# 4.4:	This mechanism matches if <ip> is one of the target's
+#	IP addresses.
+#	e.g: a:smtp.example.com a:mail.%{d} a
 #
-proc ::spf::_a {sender target param} {
+proc ::spf::_a {ip domain sender param} {
     variable log
-    foreach {domain bits} [ip::SplitIp [string trimleft $param :]] {}
-    if {$domain == {}} {
-        set domain $target
+    foreach {testdomain bits} [ip::SplitIp [string trimleft $param :]] {}
+    if {[string length $testdomain] < 1} {
+        set testdomain $domain
+    } else {
+        set testdomain [Expand $testdomain $ip $domain $sender]
     }
-    set dips [A $domain]
+    ${log}::debug "  fetching A for $testdomain"
+    set dips [A $testdomain];           # get the IPs for the testdomain
     foreach dip $dips {
-        ${log}::debug "  compare: ${sender}/${bits} with ${dip}/${bits}"
-        if {[ip::equal $sender/$bits $dip/$bits]} {
+        ${log}::debug "  compare: ${ip}/${bits} with ${dip}/${bits}"
+        if {[ip::equal $ip/$bits $dip/$bits]} {
             return 1
         }
     }
@@ -195,22 +217,23 @@ proc ::spf::_a {sender target param} {
 # 4.5: This mechanism matches if the <sending-host> is one of the MX hosts
 #      for a domain name.
 #
-proc ::spf::_mx {sender target param} {
+proc ::spf::_mx {ip domain sender param} {
     variable log
-    ${log}::debug "'$sender' '$target' '$param'"
-    foreach {domain bits} [ip::SplitIp [string trimleft $param :]] {}
-    if {[string length $domain] < 1} {
-        set domain $target
+    foreach {testdomain bits} [ip::SplitIp [string trimleft $param :]] {}
+    if {[string length $testdomain] < 1} {
+        set testdomain $domain
+    } else {
+        set testdomain [Expand $testdomain $ip $domain $sender]
     }
-    ${log}::debug "  fetching MX for $domain"
-    set mxs [MX $domain]
+    ${log}::debug "  fetching MX for $testdomain"
+    set mxs [MX $testdomain]
 
     foreach mx $mxs {
         set mx [lindex $mx 1]
         set mxips [A $mx]
         foreach mxip $mxips {
-            ${log}::debug "  compare: ${sender}/${bits} with ${mxip}/${bits}"
-            if {[ip::equal $sender/$bits $mxip/$bits]} {
+            ${log}::debug "  compare: ${ip}/${bits} with ${mxip}/${bits}"
+            if {[ip::equal $ip/$bits $mxip/$bits]} {
                 return 1
             }
         }
@@ -221,17 +244,17 @@ proc ::spf::_mx {sender target param} {
 # 4.6: This mechanism tests if the <sending-host>'s name is within a
 #      particular domain.
 #
-proc ::spf::_ptr {sender target param} {
+proc ::spf::_ptr {ip domain sender param} {
     variable log
     set validnames {}
-    if {[catch { set names [PTR $sender] } msg]} {
-        ${log}::debug "  \"$sender\" $msg"
+    if {[catch { set names [PTR $ip] } msg]} {
+        ${log}::debug "  \"$ip\" $msg"
         return 0
     }
     foreach name $names {
-        set ips [A $name]
-        foreach ip $ips {
-            if {[string equal $ip $sender]} {
+        set addrs [A $name]
+        foreach addr $addrs {
+            if {[ip::equal $ip $addr]} {
                 lappend validnames $name
                 continue
             }
@@ -239,12 +262,12 @@ proc ::spf::_ptr {sender target param} {
     }
 
     ${log}::debug "  validnames: $validnames"
-    set domain [string trimleft $param :]
-    if {$domain == {}} {
-        set domain $target
+    set testdomain [Expand [string trimleft $param :] $ip $domain $sender]
+    if {$testdomain == {}} {
+        set testdomain $domain
     }
     foreach name $validnames {
-        if {[string match "*$domain" $name]} {
+        if {[string match "*$testdomain" $name]} {
             return 1
         }
     }
@@ -255,75 +278,168 @@ proc ::spf::_ptr {sender target param} {
 # 4.7: These mechanisms test if the <sending-host> falls into a given IP
 #      network.
 #
-proc ::spf::_ip4 {sender target param} {
+proc ::spf::_ip4 {ip domain sender param} {
     variable log
     foreach {network bits} [ip::SplitIp [string range $param 1 end]] {}
-    ${log}::debug "  compare ${sender}/${bits} to ${network}/${bits}"
-    if {[ip::equal $sender/$bits $network/$bits]} {
+    ${log}::debug "  compare ${ip}/${bits} to ${network}/${bits}"
+    if {[ip::equal $ip/$bits $network/$bits]} {
         return 1
     }
     return 0
 }
 
-# 4.7: These mechanisms test if the <sending-host> falls into a given IP
+# 4.6: These mechanisms test if the <sending-host> falls into a given IP
 #      network.
 #
-proc ::spf::_ip6 {sender target param} {
+proc ::spf::_ip6 {ip domain sender param} {
     variable log
     foreach {network bits} [ip::SplitIp [string range $param 1 end]] {}
-    ${log}::debug "  compare ${sender}/${bits} to ${network}/${bits}"
-    if {[ip::equal $sender/$bits $network/$bits]} {
+    ${log}::debug "  compare ${ip}/${bits} to ${network}/${bits}"
+    if {[ip::equal $ip/$bits $network/$bits]} {
         return 1
     }
     return 0
 }
 
-# 4.8: This mechanism is used to construct an arbitrary host name that is
+# 4.7: This mechanism is used to construct an arbitrary host name that is
 #      used for a DNS A record query.  It allows for complicated schemes
 #      involving arbitrary parts of the mail envelope to determine what is
 #      legal.
 #
-proc ::spf::_exists {sender target param} {
+proc ::spf::_exists {ip domain sender param} {
     variable log
-    set param [string range $param 1 end]
-    ${log}::warn "exists mechanism handling not implemented."
-    return 0
+    set testdomain [Expand [string range $param 1 end] $ip $domain $sender]
+    ${log}::debug "   checking existence of '$testdomain'"
+    if {[catch {A $testdomain}]} {
+        return 0
+    }
+    return return 1
 }
 
 # 5.1: Redirected query
 #
-proc ::spf::_redirect {sender target param} {
+proc ::spf::_redirect {ip domain sender param} {
     variable log
-    set domain [string range $param 1 end]
-    ${log}::debug ">> redirect to $domain"
-    set r [spf $sender $target $domain]
-    return -code return $r
+    set new_domain [Expand [string range $param 1 end] $ip $domain $sender]
+    ${log}::debug ">> redirect to '$new_domain'"
+    set spf [SPF $new_domain]
+    if {![string equal $spf none]} {
+        set spf [Spf $ip $new_domain $sender $spf]
+    }
+    ${log}::debug "<< redirect returning '$spf'"
+    return -code return $spf
 }
 
 # 5.2: Explanation
 #
-proc ::spf::_exp {sender target param} {
+proc ::spf::_exp {ip domain sender param} {
     variable log
-    set domain [string range $param 1 end]
-    set exp [TXT $domain]
-    set exp [Expand $exp]
+    set new_domain [string range $param 1 end]
+    set exp [TXT $new_domain]
+    set exp [Expand $exp $ip $domain $sender]
+    ${log}::debug "exp expanded to \"$exp\""
+    # FIX ME: need to store this somehow.
 }
 
 # 5.3: Sender accreditation
 #
-proc ::spf::_accredit {sender target param} {
+proc ::spf::_accredit {ip domain sender param} {
     variable log
-    ${log}::debug "  accredit modifier ignored"
+    set accredit [Expand [string range $param 1 end] $ip $domain $sender]
+    ${log}::debug "  accreditation '$accredit'"
+    # We are not using this at the moment.
     return 0
 }
 
 
 # 7: Macro expansion
 #
-proc ::spf::Expand {txt} {
+proc ::spf::Expand {txt ip domain sender} {
     variable log
-    ${log}::warn "macro expansion not implemented"
-    return 0
+    set re {%\{[[:alpha:]](?:\d+)?r?[\+\-\.,/_=]*\}}
+    set txt [string map {\[ \\\[ \] \\\]} $txt]
+    regsub -all $re $txt {[ExpandMacro & $ip $domain $sender]} cmd
+    set cmd [string map {%% % %_ \  %- %20} $cmd]
+    return [subst -novariables $cmd]
+}
+
+proc ::spf::ExpandMacro {macro ip domain sender} {
+    variable log
+    set re {%\{([[:alpha:]])(\d+)?(r)?([\+\-\.,/_=]*)\}}
+    set C {} ; set T {} ; set R {}; set D {}
+    set r [regexp $re $macro -> C T R D]
+    if {$D == {}} {set D .}
+    if {$R == {}} {set R 0} else {set R 1}
+    set res $macro
+    if {$r} {
+        set enc [string is upper $C]
+        switch -exact -- [string tolower $C] {
+            s { set res $sender }
+            l {
+                set addr [split $sender @]
+                if {[llength $addr] < 2} {
+                    set res postmaster
+                } else {
+                    set res [lindex $addr 0]
+                }
+            }
+            o {
+                set addr [split $sender @]
+                if {[llength $addr] < 2} {
+                    set res $sender
+                } else {
+                    set res [lindex $addr 1]
+                }
+            }
+            h - d { set res $domain }
+            i { 
+                set res [ip::normalize $ip]
+                if {[ip::is ipv6 $res]} {
+                    set t {}
+                    # Convert 0000:0001 to 0.1
+                    foreach octet [split $res :] {
+                        lappend t [format %x [scan $octet %x]]
+                    }
+                    set res [join $t .]
+                }
+            }
+            v { 
+                if {[ip::is ipv6 $ip]} {
+                    set res ip6
+                } else {
+                    set res "in-addr"
+                }
+            }
+            c { 
+                set res [ip::normalize $ip]
+                if {[ip::is ipv6 $res]} {
+                    set res [ip::contract $res]
+                }
+            }
+            r { 
+                set s [socket -server {} -myaddr [info host] 0]
+                set res [lindex [fconfigure $s -sockname] 1]
+                close $s
+            }
+            t { set res [clock seconds] }
+        }
+        if {$T != {} || $R} {
+            set res [split $res $D]
+            if {$R} {
+                set res [struct::list::Lreverse $res]
+            }
+            if {$T != {}} {
+                incr T -1
+                set res [join [lrange $res end-$T end] $D]
+            }
+            set res [join $res $D]
+        }
+        if {$enc} {
+            # URI encode the result.
+            set res [uri::urn::quote $res]
+        }
+    }
+    return $res
 }
 
 # -------------------------------------------------------------------------
@@ -338,25 +454,37 @@ proc ::spf::Resolve {domain type resultproc} {
     }
     set tok [dns::resolve $domain -type $type]
     dns::wait $tok
+    set errorcode NONE
     if {[string equal [dns::status $tok] "ok"]} {
         set result [$resultproc $tok]
         set code   ok
     } else {
-        set result [dns::error $tok]
-        set code   error
+        set result    [dns::error $tok]
+        set errorcode [dns::errorcode $tok]
+        set code      error
     }
     dns::cleanup $tok
-    return -code $code $result
+    return -code $code -errorcode $errorcode $result
 }
 
+# 3.4: Record lookup
 proc ::spf::SPF {domain} {
-    set r [Resolve $domain SPF ::dns::result]
     set txt ""
-    foreach res $r {
-        set ndx [lsearch $res rdata]
-        incr ndx
-        if {$ndx != 0} {
-            append txt [string range [lindex $res $ndx] 1 end]
+    if {[catch {Resolve $domain SPF ::dns::result} spf]} {
+        set code $::errorCode
+        ${log}::debug "error fetching SPF record: $r"
+        switch -exact -- $code {
+            3 { return -code return [list - "Domain Does Not Exist"] }
+            2 { return -code error -errorcode temporary $spf }
+        }
+        set txt none
+    } else {
+        foreach res $spf {
+            set ndx [lsearch $res rdata]
+            incr ndx
+            if {$ndx != 0} {
+                append txt [string range [lindex $res $ndx] 1 end]
+            }
         }
     }
     return $txt
