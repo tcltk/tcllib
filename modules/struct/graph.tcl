d54c666c709c7018f994110e753d211f66a42762
Andreas Kupries <akupries@shaw.ca>
2004-09-29 17:34:04 +0000

    * tree.tcl:        Fixed [Tcllib SF Bug 1034924]. Both tree and graph 	* struct_tree.man: depend on struct::list for some of their methods. 	* tree.test:       Now in the code, documented as well, testsuite 	* graph.tcl:       header code extended. 	* graph.man:

--- modules/struct/graph.tcl
+++ modules/struct/graph.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: graph.tcl,v 1.20 2004/09/24 06:54:25 andreas_kupries Exp $
+# RCS: @(#) $Id: graph.tcl,v 1.21 2004/09/29 17:34:04 andreas_kupries Exp $
 
 # Create the namespace before determining cgraph vs. tcl
 # Otherwise the loading 'struct.tcl' may get into trouble
@@ -36,6 +36,8 @@ if {
     }
     return
 }
+
+package require struct::list
 
 namespace eval ::struct {}
 namespace eval ::struct::graph {
