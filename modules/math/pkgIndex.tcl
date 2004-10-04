a7094335568c7f27ec08bc0c39c2b8633f5ba6d1
Arjen Markus <arjen.markus@deltares.nl>
2004-10-04 09:50:37 +0000

    Added new module: discrete (ordinary and fast) Fourier transforms Added preliminary documentation for geometry module

--- modules/math/pkgIndex.tcl
+++ modules/math/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded math                    1.2.2 [list source [file join $dir math.tcl]]
-package ifneeded math::geometry          1.0.1 [list source [file join $dir geometry.tcl]]
+package ifneeded math::geometry          1.0.2 [list source [file join $dir geometry.tcl]]
 package ifneeded math::calculus          0.6   [list source [file join $dir calculus.tcl]]
 package ifneeded math::fuzzy             0.2   [list source [file join $dir fuzzy.tcl]]
 package ifneeded math::statistics        0.1.2 [list source [file join $dir statistics.tcl]]
@@ -12,3 +12,4 @@ package ifneeded math::constants         1.0   [list source [file join $dir cons
 package ifneeded math::polynomials       1.0   [list source [file join $dir polynomials.tcl]]
 package ifneeded math::rationalfunctions 1.0   [list source [file join $dir rational_funcs.tcl]]
 package ifneeded math::bignum            3.0   [list source [file join $dir bignum.tcl]]
+package ifneeded math::fourier           1.0   [list source [file join $dir fourier.tcl]]
