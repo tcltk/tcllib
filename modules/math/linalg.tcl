70217a6edc3a065394dfaf181a20c9a209057186
Arjen Markus <arjen.markus@deltares.nl>
2004-11-12 10:42:33 +0000

    Added tests and a re-implementation of the SVD algorithms plus various matrix procedures

--- modules/math/linalg.tcl
+++ modules/math/linalg.tcl
@@ -33,9 +33,10 @@ namespace eval ::math::linearalgebra {
     namespace export scale scale_vect scale_mat
     namespace export rotate
     namespace export getrow getcol getelem setrow setcol setelem
-    namespace export mkVector mkMatrix mkIdentity mkDiag
+    namespace export mkVector mkMatrix mkIdentity mkDiagonal
     namespace export mkHilbert mkDingdong mkBorder mkFrank
     namespace export solveGauss solveTriangular
+    namespace export determineSVD
 }
 
 # dim --
@@ -157,6 +158,21 @@ proc ::math::linearalgebra::dotproduct { vect1 vect2 } {
         set sum [expr {$sum + $c1*$c2}]
     }
     return $sum
+}
+
+# normalize --
+#     Normalize a vector and return the result
+# Arguments:
+#     vector     Vector to be normalized
+# Result:
+#     A vector of length 1
+#
+proc ::math::linearalgebra::normalize { vector } {
+    set scale [norm $vector]
+    if { $scale == 0.0 } {
+        return -code error "Can not normalize a null-vector"
+    }
+    return [scale [expr {1.0/$scale}] $vector]
 }
 
 # axpy --
@@ -371,7 +387,7 @@ proc ::math::linearalgebra::rotate { c s vect1 vect2 } {
     set result2 {}
     foreach v1 $vect1 v2 $vect2 {
         lappend result1 [expr {$c*$v1-$s*$v2}]
-        lappend result1 [expr {$s*$v1+$c*$v2}]
+        lappend result2 [expr {$s*$v1+$c*$v2}]
     }
     return [list $result1 $result2]
 }
@@ -382,8 +398,13 @@ proc ::math::linearalgebra::rotate { c s vect1 vect2 } {
 #     matrix     Matrix to be transposed
 # Result:
 #     The transposed matrix
+# Note:
+#     The second transpose implementation is faster on large
+#     matrices (100x100 say), there is no significant difference
+#     on small ones (10x10 say).
 #
-proc transpose { matrix } {
+#
+proc ::math::linearalgebra::transpose_old { matrix } {
    set row {}
    set transpose {}
    foreach c [lindex $matrix 0] {
@@ -403,6 +424,45 @@ proc transpose { matrix } {
       incr nr
    }
    return $transpose
+}
+
+proc ::math::linearalgebra::transpose { matrix } {
+   set transpose {}
+   set c 0
+   foreach col [lindex $matrix 0] {
+       set newrow {}
+       foreach row $matrix {
+           lappend newrow [lindex $row $c]
+       }
+       lappend transpose $newrow
+       incr c
+   }
+   return $transpose
+}
+
+# matmul --
+#     Multiply a vector/matrix with another vector/matrix
+# Arguments:
+#     mv1        First vector/matrix (x)
+#     mv2        Second vector/matrix (y)
+# Result:
+#     The result of x*y
+#
+proc ::math::linearalgebra::matmul { mv1 mv2 } {
+    if { [llength [lindex $mv1 0]] > 0 } {
+        if { [llength [lindex $mv2 0]] > 0 } {
+            return [matmul_mm $mv1 $mv2]
+        } else {
+            return [matmul_mv $mv1 $mv2]
+        }
+    } else {
+        if { [llength [lindex $mv2 0]] > 0 } {
+            return [matmul_vm $mv1 $mv2]
+        } else {
+            return [matmul_vv $mv1 $mv2]
+        }
+        return [sub_vect $mv1 $mv2]
+    }
 }
 
 # matmul_mv --
@@ -425,6 +485,63 @@ proc matmul_mv { matrix vector } {
    return $newvect
 }
 
+# matmul_vm --
+#     Multiply a row vector with a matrix
+# Arguments:
+#     vector     Vector (interpreted as row vector: x)
+#     matrix     Matrix (applied right: A)
+# Result:
+#     The vector xtrans*A = Atrans*x
+#
+proc matmul_vm { vector matrix } {
+   return [matmul_mv [transpose $matrix] $vector]
+}
+
+# matmul_vv --
+#     Multiply two vectors to obtain a matrix
+# Arguments:
+#     vect1      First vector (column vector, x)
+#     vect2      Second vector (row vector, y)
+# Result:
+#     The "outer product" x*ytrans
+#
+proc matmul_vv { vect1 vect2 } {
+   set newmat {}
+   foreach v1 $vect1 {
+      set newrow {}
+      foreach v2 $vect2 {
+         lappend newrow [expr {$v1*$v2}]
+      }
+      lappend newmat $newrow
+   }
+   return $newmat
+}
+
+# matmul_mm --
+#     Multiply two matrices
+# Arguments:
+#     mat1      First matrix (A)
+#     mat2      Second matrix (B)
+# Result:
+#     The matrix product A*B
+# Note:
+#     By transposing matrix B we can access the columns
+#     as rows - much easier and quicker, as they are
+#     the elements of the outermost list.
+#
+proc matmul_mm { mat1 mat2 } {
+   set newmat {}
+   set tmat [transpose $mat2]
+   foreach row1 $mat1 {
+      set newrow {}
+      foreach row2 $tmat {
+         lappend newrow [dotproduct $row1 $row2]
+      }
+      lappend newmat $newrow
+   }
+   return $newmat
+}
+
 # mkVector --
 #     Make a vector of a given size
 # Arguments:
@@ -436,9 +553,28 @@ proc matmul_mv { matrix vector } {
 proc ::math::linearalgebra::mkVector { ndim {value 0.0} } {
     set result {}
 
-    while { $ndim >= 0 } {
+    while { $ndim > 0 } {
         lappend result $value
         incr ndim -1
+    }
+    return $result
+}
+
+# mkUnitVector --
+#     Make a unit vector in a given direction
+# Arguments:
+#     ndim       Dimension of the vector
+#     dir        The direction (0, ... ndim-1)
+# Result:
+#     A list with ndim elements, representing a unit vector
+#
+proc ::math::linearalgebra::mkUnitVector { ndim dir } {
+
+    if { $dir < 0 || $dir >= $ndim } {
+        return -code error "Invalid direction for unit vector - $dir"
+    } else {
+        set result [mkVector $ndim]
+        lset result $dir 1.0
     }
     return $result
 }
@@ -455,7 +591,7 @@ proc ::math::linearalgebra::mkVector { ndim {value 0.0} } {
 proc ::math::linearalgebra::mkMatrix { nrows ncols {value 0.0} } {
     set result {}
 
-    while { $nrows >= 0 } {
+    while { $nrows > 0 } {
         lappend result [mkVector $ncols $value]
         incr nrows -1
     }
@@ -479,7 +615,7 @@ proc ::math::linearalgebra::mkIdentity { size } {
     return $result
 }
 
-# mkDiag --
+# mkDiagonal --
 #     Make a diagonal matrix of a given size
 # Arguments:
 #     diag       List of values to appear on the diagonal
@@ -547,6 +683,18 @@ proc ::math::linearalgebra::mkDingdong { size } {
     return $result
 }
 
+# mkOnes --
+#     Make a square matrix consisting of ones
+# Arguments:
+#     size       Number of rows/columns
+# Result:
+#     A nested list, representing a size x size matrix,
+#     filled with 1.0
+#
+proc ::math::linearalgebra::mkOnes { size } {
+    return [mkMatrix $size $size 1.0]
+}
+
 # getrow --
 #     Get the specified row from a matrix
 # Arguments:
@@ -590,8 +738,9 @@ proc ::math::linearalgebra::setrow { matrix row newvalues } {
 proc ::math::linearalgebra::getcol { matrix col } {
     set result {}
     foreach r $matrix {
-        lappend result [lindex $row $col]
+        lappend result [lindex $r $col]
     }
+    return $result
 }
 
 # setcol --
@@ -624,7 +773,7 @@ proc ::math::linearalgebra::setcol { matrix col newvalues } {
 # Result:
 #     The matrix element (row,column)
 #
-proc ::math::linearalgebra::getcol { matrix row col } {
+proc ::math::linearalgebra::getelem { matrix row col } {
     lindex $matrix $row $col
 }
 
@@ -712,16 +861,112 @@ proc ::math::linearalgebra::solveTriangular { matrix bvect } {
     return $bvect
 }
 
+# determineSVD --
+#     Determine the singular value decomposition of a matrix
+# Arguments:
+#     A          Matrix to be examined
+#     epsilon    Tolerance for the procedure (defaults to 2.3e-16)
+#
+# Result:
+#     List of the three elements U, S and V, where:
+#     U, V orthogonal matrices, S a diagonal matrix (here a vector)
+#     such that A = USVt
+# Note:
+#     This is taken directly from Hume's LA package, and adjusted
+#     to fit the different matrix format. Also changes are applied
+#     that can be found in the second edition of Nash's book
+#     "Compact numerical methods for computers"
+#
+#     To be done: transpose the algorithm so that we can work
+#     on rows, rather than columns
+#
+proc ::math::linearalgebra::determineSVD { A {epsilon 2.3e-16} } {
+    foreach {m n} [shape $A] {break}
+    set tolerance [expr {$epsilon * $epsilon* $m * $n}]
+    set V [mkIdentity $n]
+
+    #
+    # Top of the iteration
+    #
+    set count 1
+    for {set isweep 0} {$isweep < 30 && $count > 0} {incr isweep} {
+        set count [expr {$n*($n-1)/2}] ;# count of rotations in a sweep
+        for {set j 0} {$j < [expr {$n-1}]} {incr j} {
+            for {set k [expr {$j+1}]} {$k < $n} {incr k} {
+                set p [set q [set r 0.0]]
+                for {set i 0} {$i < $m} {incr i} {
+                    set Aij [lindex $A $i $j]
+                    set Aik [lindex $A $i $k]
+                    set p [expr {$p + $Aij*$Aik}]
+                    set q [expr {$q + $Aij*$Aij}]
+                    set r [expr {$r + $Aik*$Aik}]
+                }
+                if { $q < $r } {
+                    set c 0.0
+                    set s 1.0
+                } elseif { $q * $r == 0.0 } {
+                    # Underflow of small elements
+                    incr count -1
+                    continue
+                } elseif { ($p*$p)/($q*$r) < $tolerance } {
+                    # Cols j,k are orthogonal
+                    incr count -1
+                    continue
+                } else {
+                    set q [expr {$q-$r}]
+                    set v [expr {sqrt(4.0*$p*$p + $q*$q)}]
+                    set c [expr {sqrt(($v+$q)/(2.0*$v))}]
+                    set s [expr {-$p/($v*$c)}]
+                    # s == sine of rotation angle, c == cosine
+                    # Note: -s in comparison with original LA!
+                }
+                #
+                # Rotation of A
+                #
+                set colj [getcol $A $j]
+                set colk [getcol $A $k]
+                foreach {colj colk} [rotate $c $s $colj $colk] {break}
+                setcol A $j $colj
+                setcol A $k $colk
+                #
+                # Rotation of V
+                #
+                set colj [getcol $V $j]
+                set colk [getcol $V $k]
+                foreach {colj colk} [rotate $c $s $colj $colk] {break}
+                setcol V $j $colj
+                setcol V $k $colk
+            } ;#k
+        } ;# j
+        #puts "pass=$isweep skipped rotations=$count"
+    } ;# isweep
+
+    set S {}
+    for {set j 0} {$j < $n} {incr j} {
+        set q [norm_two [getcol $A $j]]
+        lappend S $q
+        if { $q >= $tolerance } {
+            set newcol [scale [expr {1.0/$q}] [getcol $A $j]]
+            setcol A $j $newcol
+        }
+    } ;# j
+    return [list $A $S $V]
+}
+
 if { 0 } {
 Te doen:
 behoorlijke testen!
 matmul
 solveGauss_band
-transpose (sneller?)
 modified Gram-Schmidt
 svd
 join_col, join_row
 de overige testmatrices uit Nash
+to_LA, from_LA
+eigenvectoren
+is matrix symmetrisch?
+kleinste-kwadraten met SVD en met Gauss
+PCA
 }
 
 if { 0 } {
@@ -750,3 +995,6 @@ if { 0 } {
    puts [time {::math::linearalgebra::axpy_vect 1.1 $vect1 $vect2} 50000]
    puts [time {::math::linearalgebra::add_vect      $vect1 $vect2} 50000]
 }
+
+set M {{1 2} {2 1}}
+puts "[::math::linearalgebra::determineSVD $M]"
