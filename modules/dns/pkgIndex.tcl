984f78b886cf75497ec52c2f7df97a88d80a3574
patthoyts <patthoyts@users.sourceforge.net>
2004-11-21 01:05:05 +0000

    * dns.tcl:     Incremented the version to 1.2.1 and updated the * ip.tcl:      manual.

--- modules/dns/pkgIndex.tcl
+++ modules/dns/pkgIndex.tcl
@@ -1,9 +1,9 @@
 # pkgIndex.tcl -
 #
-# $Id: pkgIndex.tcl,v 1.11 2004/07/23 20:39:17 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.12 2004/11/21 01:05:06 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded dns    1.2.0 [list source [file join $dir dns.tcl]]
+package ifneeded dns    1.2.1 [list source [file join $dir dns.tcl]]
 package ifneeded resolv 1.0.3 [list source [file join $dir resolv.tcl]]
 package ifneeded ip     1.0.0 [list source [file join $dir ip.tcl]]
 package ifneeded spf    1.1.0 [list source [file join $dir spf.tcl]]
