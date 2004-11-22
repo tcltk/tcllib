6595d39dbc17064da621d5b4d950450dcebcafc0
Andreas Kupries <akupries@shaw.ca>
2004-11-22 18:48:38 +0000

    * pkgIndex.tcl: Fixed usage of wrong file for package 'dacceptor'.

--- modules/grammar_fa/pkgIndex.tcl
+++ modules/grammar_fa/pkgIndex.tcl
@@ -12,5 +12,5 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 
 package ifneeded grammar::fa            0.1 [list source [file join $dir fa.tcl]]
 package ifneeded grammar::fa::op        0.1 [list source [file join $dir faop.tcl]]
-package ifneeded grammar::fa::dacceptor 0.1 [list source [file join $dir daccept.tcl]]
+package ifneeded grammar::fa::dacceptor 0.1 [list source [file join $dir dacceptor.tcl]]
 package ifneeded grammar::fa::dexec     0.1 [list source [file join $dir dexec.tcl]]
