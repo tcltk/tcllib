5fce8f59981702fc6474453744e94fbe612a8bfb
patthoyts <patthoyts>
2004-12-03 02:23:40 +0000

    * ripemd128.test: Added test cases from RFC2286 to both digests. 	* ripemd160.test: Also included a test with leading hyphen. 	* ripemd128.tcl:  Removed usage of memchan in favour of the platform 	* ripemd168.tcl:  null device. This avoids problems with name clashes 	                  when loading in [null] or [random].

--- modules/ripemd/pkgIndex.tcl
+++ modules/ripemd/pkgIndex.tcl
@@ -4,8 +4,8 @@
 #
 # This package has been tested with tcl 8.2.3 and above.
 #
-# $Id: pkgIndex.tcl,v 1.2 2004/02/18 14:32:04 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.3 2004/12/03 02:23:40 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded ripemd128 1.0.0 [list source [file join $dir ripemd128.tcl]]
-package ifneeded ripemd160 1.0.0 [list source [file join $dir ripemd160.tcl]]
+package ifneeded ripemd128 1.0.1 [list source [file join $dir ripemd128.tcl]]
+package ifneeded ripemd160 1.0.1 [list source [file join $dir ripemd160.tcl]]
