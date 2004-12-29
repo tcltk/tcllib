7398304245a2f5ae821c938425ab84ff3631ab4b
mic42 <mic42>
2004-12-29 01:18:20 +0000

    Added support for zero args to the tcltest::tooManyArgs proc

--- all.tcl
+++ all.tcl
@@ -8,7 +8,7 @@
 # Copyright (c) 1998-2000 by Ajuba Solutions.
 # All rights reserved.
 # 
-# RCS: @(#) $Id: all.tcl,v 1.23 2004/10/20 03:55:03 andreas_kupries Exp $
+# RCS: @(#) $Id: all.tcl,v 1.24 2004/12/29 01:18:20 mic42 Exp $
 
 catch {wm withdraw .}
 
@@ -17,59 +17,59 @@ set old_auto_path $auto_path
 if {[lsearch [namespace children] ::tcltest] == -1} {
     namespace eval ::tcltest {}
     proc ::tcltest::processCmdLineArgsAddFlagsHook {} {
-	return [list -modules]
+    return [list -modules]
     }
     proc ::tcltest::processCmdLineArgsHook {argv} {
-	array set foo $argv
-	catch {set ::modules $foo(-modules)}
+    array set foo $argv
+    catch {set ::modules $foo(-modules)}
     }
     proc ::tcltest::cleanupTestsHook {{c {}}} {
-	if { [string equal $c ""] } {
-	    return
-	}
-	# Get total/pass/skip/fail counts
-	array set foo [$c eval {array get ::tcltest::numTests}]
-	foreach index [list "Total" "Passed" "Skipped" "Failed"] {
-	    incr ::tcltest::numTests($index) $foo($index)
-	}
-	incr ::tcltest::numTestFiles
+    if { [string equal $c ""] } {
+        return
+    }
+    # Get total/pass/skip/fail counts
+    array set foo [$c eval {array get ::tcltest::numTests}]
+    foreach index [list "Total" "Passed" "Skipped" "Failed"] {
+        incr ::tcltest::numTests($index) $foo($index)
+    }
+    incr ::tcltest::numTestFiles
 
-	# Append the list of failFiles if necessary
-	set f [$c eval {
-	    set ff $::tcltest::failFiles
-	    if {($::tcltest::currentFailure) && \
-		    ([lsearch -exact $ff $testFileName] == -1)} {
-		set res [file join $::tcllibModule $testFileName]
-	    } else {
-		set res ""
-	    }
-	    set res
-	}] ; # {}
-	if { ![string equal $f ""] } {
-	    lappend ::tcltest::failFiles $f
-	}
+    # Append the list of failFiles if necessary
+    set f [$c eval {
+        set ff $::tcltest::failFiles
+        if {($::tcltest::currentFailure) && \
+            ([lsearch -exact $ff $testFileName] == -1)} {
+        set res [file join $::tcllibModule $testFileName]
+        } else {
+        set res ""
+        }
+        set res
+    }] ; # {}
+    if { ![string equal $f ""] } {
+        lappend ::tcltest::failFiles $f
+    }
 
-	# Get the "skipped because" information
-	unset foo
-	array set foo [$c eval {array get ::tcltest::skippedBecause}]
-	foreach constraint [array names foo] {
-	    if { ![info exists ::tcltest::skippedBecause($constraint)] } {
-		set ::tcltest::skippedBecause($constraint) $foo($constraint)
-	    } else {
-		incr ::tcltest::skippedBecause($constraint) $foo($constraint)
-	    }
-	}
+    # Get the "skipped because" information
+    unset foo
+    array set foo [$c eval {array get ::tcltest::skippedBecause}]
+    foreach constraint [array names foo] {
+        if { ![info exists ::tcltest::skippedBecause($constraint)] } {
+        set ::tcltest::skippedBecause($constraint) $foo($constraint)
+        } else {
+        incr ::tcltest::skippedBecause($constraint) $foo($constraint)
+        }
+    }
 
-	# Clean out the state in the slave
-	$c eval {
-	    foreach index [list "Total" "Passed" "Skipped" "Failed"] {
-		set ::tcltest::numTests($index) 0
-	    }
-	    set ::tcltest::failFiles {}
-	    foreach constraint [array names ::tcltest::skippedBecause] {
-		unset ::tcltest::skippedBecause($constraint)
-	    }
-	}
+    # Clean out the state in the slave
+    $c eval {
+        foreach index [list "Total" "Passed" "Skipped" "Failed"] {
+        set ::tcltest::numTests($index) 0
+        }
+        set ::tcltest::failFiles {}
+        foreach constraint [array names ::tcltest::skippedBecause] {
+        unset ::tcltest::skippedBecause($constraint)
+        }
+    }
     }
 
     package require tcltest
@@ -124,7 +124,7 @@ set old_apath $auto_path
 
 if {![info exists modules]} then {
     foreach module [glob [file join $root modules]/*/*.test] {
-	set tmp([lindex [file split $module] end-1]) 1
+    set tmp([lindex [file split $module] end-1]) 1
     }
     set modules [array names tmp]
     unset tmp
@@ -134,7 +134,7 @@ foreach module $modules {
     set ::tcltest::testsDirectory [file join $root modules $module]
 
     if { ![file isdirectory $::tcltest::testsDirectory] } {
-	puts stdout "unknown module $module"
+    puts stdout "unknown module $module"
     }
 
     set auto_path $old_apath
@@ -147,96 +147,101 @@ foreach module $modules {
     interp alias $c pSet {} set
     # import the auto_path from the parent interp, so "package require" works
     $c eval {
-	set ::argv0 [pSet ::argv0]
-	set ::tcllibModule [pSet module]
-	set auto_path [pSet auto_path]
+    set ::argv0 [pSet ::argv0]
+    set ::tcllibModule [pSet module]
+    set auto_path [pSet auto_path]
 
-	# The next command allows the execution of 'tk' constrained
-	# tests, if Tk is present (for example when this code is run
-	# run by 'wish').
-	catch {
-	    package require Tk
-	    wm withdraw .
-	}
+    # The next command allows the execution of 'tk' constrained
+    # tests, if Tk is present (for example when this code is run
+    # run by 'wish').
+    catch {
+        package require Tk
+        wm withdraw .
+    }
 
-	package require tcltest
-	namespace import ::tcltest::*
-	set ::tcltest::testSingleFile false
-	set ::tcltest::testsDirectory [pSet ::tcltest::testsDirectory]
-	#set ::tcltest::verbose ps
+    package require tcltest
+    namespace import ::tcltest::*
+    set ::tcltest::testSingleFile false
+    set ::tcltest::testsDirectory [pSet ::tcltest::testsDirectory]
+    #set ::tcltest::verbose ps
 
-	# Add a function to construct a proper error message for
-	# 'wrong#args' situations. The format of the messages changed
-	# for 8.4
+    # Add a function to construct a proper error message for
+    # 'wrong#args' situations. The format of the messages changed
+    # for 8.4
 
-	proc ::tcltest::wrongNumArgs {functionName argList missingIndex} {
-	    # if oldstyle errors:
-	    if {[package vcompare [package provide Tcl] 8.4] < 0} {
-		set msg "no value given for parameter "
-		append msg "\"[lindex $argList $missingIndex]\" to "
-		append msg "\"$functionName\""
-	    } else {
-		set msg "wrong # args: should be \"$functionName $argList\""
-	    }
-	    return $msg
-	}
+    proc ::tcltest::wrongNumArgs {functionName argList missingIndex} {
+        # if oldstyle errors:
+        if {[package vcompare [package provide Tcl] 8.4] < 0} {
+        set msg "no value given for parameter "
+        append msg "\"[lindex $argList $missingIndex]\" to "
+        append msg "\"$functionName\""
+        } else {
+        set msg "wrong # args: should be \"$functionName $argList\""
+        }
+        return $msg
+    }
 
-	proc ::tcltest::tooManyArgs {functionName argList} {
-	    # if oldstyle errors:
-	    if {[package vcompare [package provide Tcl] 8.4] < 0} {
-		set msg "called \"$functionName\" with too many arguments"
-	    } else {
-		set msg "wrong # args: should be \"$functionName $argList\""
-	    }
-	    return $msg
-	}
+    proc ::tcltest::tooManyArgs {functionName argList} {
+        # if oldstyle errors:
+        if {[package vcompare [package provide Tcl] 8.4] < 0} {
+        set msg "called \"$functionName\" with too many arguments"
+        } else {
+            # create a different message for functions with no args
+            if {[llength $argList]} {
+            set msg "wrong # args: should be \"$functionName $argList\""
+            } else {
+            set msg "wrong # args: should be \"$functionName\""
+            }
+        }
+        return $msg
+    }
 
-	# This constraint restricts certain tests to run on tcl 8.3+, and tcl8.4+
-	if {[package vsatisfies [package provide tcltest] 2.0]} {
-	    # tcltest2.0+ has an API to specify a test constraint
-	    ::tcltest::testConstraint tcl8.3only \
-		    [expr {![package vsatisfies [package provide Tcl] 8.4]}]
-	    ::tcltest::testConstraint tcl8.3plus \
-		    [expr {[package vsatisfies [package provide Tcl] 8.3]}]
-	    ::tcltest::testConstraint tcl8.4plus \
-		    [expr {[package vsatisfies [package provide Tcl] 8.4]}]
-	    ::tcltest::testConstraint tcl8.5plus \
-		    [expr {[package vsatisfies [package provide Tcl] 8.5]}]
+    # This constraint restricts certain tests to run on tcl 8.3+, and tcl8.4+
+    if {[package vsatisfies [package provide tcltest] 2.0]} {
+        # tcltest2.0+ has an API to specify a test constraint
+        ::tcltest::testConstraint tcl8.3only \
+            [expr {![package vsatisfies [package provide Tcl] 8.4]}]
+        ::tcltest::testConstraint tcl8.3plus \
+            [expr {[package vsatisfies [package provide Tcl] 8.3]}]
+        ::tcltest::testConstraint tcl8.4plus \
+            [expr {[package vsatisfies [package provide Tcl] 8.4]}]
+        ::tcltest::testConstraint tcl8.5plus \
+            [expr {[package vsatisfies [package provide Tcl] 8.5]}]
 
-	    proc ::tcltest::queryConstraint {c} {
-		return [testConstraint $c]
-	    }
-	} else {
-	    # In tcltest1.0, a global variable needs to be set directly.
-	    set ::tcltest::testConstraints(tcl8.3only) \
-		    [expr {![package vsatisfies [package provide Tcl] 8.4]}]
-	    set ::tcltest::testConstraints(tcl8.3plus) \
-		    [expr {[package vsatisfies [package provide Tcl] 8.3]}]
-	    set ::tcltest::testConstraints(tcl8.4plus) \
-		    [expr {[package vsatisfies [package provide Tcl] 8.4]}]
-	    set ::tcltest::testConstraints(tcl8.5plus) \
-		    [expr {[package vsatisfies [package provide Tcl] 8.5]}]
+        proc ::tcltest::queryConstraint {c} {
+        return [testConstraint $c]
+        }
+    } else {
+        # In tcltest1.0, a global variable needs to be set directly.
+        set ::tcltest::testConstraints(tcl8.3only) \
+            [expr {![package vsatisfies [package provide Tcl] 8.4]}]
+        set ::tcltest::testConstraints(tcl8.3plus) \
+            [expr {[package vsatisfies [package provide Tcl] 8.3]}]
+        set ::tcltest::testConstraints(tcl8.4plus) \
+            [expr {[package vsatisfies [package provide Tcl] 8.4]}]
+        set ::tcltest::testConstraints(tcl8.5plus) \
+            [expr {[package vsatisfies [package provide Tcl] 8.5]}]
 
-	    proc ::tcltest::queryConstraint {c} {
-		if {![info exists ::tcltest::testConstraints($c)]} {
-		    # Not existing constraints are not set.
-		    return 0
-		}
-		return $::tcltest::testConstraints($c)
-	    }
-	}
+        proc ::tcltest::queryConstraint {c} {
+        if {![info exists ::tcltest::testConstraints($c)]} {
+            # Not existing constraints are not set.
+            return 0
+        }
+        return $::tcltest::testConstraints($c)
+        }
+    }
     }
     interp alias $c ::tcltest::cleanupTestsHook {} \
-	    ::tcltest::cleanupTestsHook $c
+        ::tcltest::cleanupTestsHook $c
     # source each of the specified tests
     foreach file [lsort [::tcltest::getMatchingFiles]] {
-	set tail [file tail $file]
-	puts stdout [string map [list "$root/" ""] $file]
-	$c eval {
-	    if {[catch {source [pSet file]} msg]} {
-		puts stdout $errorInfo
-	    }
-	}
+    set tail [file tail $file]
+    puts stdout [string map [list "$root/" ""] $file]
+    $c eval {
+        if {[catch {source [pSet file]} msg]} {
+        puts stdout $errorInfo
+        }
+    }
     }
     interp delete $c
     puts stdout ""
