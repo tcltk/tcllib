e8764a72ff4037e21874376db119dcfd32405d90
sarnold75 <sarnold75>
2005-02-08 10:55:34 +0000

    Demonstration of math::bigfloat and math::bignum abilities. Changes needed by the package math::bigfloat version 1.2. Added a 'bigfloat.help' file to advise the users.

--- examples/math/bigfloat.demo.tcl
+++ examples/math/bigfloat.demo.tcl
@@ -43,33 +43,40 @@
 # permission to use and distribute the software in accordance with the
 # terms specified in this license.
 #
-# Web site : http://sarnold.free.fr/
 ########################################################################
 
-# first thing to do : load the package in memory
+package require math::bignum
 package require math::bigfloat
 
 set nbButtons 0
 proc addButton {command} {
     global nbButtons
-    set ::buttons($nbButtons,command) $command
+    set ::buttons($nbButtons,command) _$command
     set ::buttons($nbButtons,texte) $command
     incr nbButtons
 }
 
-proc addButtonF {commande} {
+proc addButtonTwo {commande} {
     addButton $commande
-    proc $commande {} "pop a
-    isFloat \$a
-    fpush \[::math::bigfloat::$commande \[lindex \$a 1\]\]"
+    proc _$commande {} "if {\[catch {pop a} msg\]} {tk_messageBox -message \$msg;return}
+    if {\[catch {pop b} msg\]} {push \$a
+        tk_messageBox -message \$msg;return}
+    if {\[catch {set result \[$commande \$a \$b\]} msg\]} {
+        push \$b
+        push \$a
+        tk_messageBox -message \$msg
+        return}
+    push \$result"
 }
 
 
-proc addButtonI {commande} {
+proc addButtonOne {commande} {
     addButton $commande
-    proc $commande {} "pop a
-    isFloat \$a
-    ipush \[::math::bigfloat::$commande \[lindex \$a 1\]\]"
+    proc _$commande {} "if {\[catch {pop a} msg\]} {tk_messageBox -message \$msg;return}
+    if {\[catch {set result \[$commande \$a\]} msg\]} {push \$a
+        tk_messageBox -message \$msg
+        return}
+    push \$result"
 }
 
 
@@ -81,45 +88,35 @@ proc drawButtons {} {
         set line [expr {$i/$nbLines}]
         set commande $::buttons($i,command)
         set texte $::buttons($i,texte)
-        button .functions.$commande -text $texte -command $commande
+        button .functions.$commande -text $texte -command $commande -width 10
         grid .functions.$commande -column $col -row $line -in .functions
         
     }
 }
 
 proc initStack {} {
-    set ::stack {}
     foreach i {1 2 3 4} {
-        frame .stack.f$i
-        pack .stack.f$i -in .stack -side top
-        label .stack.f$i.i -text "$i :"
-        pack .stack.f$i.i -in .stack.f$i -anchor w -side left
-        label .stack.f$i.l -text "Empty"
-        pack .stack.f$i.l -in .stack.f$i -anchor e -side right
-        
+        label .stack.l$i -text "[expr {5-$i}] :" -foreground #079 -width 5
+        grid .stack.l$i -in .stack -row $i -column 1
+        label .stack.n$i -text "Empty" -foreground #097 -width 85
+        grid .stack.n$i -in .stack -row $i -column 2
     }
-    drawStack
+    set ::stack [list]
 }
 
-proc FloatPush {} {
-    set x [split $::bigfloat e]
-    set mantissa [lindex $x 0][math::bigfloat::zero $::zeros]
-    if {[llength $x]>1} {
-        append mantissa e[lindex $x 1]
+proc Push {} {
+    set x [fromstr $::bignum]
+    if {![isInt $x]} {
+        set x [fromstr $::bignum $::zeros]
     }
-    lappend ::stack [list Float\
-           [math::bigfloat::fromstr $mantissa]]
-    set ::bigfloat 0.0
-    set ::zeros 1
+    lappend ::stack $x
+    set ::bignum 1.00
+    set ::zeros 0
 }
 
-proc IntPush {} {
-    lappend ::stack [list Int $::integer]
-    set ::integer 0
-    drawStack
-}
 
-proc toStrInt {n} {
+proc toStr {n} {
+    set n [math::bigfloat::tostr $n]
     set resultat ""
     while {[string length $n]>80} {
         append resultat "[string range $n 0 79]...\n"
@@ -127,53 +124,38 @@ proc toStrInt {n} {
     }
     append resultat $n
 }
-proc toStrFloat {x} {
-    set resultat ""
-    set n [math::bigfloat::tostr $x]
-    while {[string length $n]>80} {
-        append resultat "[string range $n 0 79]...\n"
-        set n [string range $n 80 end]
-    }
-    append resultat $n
-}
+
 
 proc drawStack {args} {
     set l [lrange $::stack end-3 end]
     for {set i 4} {$i>[llength $l]} {incr i -1} {
-        .stack.f[expr {5-$i}].l configure -text "Empty"
+        .stack.n[expr {5-$i}] configure -text "Empty" -foreground #097
     }
     for {set i 0} {$i<[llength $l]} {incr i} {
-        foreach {type valeur} [lindex $l end-$i] {break}
-        .stack.f[expr {4-$i}].l configure -text [toStr$type $valeur]
+        set number [lindex $::stack end-$i]
+        .stack.n[expr {4-$i}] configure -text [toStr $number] -foreground #000
     }
 }
 
 proc init {} {
-    wm title . "BigFloatDemo 1.0"
+    wm title . "BigFloatDemo 1.1"
     # the stack (for RPN)
     frame .stack
     pack .stack
     initStack
     # the commands for input
     set c [frame .commands]
-    pack $c
-    set ::bigfloat 0.0
-    entry $c.bigfloat -textvariable ::bigfloat
-    pack $c.bigfloat -in $c -side left
-    label $c.labelZero -text "add precision"
+    pack $c -padx 10 -pady 10
+    set ::bignum 1.00
+    entry $c.bignum -textvariable ::bignum -width 16
+    pack $c.bignum -in $c -side left
+    label $c.labelZero -text "append zeros"
     pack $c.labelZero -in $c -side left
-    set ::zeros 1
-    entry $c.zeros -textvariable ::zeros -width 5
+    set ::zeros 0
+    entry $c.zeros -textvariable ::zeros -width 4
     pack $c.zeros -in $c -side left
-    button $c.fenter -text "Push float" -command FloatPush
+    button $c.fenter -text "Push" -command Push
     pack $c.fenter -in $c -side left
-    set ::integer 0
-    set d [frame .icommands]
-    pack $d
-    entry $d.integer -textvariable ::integer
-    pack $d.integer -in $d -side left
-    button $d.ienter -text "Push integer" -command IntPush
-    pack $d.ienter -in $d -side left
     # the functions for numbers
     frame .functions
     pack .functions
@@ -182,32 +164,34 @@ proc init {} {
     # à un bouton portant un libellé, et d'autre part
     # à une commande Tcl
     # ici nous associons le bouton "add" à la commande "add"
-    addButton add
+    addButtonTwo add
     # toutes ces commandes se trouvent à la fin de ce fichier
-    addButton sub
-    addButton mul
-    addButton div
-    addButton mod
-    addButtonF opp
-    addButtonF abs
-    addButtonI round
-    addButtonI ceil
-    addButtonI floor
-    addButton pow
-    addButtonF sqrt
-    addButtonF log
-    addButtonF exp
-    addButtonF cos
-    addButtonF sin
-    addButtonF tan
-    addButtonF acos
-    addButtonF asin
-    addButtonF atan
-    addButtonF cotan
-    addButtonF cosh
-    addButtonF sinh
-    addButtonF tanh
-    addButton pi
+    addButtonTwo sub
+    addButtonTwo mul
+    addButtonTwo div
+    addButtonTwo mod
+    addButtonOne opp
+    addButtonOne abs
+    addButtonOne round
+    addButtonOne ceil
+    addButtonOne floor
+    addButtonTwo pow
+    addButtonOne sqrt
+    addButtonOne log
+    addButtonOne exp
+    addButtonOne cos
+    addButtonOne sin
+    addButtonOne tan
+    addButtonOne acos
+    addButtonOne asin
+    addButtonOne atan
+    addButtonOne cotan
+    addButtonOne cosh
+    addButtonOne sinh
+    addButtonOne tanh
+    addButtonOne pi
+    addButtonOne rad2deg
+    addButtonOne deg2rad
     addButton del
     addButton swap
     addButton help
@@ -220,63 +204,65 @@ proc init {} {
 # procedures that corresponds to functions (add,mul,etc.)
 ################################################################################
 
-proc save {} {
+proc _save {} {
     set fichier [tk_getSaveFile -filetypes {{{Text Files} {.txt}}} -title "Save the stack as ..."]
     if {$fichier == ""} {
         error "You should give a name to the file. Aborting saving operation. Sorry."
     }
+    if {[lindex [split $fichier .] end]!="txt"} {
+        append fichier .txt
+    }
     if {[catch {set file [open $fichier w]}]} {
         error "Write impossible on file : '$fichier'"
     }
-    foreach i $::stack {
-        foreach {type valeur} $i {
-            switch -- $type {
-                Int {puts $file $valeur}
-                Float -
-                default {puts $file [::math::bigfloat::tostr $valeur]}
-            }
-        }
+    foreach valeur $::stack {
+        puts $file [::math::bigfloat::tostr $valeur]
     }
     close $file
 }
 
-proc help {} {
-    toplevel .help
-    text .help.texte -width 80 -height 25
-    pack .help.texte -in .help
-    set fd [open license_terms.txt]
-    .help.texte insert 0.0 [read $fd]
+proc ShowFile {filename buttonText} {
+    if {[catch {toplevel .help}]} {
+        tk_messageBox -message "Unable to create the window ; please close the current help window"
+        return
+    }
+    frame .help.licence
+    text .help.licence.t -yscrollcommand {.help.licence.s set}
+    scrollbar .help.licence.s -command {.help.licence.t yview}
+    grid .help.licence.t .help.licence.s -sticky nsew
+    grid columnconfigure .help.licence 0 -weight 1
+    grid rowconfigure .help.licence 0 -weight 1
+    
+    pack .help.licence -in .help
+    set fd [open $filename]
+    .help.licence.t insert 0.0 [read $fd]
     close $fd
-    .help.texte configure -state disabled
-    button .help.bouton -text "I Agree" -command {destroy .help}
+    .help.licence.t configure -state disabled
+    button .help.bouton -text $buttonText -command {destroy .help}
     pack .help.bouton -in .help
+    focus -force .help
 }
 
-proc del {} {
-    if {[llength $::stack]==1} {
+proc _help {args} {
+    # display some help
+    ShowFile bigfloat.help Close
+}
+
+proc _del {} {
+    if {[llength $::stack]<=1} {
         set ::stack {}
     } else  {
         set ::stack [lrange $::stack 0 end-1]
     }
 }
 
-proc swap {} {
+proc _swap {} {
     set last [lindex $::stack end]
     lset ::stack end [lindex $::stack end-1]
     lset ::stack end-1 $last
 }
 
-proc isFloat {data} {
-    if {![string equal [lindex $data 0] Float]} {
-        error "The argument : [lindex $data 1] has to be a float"
-    }
-}
 
-proc isInt {data} {
-    if {![string equal [lindex $data 0] Int]} {
-        error "The argument has to be an integer"
-    }
-}
 
 
 
@@ -290,68 +276,20 @@ proc pop {varname} {
     return
 }
 
-proc fpush {x} {
-    lappend ::stack [list Float $x]
-}
-
-proc ipush {x} {
-    lappend ::stack [list Int $x]
-}
 
 proc push {x} {
-    if {[::math::bigfloat::isInt $x]} {ipush $x} else {fpush $x}
-}
-proc add {} {
-    pop a
-    pop b
-    set resultat [math::bigfloat::add [lindex $a 1] [lindex $b 1]]
-    push $resultat
+    lappend ::stack $x
 }
 
-proc sub {} {
-    pop a
-    pop b
-    set resultat [math::bigfloat::sub [lindex $a 1] [lindex $b 1]]
-    push $resultat
-}
-
-proc mul {} {
-    pop a
-    pop b
-    set resultat [math::bigfloat::mul [lindex $a 1] [lindex $b 1]]
-    push $resultat
-}
-
-proc div {} {
-    pop a
-    pop b
-    set resultat [math::bigfloat::div [lindex $a 1] [lindex $b 1]]
-    push $resultat
-}
-
-proc mod {} {
-    pop a
-    pop b
-    set resultat [math::bigfloat::mod [lindex $a 1] [lindex $b 1]]
-    push $resultat
-}
-
-proc pow {} {
-    pop a
-    pop b
-    isInt $b
-    push [math::bigfloat::pow [lindex $a 1] [lindex $b 1]]
+proc _exit {} {
+    update
+    exit
 }
 
 
-proc pi {} {
-    pop a
-    isInt $a
-    push [math::bigfloat::pi [lindex $a 1]]
-}
 
-# initialise la calculatrice en créant l'interface GUI
+# initialize the calculator and create the widgets (GUI)
 init
 # chaque fois qu'une commande modifie la pile de nombres,
 # la commande drawStack sera appelée pour la réactualiser
-trace add variable stack write drawStack
+trace add variable ::stack write drawStack
