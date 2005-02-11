a575416ff31384aad3492123b96863eea0a6492f
patthoyts <patthoyts@users.sourceforge.net>
2005-02-11 02:47:58 +0000

    * sasl.test:  Added some tests. 	* sasl.tcl:   Changed namespace and package to SASL. 	* ntlm.tcl:

--- modules/sasl/pkgIndex.tcl
+++ modules/sasl/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # pkgIndex.tcl                                                -*- tcl -*-
 # Copyright (C) 2005 Pat Thoyts <patthoyts@users.sourceforge.net>
-# $Id: pkgIndex.tcl,v 1.1 2005/02/01 02:41:00 patthoyts Exp $
-package ifneeded sasl 1.0.0 [list source [file join $dir sasl.tcl]]
-package ifneeded sasl::ntlm 1.0.0 [list source [file join $dir ntlm.tcl]]
+# $Id: pkgIndex.tcl,v 1.2 2005/02/11 02:47:58 patthoyts Exp $
+package ifneeded SASL 1.0.0 [list source [file join $dir sasl.tcl]]
+package ifneeded SASL::NTLM 1.0.0 [list source [file join $dir ntlm.tcl]]
