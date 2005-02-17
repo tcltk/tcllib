a57d1ccd42a8cc1f9d092c1d13b7c02ede1bc3ae
patthoyts <patthoyts@users.sourceforge.net>
2005-02-17 15:14:25 +0000

    * autoproxy.tcl: Dealt with the parsing requirements as mentioned 	* autoproxy.man: in bug #1099162. In theory we may have different 	* pkgIndex.tcl:  proxy settings for https and http (and other 	protocols) but to deal with these we will need to change the http 	package so we do not deal with them now.

--- modules/http/pkgIndex.tcl
+++ modules/http/pkgIndex.tcl
@@ -1,6 +1,6 @@
 # pkgIndex.tcl for the tcllib http module.
 #
-# $Id: pkgIndex.tcl,v 1.2 2004/07/19 13:40:18 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.3 2005/02/17 15:14:25 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded autoproxy 1.2.0 [list source [file join $dir autoproxy.tcl]]
+package ifneeded autoproxy 1.2.1 [list source [file join $dir autoproxy.tcl]]
