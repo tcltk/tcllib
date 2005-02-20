4cd610fcc00b9f95a53e67d0b18e6718fb7c57d0
patthoyts <patthoyts@users.sourceforge.net>
2005-02-20 01:56:18 +0000

    * rc4.tcl: Ensure all implementations available are tested. Added 	* rc4.man: improved hypen handling. '--' is an optional end-of-options 	* pkgIndex.tcl:	marker.

--- modules/rc4/pkgIndex.tcl
+++ modules/rc4/pkgIndex.tcl
@@ -4,7 +4,7 @@
 #
 # This package has been tested with tcl 8.2.3 and above.
 #
-# $Id: pkgIndex.tcl,v 1.1 2004/07/02 00:01:03 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.2 2005/02/20 01:56:20 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded rc4 1.0.0 [list source [file join $dir rc4.tcl]]
+package ifneeded rc4 1.0.1 [list source [file join $dir rc4.tcl]]
