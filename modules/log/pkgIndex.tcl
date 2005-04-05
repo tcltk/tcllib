48861405f9123942ff1472c52eec213adc77e1e7
Andreas Kupries <akupries@shaw.ca>
2005-04-05 07:11:25 +0000

    Synchronized indexed vs provided versions.
    Synchronized indexed vs provided versions.
    
    Synchronized indexed vs provided versions of logger.
    
    Synchronized indexed vs provided versions.

--- modules/log/pkgIndex.tcl
+++ modules/log/pkgIndex.tcl
@@ -12,4 +12,5 @@ if {![package vsatisfies [package provide Tcl] 8]} {return}
 package ifneeded log 1.2 [list source [file join $dir log.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded logger 0.5 [list source [file join $dir logger.tcl]]
+package ifneeded logger 0.6 [list source [file join $dir logger.tcl]]
+
