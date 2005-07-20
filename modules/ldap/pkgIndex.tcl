56c6b338ed7a9b986edc8f19fadab4d19e3ed8b6
mic42 <mic42>
2005-07-20 11:58:17 +0000

    * ldap.tcl: Applied fix for bug 1239915. Thanks to Pierre David for the patch.    * pkgIndex.tcl: Version raised to 1.2.1

--- modules/ldap/pkgIndex.tcl
+++ modules/ldap/pkgIndex.tcl
@@ -1,4 +1,4 @@
 # Tcl package index file, version 1.1
 
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded ldap 1.2 [list source [file join $dir ldap.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.4]} {return}
+package ifneeded ldap 1.2.1 [list source [file join $dir ldap.tcl]]
