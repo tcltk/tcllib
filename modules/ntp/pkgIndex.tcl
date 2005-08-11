e72a86ea9eb3c46036b1c100a512435a88f0393f
patthoyts <patthoyts@users.sourceforge.net>
2005-08-11 21:48:27 +0000

    * time.tcl: SNTP wasn't working (raised by Donal Fellows). 	Added support for ceptcl in addition to tcludp. Incremented 	version.

--- modules/ntp/pkgIndex.tcl
+++ modules/ntp/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded time 1.1 [list source [file join $dir time.tcl]]
+package ifneeded time 1.2 [list source [file join $dir time.tcl]]
