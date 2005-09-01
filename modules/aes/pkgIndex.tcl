79a76bc154870d94edfffd32c914fe5025fd8554
patthoyts <patthoyts@users.sourceforge.net>
2005-09-01 09:34:52 +0000

    Fix typo in pkgIndex.

--- modules/aes/pkgIndex.tcl
+++ modules/aes/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded report 1.0.0 [list source [file join $dir aes.tcl]]
+package ifneeded aes 1.0.0 [list source [file join $dir aes.tcl]]
