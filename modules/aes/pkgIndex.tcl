85cd3f786688766c7d1407273afaab8396eedda2
patthoyts <patthoyts@users.sourceforge.net>
2005-09-01 12:36:05 +0000

    Added comments and tweaked docs.

--- modules/aes/pkgIndex.tcl
+++ modules/aes/pkgIndex.tcl
@@ -1,2 +1,2 @@
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
+if {![package vsatisfies [package provide Tcl] 8.2]} { return }
 package ifneeded aes 1.0.0 [list source [file join $dir aes.tcl]]
