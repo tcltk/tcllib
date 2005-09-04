51869016cb76d7ab39d5b136f7723df79eb18b80
patthoyts <patthoyts>
2005-09-04 17:09:45 +0000

    * blowfish.tcl:   Added some performance enhancements. Added some 	* blowfish.test:  documentation for the API. Fix testing to test 	* blowfish.man:   each implementation. Checked with tcl 8.2. 	* pkgIndex.tcl:

--- modules/blowfish/pkgIndex.tcl
+++ modules/blowfish/pkgIndex.tcl
@@ -1,8 +1,5 @@
-# pkgIndex.tcl - 
-#
-# Blowfish package index file
-#
-# $Id: pkgIndex.tcl,v 1.1 2004/12/06 16:15:29 patthoyts Exp $
-
-if {![package vsatisfies [package provide Tcl] 8.4]} {return}
+if {![package vsatisfies [package provide Tcl] 8.2]} {
+    # PRAGMA: returnok
+    return
+}
 package ifneeded blowfish 1.0.0 [list source [file join $dir blowfish.tcl]]
