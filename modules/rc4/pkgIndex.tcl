bc7524fadd48961f64b5f80ed236298330b72532
patthoyts <patthoyts@users.sourceforge.net>
2005-09-04 17:19:59 +0000

    * rc4.tcl:  Frink error suppression. 	* rc4.man:  Added documentation for the programming api.

--- modules/rc4/pkgIndex.tcl
+++ modules/rc4/pkgIndex.tcl
@@ -4,7 +4,10 @@
 #
 # This package has been tested with tcl 8.2.3 and above.
 #
-# $Id: pkgIndex.tcl,v 1.2 2005/02/20 01:56:20 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.3 2005/09/04 17:19:59 patthoyts Exp $
 
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
+if {![package vsatisfies [package provide Tcl] 8.2]} {
+    # PRAGMA: returnok
+    return
+}
 package ifneeded rc4 1.0.1 [list source [file join $dir rc4.tcl]]
