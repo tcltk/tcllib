ae1c8f32582b86e2d21ac8d5b55e7364073d85f0
Andreas Kupries <akupries@shaw.ca>
2005-09-28 21:14:31 +0000

    * loggerAppender.tcl: Integrated the formatter/appender utilities 	* loggerAppender.man: provided by [SF Tcllib 1267636]. Created 	* loggerUtils.tcl:    doctools based documentation. Fixed mis- 	* loggerUtils.man:    spellings. Moved the utility code into 	* loggerUtils.test:   the namespace "logger::utils". Added the 	* pkgIndex.tcl:       new packages to the package index.

--- modules/log/pkgIndex.tcl
+++ modules/log/pkgIndex.tcl
@@ -12,5 +12,6 @@ if {![package vsatisfies [package provide Tcl] 8]} {return}
 package ifneeded log 1.2 [list source [file join $dir log.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded logger 0.6.1 [list source [file join $dir logger.tcl]]
-
+package ifneeded logger           0.6.1 [list source [file join $dir logger.tcl]]
+package ifneeded logger::utils    1.2   [list source [file join $dir loggerUtils.tcl]]
+package ifneeded logger::appender 1.2   [list source [file join $dir loggerAppender.tcl]]
