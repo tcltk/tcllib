64222f08a87ff312c30d039dbcda3322b92f0781
Andreas Kupries <andreas.kupries@gmail.com>
2005-10-03 22:59:45 +0000

    * comm.tcl: Accepted [SF Tcllib Bug 1006282], which is actually an 	* comm.man: RFE. Comm channels are extended with an option which 	            allows the user to force the server side to silently 		    ignore connection attempts where the protocol 		    negotiation with the other side failed.

--- modules/comm/pkgIndex.tcl
+++ modules/comm/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if {![package vsatisfies [package provide Tcl] 8]} {return}
-package ifneeded comm 4.2.2 [list source [file join $dir comm.tcl]]
+package ifneeded comm 4.3 [list source [file join $dir comm.tcl]]
