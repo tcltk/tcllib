58d39b824dd5c992beaf6b28e8d75a3e76b88565
patthoyts <patthoyts@users.sourceforge.net>
2005-10-07 12:54:16 +0000

    * sha1.h:  Added byte-order check to resolve bug #1315688.

--- modules/sha1/sha1.h
+++ modules/sha1/sha1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sha1.h,v 1.2 2005/02/21 14:26:26 patthoyts Exp $	*/
+/*	$NetBSD: sha1.h,v 1.3 2005/10/07 12:54:16 patthoyts Exp $	*/
 
 /*
  * SHA-1 in C
@@ -8,6 +8,51 @@
 
 #ifndef _SYS_SHA1_H_
 #define	_SYS_SHA1_H_
+
+/* ---------------------------------------------------------------------- */
+
+#ifndef BYTE_ORDER
+#if (BSD >= 199103)
+# include <machine/endian.h>
+#else
+#ifdef linux
+# include <endian.h>
+#else
+#define LITTLE_ENDIAN 1234 /* least-significant byte first (vax, pc) */
+#define BIG_ENDIAN    4321 /* most-significant byte first (IBM, net) */
+#define PDP_ENDIAN    3412 /* LSB first in word, MSW first in long (pdp)*/
+
+#if defined(vax) || defined(ns32000) || defined(sun386) || defined(i386) || \
+    defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
+    defined(__alpha__) || defined(__alpha) || defined(_M_IX86)
+#define BYTE_ORDER LITTLE_ENDIAN
+#endif
+
+#if defined(sel) || defined(pyr) || defined(mc68000) || defined(sparc) || \
+    defined(is68k) || defined(tahoe) || defined(ibm032) || defined(ibm370) || \
+    defined(MIPSEB) || defined(_MIPSEB) || defined(_IBMR2) || defined(DGUX) ||\
+    defined(apollo) || defined(__convex__) || defined(_CRAY) || \
+    defined(__hppa) || defined(__hp9000) || \
+    defined(__hp9000s300) || defined(__hp9000s700) || \
+    defined (BIT_ZERO_ON_LEFT) || defined(m68k)
+#define BYTE_ORDER BIG_ENDIAN
+#endif
+#endif /* linux */
+#endif /* BSD */
+#endif /* BYTE_ORDER */
+
+#if !defined(BYTE_ORDER) || \
+    (BYTE_ORDER != BIG_ENDIAN && BYTE_ORDER != LITTLE_ENDIAN && \
+    BYTE_ORDER != PDP_ENDIAN)
+/* you must determine what the correct bit order is for
+ * your compiler - the next line is an intentional error
+ * which will force your compiles to bomb until you fix
+ * the above macros.
+ */
+error "Undefined or invalid BYTE_ORDER";
+#endif
+
+/* ---------------------------------------------------------------------- */
 
 #ifdef __alpha
 typedef unsigned int  u_int32_t;
