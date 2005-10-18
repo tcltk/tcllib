8b8a23fadcb06f2d8ad91eefb612ba99e8d0a7d8
Andreas Kupries <akupries@shaw.ca>
2005-10-18 20:20:47 +0000

    * bench_read.tcl: New file. Command and package to read benchmark 	  data in the text, csv, or raw formats.
    	* bench.tcl (::bench::edit): New command. Changes specified
    	  interpreter path to user specified value. Needed if we wish to
    	  merge data coming from the same interpreter, for different
    	  revisions of the package under test.

--- modules/bench/pkgIndex.tcl
+++ modules/bench/pkgIndex.tcl
@@ -4,3 +4,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
 package ifneeded bench            0.1 [list source [file join $dir bench.tcl]]
 package ifneeded bench::out::text 0.1 [list source [file join $dir bench_wtext.tcl]]
 package ifneeded bench::out::csv  0.1 [list source [file join $dir bench_wcsv.tcl]]
+package ifneeded bench::in        0.1 [list source [file join $dir bench_read.tcl]]
