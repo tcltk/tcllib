c3b3141213148ba04aa3f80d6c4fdb22fce22a5c
Andreas Kupries <akupries@shaw.ca>
2005-10-21 23:41:12 +0000

    * sak.tcl: Extended benchmark facility. New command for removal of 	  columns from results. New option to explicitly specify a single 	  interpreter to use.
    	* bench.tcl (::bench::del): New command. Removal of a column from
    	  benchmark data.

--- modules/bench/bench.tcl
+++ modules/bench/bench.tcl
@@ -8,7 +8,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: bench.tcl,v 1.2 2005/10/18 20:20:47 andreas_kupries Exp $
+# RCS: @(#) $Id: bench.tcl,v 1.3 2005/10/21 23:41:13 andreas_kupries Exp $
 
 # ### ### ### ######### ######### ######### ###########################
 ## Requisites - Packages and namespace for the commands and data.
@@ -382,6 +382,45 @@ proc ::bench::edit {data col new} {
 	foreach {desc ip} [split $key ,] break
 	set DATA($desc,$new) $DATA($key)
 	unset                 DATA($key)
+    }
+
+    return [array get DATA]
+}
+
+# ::bench::del --
+#
+#	Remove the data for an interp.
+#
+# Arguments:
+#	Data to edit
+#	Index of column to remove
+#
+# Results:
+#	The changed data set.
+
+proc ::bench::del {data col} {
+
+    if {![string is integer -strict $col]} {
+	return -code error "Ref.column: Expected integer, but got \"$col\""
+    }
+    if {$col < 1} {
+	return -code error "Ref.column out of bounds"
+    }
+
+    array set DATA $data
+    set ipkeys [array names DATA interp:*]
+
+    if {$col > [llength $ipkeys]} {
+	return -code error "Ref.column out of bounds"
+    }
+    incr col -1
+    set refip [lindex [split [lindex [lsort -dict $ipkeys] $col] :] 1]
+
+    unset DATA(interp:$refip)
+
+    # Do not use 'array unset'. Keep 8.2 clean.
+    foreach key [array names DATA *,$refip] {
+	unset DATA($key)
     }
 
     return [array get DATA]
