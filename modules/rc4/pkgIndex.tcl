7d54a4729546f5142e30b6eda12e9c21649feaa0
patthoyts <patthoyts>
2005-12-20 16:19:37 +0000

    * rc4.tcl:   Dealt with bug #1386101 (rc4 critcl + channels broken) 	* rc4.test:  Also implemented a -command option. Added tests for 	* rc4.man:   both and update man page for -command. 	* pkgIndex.tcl: Incremented version to 1.1.0

--- modules/rc4/pkgIndex.tcl
+++ modules/rc4/pkgIndex.tcl
@@ -4,10 +4,10 @@
 #
 # This package has been tested with tcl 8.2.3 and above.
 #
-# $Id: pkgIndex.tcl,v 1.3 2005/09/04 17:19:59 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.4 2005/12/20 16:19:38 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded rc4 1.0.1 [list source [file join $dir rc4.tcl]]
+package ifneeded rc4 1.1.0 [list source [file join $dir rc4.tcl]]
