1f535058e0e148ac0e468c9b257a35f8f97f41ce
patthoyts <patthoyts@users.sourceforge.net>
2006-04-20 01:03:30 +0000

    * gtoken.tcl: Support the X-GOOGLE-TOKEN SASL mechanism. Done 	              as separate package due to additional dependencies 	              (http and tls required)

--- modules/sasl/pkgIndex.tcl
+++ modules/sasl/pkgIndex.tcl
@@ -1,9 +1,10 @@
 # pkgIndex.tcl                                                -*- tcl -*-
 # Copyright (C) 2005 Pat Thoyts <patthoyts@users.sourceforge.net>
-# $Id: pkgIndex.tcl,v 1.3 2005/09/19 12:12:39 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.4 2006/04/20 01:03:30 patthoyts Exp $
 if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
 package ifneeded SASL 1.0.0 [list source [file join $dir sasl.tcl]]
 package ifneeded SASL::NTLM 1.0.0 [list source [file join $dir ntlm.tcl]]
+package ifneeded SASL::XGoogleToken 1.0.0 [list source [file join $dir gtoken.tcl]]
