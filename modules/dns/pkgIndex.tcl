42c316fa082343c2b2b6a04d8ff890785f6be88a
patthoyts <patthoyts@users.sourceforge.net>
2006-04-21 14:29:42 +0000

    Forgot to increment the version

--- modules/dns/pkgIndex.tcl
+++ modules/dns/pkgIndex.tcl
@@ -1,9 +1,9 @@
 # pkgIndex.tcl -
 #
-# $Id: pkgIndex.tcl,v 1.13 2005/09/30 05:36:39 andreas_kupries Exp $
+# $Id: pkgIndex.tcl,v 1.14 2006/04/21 14:29:42 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded dns    1.3.0 [list source [file join $dir dns.tcl]]
+package ifneeded dns    1.3.1 [list source [file join $dir dns.tcl]]
 package ifneeded resolv 1.0.3 [list source [file join $dir resolv.tcl]]
 package ifneeded ip     1.1.0 [list source [file join $dir ip.tcl]]
 package ifneeded spf    1.1.0 [list source [file join $dir spf.tcl]]
