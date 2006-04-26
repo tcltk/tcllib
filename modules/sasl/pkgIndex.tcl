4207739fe7159aa7d5e451515439302fc3da04e5
patthoyts <patthoyts@users.sourceforge.net>
2006-04-26 09:05:11 +0000

    * all:        Incremented version to 1.2.0 	* sasl.man:   Updated documentation. 	* sasl.tcl:   Implemented DIGEST-MD5 server. Enhanced the mechanisms 	              command so we can obtain either client or server 	              mechanisms. Added a threshold value to the mechanisms 		      command to set minimum security. Changed register to 	              uniquely register mechanisms to permit re-sourcing 		      the script.

--- modules/sasl/pkgIndex.tcl
+++ modules/sasl/pkgIndex.tcl
@@ -1,10 +1,10 @@
 # pkgIndex.tcl                                                -*- tcl -*-
 # Copyright (C) 2005 Pat Thoyts <patthoyts@users.sourceforge.net>
-# $Id: pkgIndex.tcl,v 1.5 2006/04/20 02:16:15 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.6 2006/04/26 09:05:11 patthoyts Exp $
 if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded SASL 1.1.0 [list source [file join $dir sasl.tcl]]
+package ifneeded SASL 1.2.0 [list source [file join $dir sasl.tcl]]
 package ifneeded SASL::NTLM 1.0.0 [list source [file join $dir ntlm.tcl]]
 package ifneeded SASL::XGoogleToken 1.0.0 [list source [file join $dir gtoken.tcl]]
