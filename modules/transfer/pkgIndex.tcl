d2265e775d9f2e4b9496e06bfd6dd96e9419ee91
Andreas Kupries <akupries@shaw.ca>
2006-05-24 06:32:27 +0000

    Added queue management package, fixed some markup in previous docs.

--- modules/transfer/pkgIndex.tcl
+++ modules/transfer/pkgIndex.tcl
@@ -1,2 +1,3 @@
 if {![package vsatisfies [package provide Tcl] 8.4]} return
 package ifneeded transfer::copy              0.1 [list source [file join $dir copyops.tcl]]
+package ifneeded transfer::copy::queue       0.1 [list source [file join $dir tqueue.tcl]]
