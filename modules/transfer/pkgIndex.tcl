1043db6e542a63bec99e8fba1709262cad7bd07f
Andreas Kupries <akupries@shaw.ca>
2006-05-25 04:45:39 +0000

    Fixed typo.
    Added package for connection configuration and setup.

--- modules/transfer/pkgIndex.tcl
+++ modules/transfer/pkgIndex.tcl
@@ -3,3 +3,4 @@ package ifneeded transfer::copy              0.1 [list source [file join $dir co
 package ifneeded transfer::copy::queue       0.1 [list source [file join $dir tqueue.tcl]]
 package ifneeded transfer::data::source      0.1 [list source [file join $dir dsource.tcl]]
 package ifneeded transfer::data::destination 0.1 [list source [file join $dir ddest.tcl]]
+package ifneeded transfer::connect           0.1 [list source [file join $dir connect.tcl]]
