6a75acd26c50dbda16ac0316f5c40680313f8fd2
Andreas Kupries <andreas.kupries@gmail.com>
2006-06-30 18:47:59 +0000

    * installed_modules.tcl: Changed to a declarative style (more 	  amenable to automated processing).

--- installed_modules.tcl
+++ installed_modules.tcl
@@ -4,103 +4,110 @@
 # doing so.
 #
 # This file is shared between 'installer.tcl' and 'sak.tcl', like
-# 'tcllib_version.tcl'. The swiss army knife requires access to the
+# 'package_version.tcl'. The swiss army knife requires access to the
 # data in this file to be able to check if there are modules in the
 # directory hierarchy, but missing in the list of installed modules.
 # --------------------------------------------------------------
 
-# Excluded:
-set excluded [list \
-	calendar \
-	]
+proc Exclude     {m} {global excluded ; lappend excluded $m ; return }
+proc Application {a} {global apps     ; lappend apps     $a ; return }
 
-set     modules [list]
-array set guide {}
-foreach {m pkg doc exa} {
-    aes         _tcl  _man  _null
-    asn		_tcl  _man  _null
-    base64	_tcl  _man  _null
-    base32	_tcl  _man  _null
-    bee		_tcl  _man  _null
-    bench	_tcl  _null _null
-    bibtex	_tcl  _man  _null
-    blowfish	_tcl  _man  _null
-    calendar	 _tci _man  _null
-    cmdline	_tcl  _man  _null
-    comm	_tcl  _man  _null
-    control	 _tci _man  _null
-    counter	_tcl  _man  _null
-    crc		_tcl  _man  _null
-    csv		_tcl  _man _exa
-    des		_tcl  _man  _null
-    dns		 _msg _man _exa
-    docstrip	_tcl  _man  _null
-    doctools	 _doc _man _exa
-    exif	_tcl  _man  _null
-    fileutil	_tcl  _man  _null
-    ftp		_tcl  _man _exa
-    ftpd	_tcl  _man _exa
-    fumagic	_tcl  _man  _null
-    grammar_fa  _tcl  _man  _null
-    grammar_me  _tcl  _man  _null
-    grammar_peg _tcl  _man  _null
-    html	_tcl  _man  _null
-    htmlparse	_tcl  _man  _null
-    http	_tcl  _man  _null
-    ident       _tcl  _man  _null
-    inifile     _tcl  _man  _null
-    irc		_tcl  _man _exa
-    javascript	_tcl  _man  _null
-    jpeg        _tcl  _man  _null
-    ldap        _tcl  _man _exa
-    log		 _msg _man  _null
-    math	 _tci _man _exa
-    md4		_tcl  _man  _null
-    md5		_tcl  _man  _null
-    md5crypt	_tcl  _man _null
-    mime	_tcl  _man _exa
-    multiplexer _tcl  _man  _null
-    ncgi	_tcl  _man  _null
-    nntp	_tcl  _man _exa
-    ntp		_tcl  _man _exa
-    page         _tcr _man  _null
-    pluginmgr	_tcl  _man  _null
-    png	        _tcl  _man  _null
-    pop3	_tcl  _man  _null
-    pop3d	_tcl  _man  _null
-    profiler	_tcl  _man  _null
-    rc4         _tcl  _man  _null
-    rcs         _tcl  _man  _null
-    report	_tcl  _man  _null
-    ripemd      _tcl  _man  _null
-    sasl        _tcl  _man  _null
-    sha1	_tcl  _man  _null
-    smtpd	_tcl  _man _exa
-    snit        _tcl  _man  _null
-    soundex	_tcl  _man  _null
-    stooop	_tcl  _man  _null
-    struct	_tcl  _man _exa
-    tar         _tcl  _man  _null
-    textutil	_tex _man  _null
-    tie		_tcl _man  _null
-    tiff	_tcl _man  _null
-    transfer	_tcl  _man  _null
-    treeql	_tcl _man  _null
-    units      	_tcl  _man  _null
-    uri		_tcl  _man  _null
-    uuid	_tcl  _man  _null
-} {
-    lappend modules $m
+proc Module  {m pkg doc exa} {
+    global modules guide
+
+    lappend   modules $m
     set guide($m,pkg) $pkg
     set guide($m,doc) $doc
     set guide($m,exa) $exa
+    return
 }
 
-# And a list of applications.
-set     apps [list  \
-	dtplite     \
-	tcldocstrip \
-	page        \
-]
+set excluded [list]
+set modules  [list]
+set apps     [list]
+array set guide {}
 
+# --------------------------------------------------------------
+# @@ Registration START
+
+Exclude calendar
+
+Module  aes         _tcl  _man  _null
+Module  asn         _tcl  _man  _null
+Module  base64      _tcl  _man  _null
+Module  base32      _tcl  _man  _null
+Module  bee         _tcl  _man  _null
+Module  bench       _tcl  _null _null
+Module  bibtex      _tcl  _man  _null
+Module  blowfish    _tcl  _man  _null
+Module  calendar     _tci _man  _null
+Module  cmdline     _tcl  _man  _null
+Module  comm        _tcl  _man  _null
+Module  control      _tci _man  _null
+Module  counter     _tcl  _man  _null
+Module  crc         _tcl  _man  _null
+Module  csv         _tcl  _man _exa
+Module  des         _tcl  _man  _null
+Module  dns          _msg _man _exa
+Module  docstrip    _tcl  _man  _null
+Module  doctools     _doc _man _exa
+Module  exif        _tcl  _man  _null
+Module  fileutil    _tcl  _man  _null
+Module  ftp         _tcl  _man _exa
+Module  ftpd        _tcl  _man _exa
+Module  fumagic     _tcl  _man  _null
+Module  grammar_fa  _tcl  _man  _null
+Module  grammar_me  _tcl  _man  _null
+Module  grammar_peg _tcl  _man  _null
+Module  html        _tcl  _man  _null
+Module  htmlparse   _tcl  _man  _null
+Module  http        _tcl  _man  _null
+Module  ident       _tcl  _man  _null
+Module  inifile     _tcl  _man  _null
+Module  irc         _tcl  _man _exa
+Module  javascript  _tcl  _man  _null
+Module  jpeg        _tcl  _man  _null
+Module  ldap        _tcl  _man _exa
+Module  log          _msg _man  _null
+Module  math         _tci _man _exa
+Module  md4         _tcl  _man  _null
+Module  md5         _tcl  _man  _null
+Module  md5crypt    _tcl  _man _null
+Module  mime        _tcl  _man _exa
+Module  multiplexer _tcl  _man  _null
+Module  ncgi        _tcl  _man  _null
+Module  nntp        _tcl  _man _exa
+Module  ntp         _tcl  _man _exa
+Module  page         _tcr _man  _null
+Module  pluginmgr   _tcl  _man  _null
+Module  png         _tcl  _man  _null
+Module  pop3        _tcl  _man  _null
+Module  pop3d       _tcl  _man  _null
+Module  profiler    _tcl  _man  _null
+Module  rc4         _tcl  _man  _null
+Module  rcs         _tcl  _man  _null
+Module  report      _tcl  _man  _null
+Module  ripemd      _tcl  _man  _null
+Module  sasl        _tcl  _man  _null
+Module  sha1        _tcl  _man  _null
+Module  smtpd       _tcl  _man _exa
+Module  snit        _tcl  _man  _null
+Module  soundex     _tcl  _man  _null
+Module  stooop      _tcl  _man  _null
+Module  struct      _tcl  _man _exa
+Module  tar         _tcl  _man  _null
+Module  textutil    _tex _man  _null
+Module  tie         _tcl _man  _null
+Module  tiff        _tcl _man  _null
+Module  transfer    _tcl  _man  _null
+Module  treeql      _tcl _man  _null
+Module  units       _tcl  _man  _null
+Module  uri         _tcl  _man  _null
+Module  uuid        _tcl  _man  _null
+
+Application  dtplite
+Application  tcldocstrip
+Application  page
+
+# @@ Registration END
 # --------------------------------------------------------------
