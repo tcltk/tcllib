a7109ce4b7c779e0b6e1fc4524c7666febb6a3fb
duquette <duquette>
2006-08-06 16:21:17 +0000

    Code reorganization; see ChangeLog

--- modules/snit/snit.tcl
+++ modules/snit/snit.tcl
@@ -1,20 +1,41 @@
-# snit.tcl
-# Snit's Not Incr Tcl, a simple object system in Pure Tcl.
+#-----------------------------------------------------------------------
+# TITLE:
+#	snit.tcl
 #
-# Copyright (C) 2003-2005 by William H. Duquette
-# This code is licensed as described in license.txt.
+# AUTHOR:
+#	Will Duquette
+#
+# DESCRIPTION:
+#       Snit's Not Incr Tcl, a simple object system in Pure Tcl.
+#
+#       Snit 1.x Loader 
+#
+#       Copyright (C) 2003-2006 by William H. Duquette
+#       This code is licensed as described in license.txt.
+#
+#-----------------------------------------------------------------------
 
 package require Tcl 8.3
+
+# Define the snit namespace and save the library directory
+
+namespace eval ::snit:: {
+    set library [file dirname [info script]]
+}
 
 # Select the implementation based on the version of the Tcl core
 # executing this code. For 8.3 we use a backport emulating various
 # 8.4 features
 
 if {[package vsatisfies [package provide Tcl] 8.4]} {
-    source [file join [file dirname [info script]] snit84.tcl]
+    source [file join $::snit::library main1.tcl]
 } else {
-    source [file join [file dirname [info script]] snit83.tcl]
+    source [file join $::snit::library main1_83.tcl]
 }
 
-package provide snit 1.1
+# Load the library of Snit validation types.
+
+source [file join $::snit::library validate.tcl]
+
+package provide snit 1.2
 
