aa9338fee79a7cdf46da79b64d4472107dbb625f
mic42 <mic42>
2006-09-01 14:21:34 +0000

    Major upgrades to the ldap package, SASL, TLS and various others

--- modules/ldap/pkgIndex.tcl
+++ modules/ldap/pkgIndex.tcl
@@ -1,4 +1,4 @@
 # Tcl package index file, version 1.1
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded ldap 1.5 [list source [file join $dir ldap.tcl]]
+package ifneeded ldap 1.6 [list source [file join $dir ldap.tcl]]
