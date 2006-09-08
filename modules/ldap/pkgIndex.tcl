e272a4e2229fea50f56ee236a241951f56a47b98
mic42 <mic42>
2006-09-08 21:58:07 +0000

    Added the ldapx subpackage

--- modules/ldap/pkgIndex.tcl
+++ modules/ldap/pkgIndex.tcl
@@ -2,3 +2,6 @@
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded ldap 1.6 [list source [file join $dir ldap.tcl]]
+
+# the OO level wrapper for ldap
+package ifneeded ldapx 0.1 [list source [file join $dir ldapx.tcl]]
