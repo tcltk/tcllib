9b548105b6b3bebad654b25b9753547fc03319ce
patthoyts <patthoyts@users.sourceforge.net>
2006-09-19 10:37:21 +0000

    bug 1560822 - improve handling of hyphen in data

--- modules/blowfish/pkgIndex.tcl
+++ modules/blowfish/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded blowfish 1.0.0 [list source [file join $dir blowfish.tcl]]
+package ifneeded blowfish 1.0.1 [list source [file join $dir blowfish.tcl]]
