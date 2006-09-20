bb42123a9a80ba307122399d6a8eeea343816878
mic42 <mic42>
2006-09-20 21:25:36 +0000

    Bugfixes for broken error handling when calling searchInit with invalid arguments. The search session was not finalized in that case.

--- modules/ldap/pkgIndex.tcl
+++ modules/ldap/pkgIndex.tcl
@@ -1,7 +1,7 @@
 # Tcl package index file, version 1.1
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded ldap 1.6.3 [list source [file join $dir ldap.tcl]]
+package ifneeded ldap 1.6.4 [list source [file join $dir ldap.tcl]]
 
 # the OO level wrapper for ldap
-package ifneeded ldapx 0.2.1 [list source [file join $dir ldapx.tcl]]
+package ifneeded ldapx 0.2.2 [list source [file join $dir ldapx.tcl]]
