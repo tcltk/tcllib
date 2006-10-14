bbfc923676905ab93d1a2f1bbf280cf6908ade2c
mic42 <mic42>
2006-10-14 19:09:20 +0000

    Fixed broken ABANDON message format

--- modules/ldap/pkgIndex.tcl
+++ modules/ldap/pkgIndex.tcl
@@ -1,7 +1,7 @@
 # Tcl package index file, version 1.1
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded ldap 1.6.6 [list source [file join $dir ldap.tcl]]
+package ifneeded ldap 1.6.7 [list source [file join $dir ldap.tcl]]
 
 # the OO level wrapper for ldap
 package ifneeded ldapx 0.2.3 [list source [file join $dir ldapx.tcl]]
