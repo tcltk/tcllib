ac6d06b81ac3ed0b294e9bd2dc9334459b6b3fe0
patthoyts <patthoyts@users.sourceforge.net>
2006-11-06 21:21:59 +0000

    bug #827436 - ensure data section is terminated with CRLF.CRLF on the non Trf code path.

--- modules/mime/pkgIndex.tcl
+++ modules/mime/pkgIndex.tcl
@@ -1,3 +1,3 @@
 if {![package vsatisfies [package provide Tcl] 8.3]} {return}
 package ifneeded mime 1.5.2 [list source [file join $dir mime.tcl]]
-package ifneeded smtp 1.4.2 [list source [file join $dir smtp.tcl]]
+package ifneeded smtp 1.4.3 [list source [file join $dir smtp.tcl]]
