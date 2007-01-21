c3a7def942f31819c07b4e4a193a214ab1f62b28
Andreas Kupries <akupries@shaw.ca>
2007-01-21 22:15:59 +0000

    * struct_set.man: Updated documentation to mention the critcl 	  implementation, version number, etc.
    	* pkgIndex.tcl: Version of sets bumped to 2.2.
    
    	* sets.tcl:     Changed core sets code to support multiple
    	* sets_tcl.tcl: implementations, and Tcl implementation.
    
    	* sets_c.tcl: Critcl based implementation of sets.
    	* sets/ds.h:
    	* sets/m.c:
    	* sets/m.h:
    	* sets/s.c:
    	* sets/s.h:
    
    	* sets.testsuite: Reworked the testsuite to handle both Tcl and
    	* sets.test:      critcl implementations.

--- modules/struct/sets.tcl
+++ modules/struct/sets.tcl
@@ -4,438 +4,179 @@
 #
 #	Definitions for the processing of sets.
 #
-# Copyright (c) 2004 by Andreas Kupries.
+# Copyright (c) 2004-2007 by Andreas Kupries.
 #
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: sets.tcl,v 1.11 2006/09/19 23:36:18 andreas_kupries Exp $
+# RCS: @(#) $Id: sets.tcl,v 1.12 2007/01/21 22:15:59 andreas_kupries Exp $
 #
 #----------------------------------------------------------------------
 
-package require Tcl 8.0
+# @mdgen EXCLUDE: sets_c.tcl
 
-namespace eval ::struct { namespace eval set {} }
+package require Tcl 8.2
 
-namespace eval ::struct::set {
-    namespace export set
-}
+namespace eval ::struct::set {}
 
-##########################
-# Public functions
+# ### ### ### ######### ######### #########
+## Management of set implementations.
 
-# ::struct::set::set --
+# ::struct::set::LoadAccel --
 #
-#	Command that access all set commands.
+#	Loads a named implementation, if possible.
 #
 # Arguments:
-#	cmd	Name of the subcommand to dispatch to.
-#	args	Arguments for the subcommand.
+#	key	Name of the implementation to load.
 #
 # Results:
-#	Whatever the result of the subcommand is.
+#	A boolean flag. True if the implementation
+#	was successfully loaded; and False otherwise.
 
-proc ::struct::set::set {cmd args} {
-    # Do minimal args checks here
-    if { [llength [info level 0]] == 1 } {
-	return -code error "wrong # args: should be \"$cmd ?arg arg ...?\""
-    }
-    ::set sub S$cmd
-    if { [llength [info commands ::struct::set::$sub]] == 0 } {
-	::set optlist [info commands ::struct::set::S*]
-	::set xlist {}
-	foreach p $optlist {
-	    lappend xlist [string range $p 16 end]
+proc ::struct::set::LoadAccel {key} {
+    variable accel
+    set r 0
+    switch -exact -- $key {
+	critcl {
+	    # Critcl implementation of set requires Tcl 8.4.
+	    if {![package vsatisfies [package provide Tcl] 8.4]} {return 0}
+	    if {[catch {package require tcllibc}]} {return 0}
+	    set r [llength [info commands ::struct::set_critcl]]
 	}
-	return -code error \
-		"bad option \"$cmd\": must be [linsert [join [lsort $xlist] ", "] "end-1" "or"]"
+	tcl {
+	    variable selfdir
+	    source [file join $selfdir sets_tcl.tcl]
+	    set r 1
+	}
+        default {
+            return -code error "invalid accelerator/impl. package $key:\
+                must be one of [join [KnownImpl] {, }]"
+        }
     }
-    return [uplevel 1 [linsert $args 0 ::struct::set::$sub]]
+    set accel($key) $r
+    return $r
 }
 
-##########################
-# Implementations of the functionality.
+# ::struct::set::SwitchTo --
 #
-
-# ::struct::set::Sempty --
+#	Activates a loaded named implementation.
 #
-#       Determines emptiness of the set
-#
-# Parameters:
-#       set	-- The set to check for emptiness.
+# Arguments:
+#	key	Name of the implementation to activate.
 #
 # Results:
-#       A boolean value. True indicates that the set is empty.
-#
-# Side effects:
-#       None.
-#
-# Notes:
+#	None.
 
-proc ::struct::set::Sempty {set} {
-    return [expr {[llength $set] == 0}]
-}
+proc ::struct::set::SwitchTo {key} {
+    variable accel
+    variable loaded
 
-# ::struct::set::Ssize --
-#
-#	Computes the cardinality of the set.
-#
-# Parameters:
-#	set	-- The set to inspect.
-#
-# Results:
-#       An integer greater than or equal to zero.
-#
-# Side effects:
-#       None.
+    if {[string equal $key $loaded]} {
+	# No change, nothing to do.
+	return
+    } elseif {![string equal $key ""]} {
+	# Validate the target implementation of the switch.
 
-proc ::struct::set::Ssize {set} {
-    return [llength [Cleanup $set]]
-}
-
-# ::struct::set::Scontains --
-#
-#	Determines if the item is in the set.
-#
-# Parameters:
-#	set	-- The set to inspect.
-#	item	-- The element to look for.
-#
-# Results:
-#	A boolean value. True indicates that the element is present.
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Scontains {set item} {
-    return [expr {[lsearch -exact $set $item] >= 0}]
-}
-
-# ::struct::set::Sunion --
-#
-#	Computes the union of the arguments.
-#
-# Parameters:
-#	args	-- List of sets to unify.
-#
-# Results:
-#	The union of the arguments.
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Sunion {args} {
-    switch -exact -- [llength $args] {
-	0 {return {}}
-	1 {return [lindex $args 0]}
+	if {![info exists accel($key)]} {
+	    return -code error "Unable to activate unknown implementation \"$key\""
+	} elseif {![info exists accel($key)] || !$accel($key)} {
+	    return -code error "Unable to activate missing implementation \"$key\""
+	}
     }
-    foreach setX $args {
-	foreach x $setX {::set ($x) {}}
+
+    # Deactivate the previous implementation, if there was any.
+
+    if {![string equal $loaded ""]} {
+	rename ::struct::set ::struct::set_$loaded
     }
-    return [array names {}]
+
+    # Activate the new implementation, if there is any.
+
+    if {![string equal $key ""]} {
+	rename ::struct::set_$key ::struct::set
+    }
+
+    # Remember the active implementation, for deactivation by future
+    # switches.
+
+    set loaded $key
+    return
 }
 
+proc ::struct::set::Loaded {} {
+    variable loaded
+    return  $loaded
+}
 
-# ::struct::set::Sintersect --
+# ::struct::set::Implementations --
 #
-#	Computes the intersection of the arguments.
+#	Determines which implementations are
+#	present, i.e. loaded.
 #
-# Parameters:
-#	args	-- List of sets to intersect.
+# Arguments:
+#	None.
 #
 # Results:
-#	The intersection of the arguments
-#
-# Side effects:
-#       None.
+#	A list of implementation keys.
 
-proc ::struct::set::Sintersect {args} {
-    switch -exact -- [llength $args] {
-	0 {return {}}
-	1 {return [lindex $args 0]}
+proc ::struct::set::Implementations {} {
+    variable accel
+    set res {}
+    foreach n [array names accel] {
+	if {!$accel($n)} continue
+	lappend res $n
     }
-    ::set res [lindex $args 0]
-    foreach set [lrange $args 1 end] {
-	if {[llength $res] && [llength $set]} {
-	    ::set res [Intersect $res $set]
-	} else {
-	    # Squash 'res'. Otherwise we get the wrong result if res
-	    # is not empty, but 'set' is.
-	    ::set res {}
+    return $res
+}
+
+# ::struct::set::KnownImpl --
+#
+#	Determines which implementations are known
+#	as possible implementations.
+#
+# Arguments:
+#	None.
+#
+# Results:
+#	A list of implementation keys. In the order
+#	of preference, most prefered first.
+
+proc ::struct::set::KnownImpl {} {
+    return {critcl tcl}
+}
+
+# ### ### ### ######### ######### #########
+## Initialization: Data structures.
+
+namespace eval ::struct::set {
+    variable  selfdir [file dirname [info script]]
+    variable  accel
+    array set accel   {tcl 0 critcl 0}
+    variable  loaded  {}
+}
+
+# ### ### ### ######### ######### #########
+## Initialization: Choose an implementation,
+## most prefered first. Loads only one of the
+## possible implementations. And activates it.
+
+namespace eval ::struct::set {
+    variable e
+    foreach e [KnownImpl] {
+	if {[LoadAccel $e]} {
+	    SwitchTo $e
 	    break
 	}
     }
-    return $res
+    unset e
 }
-
-proc ::struct::set::Intersect {A B} {
-    if {[llength $A] == 0} {return {}}
-    if {[llength $B] == 0} {return {}}
-
-    # This is slower than local vars, but more robust
-    if {[llength $B] > [llength $A]} {
-	::set res $A
-	::set A $B
-	::set B $res
-    }
-    ::set res {}
-    foreach x $A {::set ($x) {}}
-    foreach x $B {
-	if {[info exists ($x)]} {
-	    lappend res $x
-	}
-    }
-    return $res
-}
-
-# ::struct::set::Sdifference --
-#
-#	Compute difference of two sets.
-#
-# Parameters:
-#	A, B	-- Sets to compute the difference for.
-#
-# Results:
-#	A - B
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Sdifference {A B} {
-    if {[llength $A] == 0} {return {}}
-    if {[llength $B] == 0} {return $A}
-
-    array set tmp {}
-    foreach x $A {::set tmp($x) .}
-    foreach x $B {catch {unset tmp($x)}}
-    return [array names tmp]
-}
-
-if {0} {
-    # Tcllib SF Bug 1002143. We cannot use the implementation below.
-    # It will treat set elements containing '(' and ')' as array
-    # elements, and this screws up the storage of elements as the name
-    # of local vars something fierce. No way around this. Disabling
-    # this code and always using the other implementation (s.a.) is
-    # the only possible fix.
-
-    if {[package vcompare [package provide Tcl] 8.4] < 0} {
-	# Tcl 8.[23]. Use explicit array to perform the operation.
-    } else {
-	# Tcl 8.4+, has 'unset -nocomplain'
-
-	proc ::struct::set::Sdifference {A B} {
-	    if {[llength $A] == 0} {return {}}
-	    if {[llength $B] == 0} {return $A}
-
-	    # Get the variable B out of the way, avoid collisions
-	    # prepare for "pure list optimization"
-	    ::set ::struct::set::tmp [lreplace $B -1 -1 unset -nocomplain]
-	    unset B
-
-	    # unset A early: no local variables left
-	    foreach [lindex [list $A [unset A]] 0] {.} {break}
-
-	    eval $::struct::set::tmp
-	    return [info locals]
-	}
-    }
-}
-
-# ::struct::set::Ssymdiff --
-#
-#	Compute symmetric difference of two sets.
-#
-# Parameters:
-#	A, B	-- The sets to compute the s.difference for.
-#
-# Results:
-#	The symmetric difference of the two input sets.
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Ssymdiff {A B} {
-    # symdiff == (A-B) + (B-A) == (A+B)-(A*B)
-    if {[llength $A] == 0} {return $B}
-    if {[llength $B] == 0} {return $A}
-    return [Sunion [Sdifference $A $B] [Sdifference $B $A]]
-}
-
-# ::struct::set::Sintersect3 --
-#
-#	Return intersection and differences for two sets.
-#
-# Parameters:
-#	A, B	-- The sets to inspect.
-#
-# Results:
-#	List containing A*B, A-B, and B-A
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Sintersect3 {A B} {
-    return [list [Sintersect $A $B] [Sdifference $A $B] [Sdifference $B $A]]
-}
-
-# ::struct::set::Sequal --
-#
-#	Compares two sets for equality.
-#
-# Parameters:
-#	a	First set to compare.
-#	b	Second set to compare.
-#
-# Results:
-#	A boolean. True if the lists are equal.
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Sequal {A B} {
-    ::set A [Cleanup $A]
-    ::set B [Cleanup $B]
-
-    # Equal if of same cardinality and difference is empty.
-
-    if {[::llength $A] != [::llength $B]} {return 0}
-    return [expr {[llength [Sdifference $A $B]] == 0}]
-}
-
-
-proc ::struct::set::Cleanup {A} {
-    # unset A to avoid collisions
-    if {[llength $A] < 2} {return $A}
-    foreach [lindex [list $A [unset A]] 0] {.} {break}
-    return [info locals]
-}
-
-# ::struct::set::Sinclude --
-#
-#	Add an element to a set.
-#
-# Parameters:
-#	Avar	-- Reference to the set variable to extend.
-#	element	-- The item to add to the set.
-#
-# Results:
-#	None.
-#
-# Side effects:
-#       The set in the variable referenced by Avar is extended
-#	by the element (if the element was not already present).
-
-proc ::struct::set::Sinclude {Avar element} {
-    # Avar = Avar + {element}
-    upvar 1 $Avar A
-    if {![info exists A] || ![Scontains $A $element]} {
-	lappend A $element
-    }
-    return
-}
-
-# ::struct::set::Sexclude --
-#
-#	Remove an element from a set.
-#
-# Parameters:
-#	Avar	-- Reference to the set variable to shrink.
-#	element	-- The item to remove from the set.
-#
-# Results:
-#	None.
-#
-# Side effects:
-#       The set in the variable referenced by Avar is shrunk,
-#	the element remove (if the element was actually present).
-
-proc ::struct::set::Sexclude {Avar element} {
-    # Avar = Avar + {element}
-    upvar 1 $Avar A
-    while {[::set pos [lsearch -exact $A $element]] >= 0} {
-	::set A [lreplace [K $A [::set A {}]] $pos $pos]
-    }
-    return
-}
-
-# ::struct::set::Sadd --
-#
-#	Add a set to a set. Similar to 'union', but the first argument
-#	is a variable.
-#
-# Parameters:
-#	Avar	-- Reference to the set variable to extend.
-#	B	-- The set to add to the set in Avar.
-#
-# Results:
-#	None.
-#
-# Side effects:
-#       The set in the variable referenced by Avar is extended
-#	by all the elements in B.
-
-proc ::struct::set::Sadd {Avar B} {
-    # Avar = Avar + B
-    upvar 1 $Avar A
-    ::set A [Sunion [K $A [::set A {}]] $B]
-    return
-}
-
-# ::struct::set::Ssubtract --
-#
-#	Remove a set from a set. Similar to 'difference', but the first argument
-#	is a variable.
-#
-# Parameters:
-#	Avar	-- Reference to the set variable to shrink.
-#	B	-- The set to remove from the set in Avar.
-#
-# Results:
-#	None.
-#
-# Side effects:
-#       The set in the variable referenced by Avar is shrunk,
-#	all elements of B are removed.
-
-proc ::struct::set::Ssubtract {Avar B} {
-    # Avar = Avar - B
-    upvar 1 $Avar A
-    ::set A [Sdifference [K $A [::set A {}]] $B]
-    return
-}
-
-# ::struct::set::Ssubsetof --
-#
-#	A predicate checking if the first set is a subset
-#	or equal to the second set.
-#
-# Parameters:
-#	A	-- The possible subset.
-#	B	-- The set to compare to.
-#
-# Results:
-#	A boolean value, true if A is subset of or equal to B
-#
-# Side effects:
-#       None.
-
-proc ::struct::set::Ssubsetof {A B} {
-    # A subset|== B <=> (A == A*B)
-    return [Sequal $A [Sintersect $A $B]]
-}
-
-# ::struct::set::K --
-# Performance helper command.
-
-proc ::struct::set::K {x y} {::set x}
 
 # ### ### ### ######### ######### #########
 ## Ready
 
 namespace eval ::struct {
-    # Get 'set::set' into the general structure namespace.
-    namespace import -force set::set
+    # Export the constructor command.
     namespace export set
 }
-package provide struct::set 2.1.1
+
+package provide struct::set 2.2
--- support/installation/version.tcl
+++ support/installation/version.tcl
@@ -19,6 +19,7 @@ critcl      sha256c      sha1/sha256c.tcl
 critcl      uuid         uuid/uuid.tcl
 critcl      struct_treec struct/tree_c.tcl
 critcl      struct_graphc struct/graph_c.tcl
+critcl      struct_setc  struct/sets_c.tcl
 critcl      base32c      base32/base32_c.tcl
 critcl      base32hexc   base32/base32hex_c.tcl
 critcl      ipMorec      dns/ipMoreC.tcl
