6c57bb62f81c58183290883d3c87556a7ec57bbc
Andreas Kupries <andreas.kupries@gmail.com>
2007-05-04 21:31:09 +0000

    * support/installation/modules.tcl: New module 'nns' added, a 	  nano-sized name service based on and for 'comm'. Derived from 	  the nserver code in the Pool_Net bundle of packages.
    	* NNS - Nano Name Service.
    	  Initial commit. TODO: Documentation for client and server, ditto
    	  testsuites, are needed. Only the trivial code shared by both is
    	  documented and tested. Manual testing has been done however,
    	  using the nns* applications, see apps/

---
+++ modules/nns/pkgIndex.tcl
@@ -0,0 +1,9 @@
+if {![package vsatisfies [package provide Tcl] 8]} {return}
+package ifneeded nameserv::common 0.1 [list source [file join $dir common.tcl]]
+
+if {![package vsatisfies [package provide Tcl] 8.4]} {return}
+package ifneeded nameserv         0.1 [list source [file join $dir nns.tcl]]
+package ifneeded nameserv::server 0.1 [list source [file join $dir server.tcl]]
+
+#package ifneeded nameserv::db     0.1 [list source [file join $dir db.tcl]]
+
