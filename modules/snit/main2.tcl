c86d69b8f9fa8d80026519c6085caf95f5e93703
Andreas Kupries <akupries@shaw.ca>
2007-07-04 01:15:00 +0000

    * main1_83.tcl (::snit::Comp.statement.oncget): Fixed double 	* main1.tcl (::snit::Comp.statement.oncget): import of instance 	* main2.tcl (::snit::Comp.statement.oncget): and type variables. 	* snit.man: Bumped versions to 1.3.1 and 2.2.1 respectively. 	* pkgIndex.tcl: 	* snit.tcl: 	* snit2.tcl:

--- modules/snit/main2.tcl
+++ modules/snit/main2.tcl
@@ -894,9 +894,6 @@ proc ::snit::Comp.statement.oncget {option body} {
         return -code error "$errRoot, option \"$option\" unknown"
     }
 
-    # Next, add variable declarations to body:
-    set body "%TVARDECS%\n%IVARDECS%\n$body"
-
     Comp.statement.method _cget$option {_option} $body
     Comp.statement.option $option -cgetmethod _cget$option
 } 
