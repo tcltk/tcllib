012903127b9bdc14537e15e086b3bd16756b60a2
Andreas Kupries <andreas.kupries@gmail.com>
2007-08-20 19:36:48 +0000

    * blowfish.man: Version bumped to 1.0.3 due to bugfix at Mar 12. 	* blowfish.tcl: 	* pkgIndex.tcl:

--- modules/blowfish/pkgIndex.tcl
+++ modules/blowfish/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded blowfish 1.0.2 [list source [file join $dir blowfish.tcl]]
+package ifneeded blowfish 1.0.3 [list source [file join $dir blowfish.tcl]]
