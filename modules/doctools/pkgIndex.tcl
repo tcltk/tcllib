d7464555e9ecd2e16f2dcc88ac04497ea5d35aee
Andreas Kupries <akupries@shaw.ca>
2007-08-20 20:11:58 +0000

    * doctools.tcl: Bumped versions to 1.3 and 0.3 respectively to 	* docidx.tcl:   reflect the extended syntax and bugfixes listed 	* doctoc.tcl:   below. 	* doctools.man: 	* docidx.man: 	* doctoc.man: 	* pkgIndex.tcl:

--- modules/doctools/pkgIndex.tcl
+++ modules/doctools/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded doctools            1.2.1 [list source [file join $dir doctools.tcl]]
-package ifneeded doctools::toc       0.2.1 [list source [file join $dir doctoc.tcl]]
-package ifneeded doctools::idx       0.2.1 [list source [file join $dir docidx.tcl]]
+package ifneeded doctools            1.3   [list source [file join $dir doctools.tcl]]
+package ifneeded doctools::toc       0.3   [list source [file join $dir doctoc.tcl]]
+package ifneeded doctools::idx       0.3   [list source [file join $dir docidx.tcl]]
 package ifneeded doctools::cvs       0.1.1 [list source [file join $dir cvs.tcl]]
 package ifneeded doctools::changelog 0.1.1 [list source [file join $dir changelog.tcl]]
