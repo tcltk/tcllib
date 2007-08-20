029db1ad87c62cdba68a7c061bd9cb929f88a772
Andreas Kupries <akupries@shaw.ca>
2007-08-20 20:11:58 +0000

    * doctools.tcl: Bumped versions to 1.3 and 0.3 respectively to 	* docidx.tcl:   reflect the extended syntax and bugfixes listed 	* doctoc.tcl:   below. 	* doctools.man: 	* docidx.man: 	* doctoc.man: 	* pkgIndex.tcl:

--- modules/doctools/doctoc.tcl
+++ modules/doctools/doctoc.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: doctoc.tcl,v 1.13 2007/03/20 05:11:17 andreas_kupries Exp $
+# RCS: @(#) $Id: doctoc.tcl,v 1.14 2007/08/20 20:11:59 andreas_kupries Exp $
 
 package require Tcl 8.2
 package require textutil::expander
@@ -908,4 +908,4 @@ namespace eval ::doctools::toc {
     catch {search [file join $here                             mpformats]}
 }
 
-package provide doctools::toc 0.2.1
+package provide doctools::toc 0.3
--- modules/doctools/doctools.tcl
+++ modules/doctools/doctools.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: doctools.tcl,v 1.21 2007/03/20 05:11:17 andreas_kupries Exp $
+# RCS: @(#) $Id: doctools.tcl,v 1.22 2007/08/20 20:11:59 andreas_kupries Exp $
 
 package require Tcl 8.2
 package require textutil::expander
@@ -1191,4 +1191,4 @@ namespace eval ::doctools {
     catch {search [file join $here                             mpformats]}
 }
 
-package provide doctools 1.2.1
+package provide doctools 1.3
