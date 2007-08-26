33da6fe3ef545a65edfeeaff24bedfffc646a95c
patthoyts <patthoyts@users.sourceforge.net>
2007-08-26 00:44:34 +0000

    Applied patch #1610330 from Sergei Golovan to provide asynchronous connection for dns over tcp.

--- modules/dns/pkgIndex.tcl
+++ modules/dns/pkgIndex.tcl
@@ -1,9 +1,9 @@
 # pkgIndex.tcl -
 #
-# $Id: pkgIndex.tcl,v 1.16 2007/08/20 20:03:04 andreas_kupries Exp $
+# $Id: pkgIndex.tcl,v 1.17 2007/08/26 00:44:34 patthoyts Exp $
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded dns    1.3.1 [list source [file join $dir dns.tcl]]
+package ifneeded dns    1.3.2 [list source [file join $dir dns.tcl]]
 package ifneeded resolv 1.0.3 [list source [file join $dir resolv.tcl]]
 package ifneeded ip     1.1.2 [list source [file join $dir ip.tcl]]
 package ifneeded spf    1.1.0 [list source [file join $dir spf.tcl]]
