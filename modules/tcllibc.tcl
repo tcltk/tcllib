23a20015bcecfcc3c0c711633ae89953bb06a727
Andreas Kupries <andreas.kupries@gmail.com>
2007-08-29 22:10:30 +0000

    * tree.testsuite: Simplified constraint definition, we now have a 	  standard syntax (backward compat def for tcltest 1.0 in the 	  devtools/testutilities.tcl).
    	* sets.testsuite: Added a test demonstrating a shimmering problem
    	  in the C implementation of struct::set (handling of pure list
    	  values is bad).
    
    	* sets/s.c: Fixed the bug demonstrated by the new test, see
    	* pkgIndex.tcl: above. Version of package bumped to 2.2.1. Version
    	  of tcllibc bumped to 0.3.2.
    
    	* modules/tcllibc.tcl: Version of package bumped to 0.3.2 for the
    	  bugfix in the C implementation of struct::set (v 2.2.1).

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.1
+package provide tcllibc 0.3.2
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.6 2006/11/04 15:20:36 patthoyts Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.7 2007/08/29 22:10:30 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
