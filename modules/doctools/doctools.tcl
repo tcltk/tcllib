5333f0e1937c86c2a8651b5e6345901b174daba6
Andreas Kupries <akupries@shaw.ca>
2007-09-25 23:11:11 +0000

    * doctools.tcl: Extended processor to pass the pass number to the 	* checker.tcl: checker layer as well. Modified the checker layer 	* pkgIndex.tcl: to suppress generation of warnings in all but the 	* doctools.man: first pass, to avoid replication. Bumped version 	  to 1.3.1. This fixes [SF Tcllib Bug 1800413].

--- modules/doctools/doctools.tcl
+++ modules/doctools/doctools.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: doctools.tcl,v 1.22 2007/08/20 20:11:59 andreas_kupries Exp $
+# RCS: @(#) $Id: doctools.tcl,v 1.23 2007/09/25 23:11:11 andreas_kupries Exp $
 
 package require Tcl 8.2
 package require textutil::expander
@@ -471,7 +471,7 @@ proc ::doctools::_format {name text} {
 	    catch {$format_ip eval fmt_shutdown}
 	    return -code error "Could not initialize pass $n of engine"
 	}
-	$chk_ip eval ck_initialize
+	$chk_ip eval ck_initialize $p
 
 	if {[catch {set result [$expander expand $text]} msg]} {
 	    catch {$format_ip eval fmt_shutdown}
@@ -1191,4 +1191,4 @@ namespace eval ::doctools {
     catch {search [file join $here                             mpformats]}
 }
 
-package provide doctools 1.3
+package provide doctools 1.3.1
--- modules/doctools/pkgIndex.tcl
+++ modules/doctools/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded doctools            1.3   [list source [file join $dir doctools.tcl]]
+package ifneeded doctools            1.3.1 [list source [file join $dir doctools.tcl]]
 package ifneeded doctools::toc       0.3   [list source [file join $dir doctoc.tcl]]
 package ifneeded doctools::idx       0.3   [list source [file join $dir docidx.tcl]]
 package ifneeded doctools::cvs       0.1.1 [list source [file join $dir cvs.tcl]]
