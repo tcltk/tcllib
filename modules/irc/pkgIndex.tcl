51227b7e3141a444886bf60a6a70feeb45fb218a
patthoyts <patthoyts@users.sourceforge.net>
2007-10-19 21:17:13 +0000

    Added picoirc package

--- modules/irc/pkgIndex.tcl
+++ modules/irc/pkgIndex.tcl
@@ -1,7 +1,8 @@
 # pkgIndex.tcl                                                    -*- tcl -*-
-# $Id: pkgIndex.tcl,v 1.7 2006/04/23 22:35:57 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.8 2007/10/19 21:17:13 patthoyts Exp $
 if { ![package vsatisfies [package provide Tcl] 8.3] } {
     # PRAGMA: returnok
     return 
 }
 package ifneeded irc 0.6 [list source [file join $dir irc.tcl]]
+package ifneeded picoirc 0.5 [list source [file join $dir picoirc.tcl]]
