64d290bf91f0bea088a115d7f99f6ae6c0cf344f
patthoyts <patthoyts>
2008-06-24 22:06:56 +0000

    Fixed ctcp responses to use NOTICE and added wubchain

--- modules/irc/pkgIndex.tcl
+++ modules/irc/pkgIndex.tcl
@@ -1,8 +1,8 @@
 # pkgIndex.tcl                                                    -*- tcl -*-
-# $Id: pkgIndex.tcl,v 1.8 2007/10/19 21:17:13 patthoyts Exp $
+# $Id: pkgIndex.tcl,v 1.9 2008/06/24 22:06:56 patthoyts Exp $
 if { ![package vsatisfies [package provide Tcl] 8.3] } {
     # PRAGMA: returnok
     return 
 }
 package ifneeded irc 0.6 [list source [file join $dir irc.tcl]]
-package ifneeded picoirc 0.5 [list source [file join $dir picoirc.tcl]]
+package ifneeded picoirc 0.5.1 [list source [file join $dir picoirc.tcl]]
