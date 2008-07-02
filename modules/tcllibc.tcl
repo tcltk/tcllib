48970f7abc9a94aacf23344b08e3053f03bac2ed
Andreas Kupries <akupries@shaw.ca>
2008-07-02 23:35:07 +0000

    * queue.tcl: Changed core queue code to support multiple 	* queue_tcl.tcl: implementations, and Tcl implementation. Bumped 	* queue.man: to version 1.4.1. Updated documentation to mention 	* pkgIndex.tcl: the critcl implementation, version number, 	  etc. Reworked the Tcl implementation as well for speed (split 	  buffer, indexing, avoid memcopies, K-operator).
    	* queue_c.tcl: Critcl based implementation of queues.
    	* queue/ds.h:
    	* queue/m.c:
    	* queue/m.h:
    	* queue/q.c:
    	* queue/q.h:
    
    	* queue.testsuite: Reworked the testsuite to handle both Tcl and
    	* queue.test:      critcl implementations.

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.2
+package provide tcllibc 0.3.4
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.7 2007/08/29 22:10:30 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.8 2008/07/02 23:35:52 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
