1a1228d011ffc39f37d7c1887c838df8835f5401
Andreas Kupries <akupries@shaw.ca>
2008-07-03 17:47:29 +0000

    * queue/m.c: Separated qdump from queue_debug mode. 	* queue/util.h: Brought all assert macros in line with the 	* stack/util.h: definitions provided to graph (which print 	* tree/util.h: file/line information). Also activated assertions 	  throughout for regular build.

--- modules/struct/tree/util.h
+++ modules/struct/tree/util.h
@@ -17,12 +17,14 @@
  */
 
 #undef  TREE_DEBUG
-/*#define TREE_DEBUG 1*/
+#define TREE_DEBUG 1
 
 #ifdef TREE_DEBUG
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
 #define RANGEOK(i,n) ((0 <= (i)) && (i < (n)))
-#define ASSERT(x,msg) if (!(x)) { Tcl_Panic (msg);}
-#define ASSERT_BOUNDS(i,n) ASSERT (RANGEOK(i,n),"array index out of bounds")
+#define ASSERT(x,msg) if (!(x)) { Tcl_Panic (msg " (" #x "), in file " __FILE__ " @line " STR(__LINE__)) ;}
+#define ASSERT_BOUNDS(i,n) ASSERT (RANGEOK(i,n),"array index out of bounds: " STR(i) " > " STR(n))
 #else
 #define ASSERT(x,msg)
 #define ASSERT_BOUNDS(i,n)
