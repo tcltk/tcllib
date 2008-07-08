20eb684dc04e05d7340650cc1e02be8e1a2441b3
Andreas Kupries <akupries@shaw.ca>
2008-07-08 23:03:58 +0000

    * changelog.man: Bumped the packages to version 1. They have 	* changelog.tcl: been on 0 long enough. 	* cvs.man: 	* cvs.tcl: 	* docidx.man: 	* docidx.tcl: 	* doctoc.man: 	* doctoc.tcl:

--- modules/doctools/docidx.tcl
+++ modules/doctools/docidx.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: docidx.tcl,v 1.16 2008/03/12 17:52:39 andreas_kupries Exp $
+# RCS: @(#) $Id: docidx.tcl,v 1.17 2008/07/08 23:03:58 andreas_kupries Exp $
 
 package require Tcl 8.2
 package require textutil::expander
@@ -902,4 +902,4 @@ namespace eval ::doctools::idx {
     catch {search [file join $here                             mpformats]}
 }
 
-package provide doctools::idx 0.3.1
+package provide doctools::idx 1
