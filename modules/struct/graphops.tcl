369656d04e06c6e4bbeb918641a8c1a67f901ea8
Andreas Kupries <akupries@shaw.ca>
2008-11-08 06:43:18 +0000

    * graphops.tcl: Continued integration of graph algorithms. 	* graphops.man: Minimum spanning tree/forest as per Prim. 	* graph/tests/ops/prim.test: Bumped version to 0.2 	* graph/tests/XOpsControl: 	* graph/tests/XOpsSetup: 	* pkgIndex.tcl:

--- modules/struct/graphops.tcl
+++ modules/struct/graphops.tcl
@@ -8,7 +8,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: graphops.tcl,v 1.2 2008/11/07 03:47:33 andreas_kupries Exp $
+# RCS: @(#) $Id: graphops.tcl,v 1.3 2008/11/08 06:43:18 andreas_kupries Exp $
 
 # ### ### ### ######### ######### #########
 ## Requisites
@@ -16,15 +16,15 @@
 package require Tcl 8.4
 
 package require struct::disjointset ; # Used by kruskal
-package require struct::prioqueue   ; # Used by kruskal
+package require struct::prioqueue   ; # Used by kruskal, prim
 
 # ### ### ### ######### ######### #########
-## 
+##
 
 namespace eval ::struct::graph::op {}
 
 # ### ### ### ######### ######### #########
-## 
+##
 
 # This command constructs an adjacency matrix representation of the
 # graph argument.
@@ -66,7 +66,7 @@ proc ::struct::graph::op::toAdjacencyMatrix {g} {
     set baseRow 0
     foreach n $nodeList {
 	lappend baseRow 0
-    }  
+    }
 
     foreach node $nodeList {
 
@@ -101,13 +101,14 @@ proc ::struct::graph::op::toAdjacencyMatrix {g} {
 }
 
 # ### ### ### ######### ######### #########
-## 
+##
 
 # This command finds a minimum spanning tree/forest (MST) of the graph
-# argument, using the algorithm developed by Kruskal. The result is a
-# set (as list) containing the names of the arcs in the MST. The set
-# of nodes of the MST is implied by set of arcs, and thus not given
-# explicitly. The algorithm does not consider arc directions.
+# argument, using the algorithm developed by Joseph Kruskal. The
+# result is a set (as list) containing the names of the arcs in the
+# MST. The set of nodes of the MST is implied by set of arcs, and thus
+# not given explicitly. The algorithm does not consider arc
+# directions. Note that unconnected nodes are left out of the result.
 
 # Reference: http://en.wikipedia.org/wiki/Kruskal%27s_algorithm
 
@@ -148,7 +149,7 @@ proc ::struct::graph::op::kruskal {g} {
     while {[$consider size]} {
 	set minarc [$consider get]
 	set origin [$g arc source $minarc]
-	set destin [$g arc target $minarc]	
+	set destin [$g arc target $minarc]
 
 	# Ignore the arc if both ends are in the same partition. Using
 	# it would add a cycle to the result, i.e. it would not be a
@@ -172,6 +173,114 @@ proc ::struct::graph::op::kruskal {g} {
     return $result
 }
 
+# ### ### ### ######### ######### #########
+##
+
+# This command finds a minimum spanning tree/forest (MST) of the graph
+# argument, using the algorithm developed by Prim. The result is a
+# set (as list) containing the names of the arcs in the MST. The set
+# of nodes of the MST is implied by set of arcs, and thus not given
+# explicitly. The algorithm does not consider arc directions.
+
+# Reference: http://en.wikipedia.org/wiki/Prim%27s_algorithm
+
+proc ::struct::graph::op::prim {g} {
+    VerifyWeightsAreOk $g
+
+    # Fill an array with all nodes, to track which nodes have been
+    # visited at least once. When the inner loop runs out of nodes and
+    # we still have some left over we restart using one of the
+    # leftover as new starting point. In this manner we get the MST of
+    # the whole graph minus unconnected nodes, instead of only the MST
+    # for the component the initial starting node is in.
+
+    array set unvisited {}
+    foreach n [$g nodes] { set unvisited($n) . }
+
+    # Transient helper data structure. A priority queue for the nodes
+    # and arcs under consideration for inclusion into the MST. Each
+    # element of the queue is a list containing node name, a flag bit,
+    # and arc name, in this order. The associated priority is the
+    # weight of the arc. The flag bit is set for the initial queue
+    # entry only, containing a fake (empty) arc, to trigger special
+    # handling.
+
+    set consider [::struct::prioqueue -dictionary consider]
+
+    # More data structures, the result arrays.
+    array set weightmap {} ; # maps nodes to min arc weight seen so
+			     # far. This is the threshold other arcs
+			     # on this node will have to beat to be
+			     # added to the MST.
+    array set arcmap    {} ; # maps arcs to nothing, these are the
+			     # arcs in the MST.
+
+    while {[array size unvisited]} {
+	# Choose a 'random' node as the starting point for the inner
+	# loop, prim's algorithm, and put it on the queue for
+	# consideration. Then we iterate until we have considered all
+	# nodes in the its component.
+
+	set startnode [lindex [array names unvisited] 0]
+	$consider put [list $startnode 1 {}] 0
+
+	while {[$consider size] > 0} {
+	    # Pull the next minimum weight to look for. This is the
+	    # priority of the next item we can get from the queue. And the
+	    # associated node/decision/arc data.
+
+	    set arcweight [$consider peekpriority 1]
+
+	    foreach {v arcundefined arc} [$consider get] break
+	    #8.5: lassign [$consider get] v arcundefined arc
+
+	    # Two cases to consider: The node v is already part of the
+	    # MST, or not. If yes we check if the new arcweight is better
+	    # than what we have stored already, and update accordingly.
+
+	    if {[info exists weightmap($v)]} {
+		set currentweight $weightmap($v)
+		if {$arcweight < $currentweight} {
+		    # The new weight is better, update to use it as
+		    # the new threshold. Note that this fill not touch
+		    # any other arcs found for this node, as these are
+		    # still minimal.
+
+		    set weightmap($v) $arcweight
+		    set arcmap($arc)  .
+		}
+	    } else {
+		# Node not yet present. Save weight and arc. The
+		# latter if and only the arc is actually defined. For
+		# the first, initial queue entry, it is not.  Then we
+		# add all the arcs adjacent to the current node to the
+		# queue to consider them in the next rounds.
+
+		set weightmap($v) $arcweight
+		if {!$arcundefined} {
+		    set arcmap($arc) .
+		}
+		foreach adjacentarc [$g arcs -adj $v] {
+		    set weight    [$g arc  getweight   $adjacentarc]
+		    set neighbour [$g node opposite $v $adjacentarc]
+		    $consider put [list $neighbour 0 $adjacentarc] $weight
+		}
+	    }
+
+	    # Mark the node as visited, belonging to the current
+	    # component. Future iterations will ignore it.
+	    unset -nocomplain unvisited($v)
+	}
+    }
+
+    # We are done. Get rid of the transient helper structure and
+    # return our result.
+
+    $consider destroy
+
+    return [array names arcmap]
+}
+
 #
 ## place holder for the operations to come
 #
@@ -193,4 +302,4 @@ namespace eval ::struct::graph::op {
     #namespace export ...
 }
 
-package provide struct::graph::op 0.1
+package provide struct::graph::op 0.2
