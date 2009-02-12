fe5efaece3448ab3b3b020bf25ec1e569e89a34f
Andreas Kupries <akupries@shaw.ca>
2009-02-12 05:42:46 +0000

    * checker_toc.tcl: TOC language extended to allow empty tocs, empty 	* docidx.test: divisions, and the mixing of items and divisions at 	* doctoc.tcl: every level. Extended testsuite. Updated documentation. 	* doctoc.test: Bumped version to 1.1. Fixed bug in the documentation 	* doctoc_lang_cmdref.man: of markup command 'item' (arguments were 	* doctoc_lang_intro.man: missing). 	* doctoc_lang_syntax.man: 	* doctoc.man: 	* pkgIndex.tcl:

--- modules/doctools/doctoc.tcl
+++ modules/doctools/doctoc.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: doctoc.tcl,v 1.18 2009/01/30 04:56:47 andreas_kupries Exp $
+# RCS: @(#) $Id: doctoc.tcl,v 1.19 2009/02/12 05:42:47 andreas_kupries Exp $
 
 package require Tcl 8.2
 package require textutil::expander
@@ -913,4 +913,4 @@ namespace eval ::doctools::toc {
     catch {search [file join $here                             mpformats]}
 }
 
-package provide doctools::toc 1
+package provide doctools::toc 1.1
