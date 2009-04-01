a35f3b5f0af9604050e0f079180b46ba80cc6d49
Andreas Kupries <andreas.kupries@gmail.com>
2009-04-01 19:50:52 +0000

    * multiop.tcl (Copy): Fix special case with copying directories 	* multiop.man: where the source is one level to deep, or copy fails. 	* pkgIndex.tcl: Bumped version to 0.5.3.

--- modules/fileutil/pkgIndex.tcl
+++ modules/fileutil/pkgIndex.tcl
@@ -6,4 +6,4 @@ package ifneeded fileutil::traverse 0.4.1 [list source [file join $dir traverse.
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded fileutil::multi     0.1   [list source [file join $dir multi.tcl]]
-package ifneeded fileutil::multi::op 0.5.2 [list source [file join $dir multiop.tcl]]
+package ifneeded fileutil::multi::op 0.5.3 [list source [file join $dir multiop.tcl]]
