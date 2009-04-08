44ccf767c46f6b60c88bd39f38c50ab88933bcc0
Andreas Kupries <akupries@shaw.ca>
2009-04-08 04:10:32 +0000

    * export_html.tcl: Fixed plugin API check.         * export_nroff.tcl: Ditto.         * export_text.tcl: Ditto.         * export_wiki.tcl: Ditto, plus fix in file mapping. 	* pkgIndex.tcl: Bumped to version 0.2.

--- modules/doctools2idx/export_nroff.tcl
+++ modules/doctools2idx/export_nroff.tcl
@@ -7,7 +7,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: export_nroff.tcl,v 1.1 2009/04/01 04:28:37 andreas_kupries Exp $
+# RCS: @(#) $Id: export_nroff.tcl,v 1.2 2009/04/08 04:10:32 andreas_kupries Exp $
 
 # This package is a plugin for the doctools::idx v2 system.  It takes
 # the list serialization of a keyword index and produces text in nroff
@@ -17,12 +17,12 @@
 ## Requisites
 
 package require Tcl 8.4
-package require doctools::idx::export       ; # Presence of this
-					      # pseudo package
-					      # indicates execution
-					      # inside of a properly
-					      # initialized plugin
-					      # interpreter.
+package require doctools::idx::export::plugin ; # Presence of this
+						# pseudo package
+						# indicates execution
+						# inside of a properly
+						# initialized plugin
+						# interpreter.
 package require doctools::idx::structure    ; # Verification that the
 					      # input is proper.
 package require doctools::text              ; # Text assembly package
@@ -207,5 +207,5 @@ proc .TH {name} {
 # ### ### ### ######### ######### #########
 ## Ready
 
-package provide doctools::idx::export::nroff 0.1
+package provide doctools::idx::export::nroff 0.2
 return
