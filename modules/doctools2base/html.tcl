0da2573f8942671a44beb7778ff8a66186c079f6
Andreas Kupries <akupries@shaw.ca>
2009-04-17 05:41:24 +0000

    * html.tcl: Fix version mismatch. 	* text.tcl: Fix version mismatch. 	* tests/common: Fix handling of directories.

--- modules/doctools2base/html.tcl
+++ modules/doctools2base/html.tcl
@@ -205,5 +205,5 @@ namespace eval ::doctools::html {
 }
 
 # # ## ### ##### ######## ############# #####################
-package provide doctools::html 1
+package provide doctools::html 0.1
 return
