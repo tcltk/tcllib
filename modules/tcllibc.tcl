d9dfad6079bca8d38030decabb0bdbffba71c40b
Andreas Kupries <andreas.kupries@gmail.com>
2009-09-15 04:00:52 +0000

    * graph/filter.c: Fixed bug in 'arcs -adj' implementation. Wrong 	* graph/tests/arcs.test: range check. Extended testsuite. Bumped 	* ../tcllibc.tcl: version of tcllibc containing this fix to 0.3.5.

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.4
+package provide tcllibc 0.3.5
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.8 2008/07/02 23:35:52 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.9 2009/09/15 04:00:52 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
