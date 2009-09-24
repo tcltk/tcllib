ae426ed096ecbc79270feec94b1b94baab38806c
Andreas Kupries <andreas.kupries@gmail.com>
2009-09-24 16:23:46 +0000

    * graph.man: Added method 'arc nodes' to the Tcl and C 	* graph.tcl: implementations. Extended testsuite and 	* graph_tcl.tcl: documentation. Bumped package to 	* graph/methods.c: version 2.4. Bumped the tcllibc 	* graph/objcmd.c: umbrella package to version 0.3.6. 	* graph/tests/arc/nodes.test: 	* graph/tests/command.test: 	* graph/tests/Xcontrol: 	* pkgIndex.tcl: 	* ../tcllibc.tcl:

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.5
+package provide tcllibc 0.3.6
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.9 2009/09/15 04:00:52 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.10 2009/09/24 16:23:46 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
