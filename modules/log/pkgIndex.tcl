ed7fb647b7151fb61c9eeb9e796e72a6b2ba7d06
Andreas Kupries <akupries@shaw.ca>
2009-10-27 20:27:10 +0000

    * log.man: Flush writes to the log channel. Bumped to 	* log.tcl: version 1.3. 	* pkgIndex.tcl:

--- modules/log/pkgIndex.tcl
+++ modules/log/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
-package ifneeded log 1.2.1 [list source [file join $dir log.tcl]]
+package ifneeded log 1.3 [list source [file join $dir log.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded logger           0.9 [list source [file join $dir logger.tcl]]
