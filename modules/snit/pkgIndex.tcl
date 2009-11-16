e7fc7da5f1c588d6489ccf9a81b3dd1390c11c6c
Andreas Kupries <akupries@shaw.ca>
2009-11-16 23:52:28 +0000

    * main2.tcl (::snit::RT.typemethod.destroy) 	(::snit::RT.typemethod.info.instances): 	* main1.tcl (::snit::RT.typemethod.info.instances) 	(::snit::RT.typemethod.destroy): [Bug 2898640]. Fixed handling of 	* snit.tcl: unrelated namespaces by restricting the set of 	* snit2.tcl: children to look at. Bumped versions of v1 and v2 to 	* pkgIndex.tcl: 1.4.1 and 2.3.1 respectively. 	* snit.man:

--- modules/snit/pkgIndex.tcl
+++ modules/snit/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {[package vsatisfies [package provide Tcl] 8.5]} {
-    package ifneeded snit 2.3 \
+    package ifneeded snit 2.3.1 \
         [list source [file join $dir snit2.tcl]]
 }
 
-package ifneeded snit 1.4 [list source [file join $dir snit.tcl]]
+package ifneeded snit 1.4.1 [list source [file join $dir snit.tcl]]
