727c4174d0744ab123b97343ed2553e6bfce2274
Andreas Kupries <akupries@shaw.ca>
2009-11-30 16:47:44 +0000

    Bumped the tcllibc umbrella package to version 0.3.7 for struct::stack 1.4 changes.

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.6
+package provide tcllibc 0.3.7
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.10 2009/09/24 16:23:46 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.11 2009/11/30 16:47:44 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
