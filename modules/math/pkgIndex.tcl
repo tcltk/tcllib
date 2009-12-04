89dd49aa8715e11ff1e106ba479439c5a818abc8
Andreas Kupries <akupries@shaw.ca>
2009-12-04 17:37:47 +0000

    * math.man: [Bug 1998628]. Accepted fix by Arjen Markus, with 	* math.tcl: modifications. Extended testsuite. Bumped version 	* math.test: to 1.2.5. 	* pkgIndex.tcl:

--- modules/math/pkgIndex.tcl
+++ modules/math/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded math                    1.2.4 [list source [file join $dir math.tcl]]
+package ifneeded math                    1.2.5 [list source [file join $dir math.tcl]]
 package ifneeded math::geometry          1.0.4 [list source [file join $dir geometry.tcl]]
 package ifneeded math::fuzzy             0.2   [list source [file join $dir fuzzy.tcl]]
 package ifneeded math::complexnumbers    1.0.2 [list source [file join $dir qcomplex.tcl]]
