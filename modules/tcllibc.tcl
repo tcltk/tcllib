47ede841a158587d5846235e89ad807bea4d65e6
Andreas Kupries <akupries@shaw.ca>
2010-03-30 22:53:28 +0000

    Bumped tcllibc version for  PT critcl implementations

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,10 +2,10 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.7
+package provide tcllibc 0.3.8
 
 namespace eval ::tcllib {
-    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.11 2009/11/30 16:47:44 andreas_kupries Exp $}
+    variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.12 2010/03/30 22:53:28 andreas_kupries Exp $}
     critcl::ccode {
         /* no code required in this file */
     }
