472a616c1db64ac3ea48756eb51bd368122a14d5
Andreas Kupries <akupries@shaw.ca>
2010-04-06 17:02:25 +0000

    * geometry.tcl (findLineIntersection): Fixed numerical 	* geometry.man: instability in the algorithm by replacing 	* geometry.test: it with Kevin's parametric code. Updated 	* pkgIndex.tcl: documentation, testsuite. Bumped to 	  version 1.1.1.

--- modules/math/pkgIndex.tcl
+++ modules/math/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded math                    1.2.5 [list source [file join $dir math.tcl]]
-package ifneeded math::geometry          1.1   [list source [file join $dir geometry.tcl]]
+package ifneeded math::geometry          1.1.1 [list source [file join $dir geometry.tcl]]
 package ifneeded math::fuzzy             0.2.1 [list source [file join $dir fuzzy.tcl]]
 package ifneeded math::complexnumbers    1.0.2 [list source [file join $dir qcomplex.tcl]]
 package ifneeded math::special           0.2.2 [list source [file join $dir special.tcl]]
