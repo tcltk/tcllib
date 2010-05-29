4b09812f370f40f85d53ae375177afc83916b6df
Andreas Kupries <andreas.kupries@gmail.com>
2010-05-29 22:41:18 +0000

    * namespacex.tcl: Fixed typos in docs, and left-over bug. 	* namespacex.man: Bumped version to 0.1. 	* pkgIndex.tcl:

--- modules/namespacex/pkgIndex.tcl
+++ modules/namespacex/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.5]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded namespacex 0 [list source [file join $dir namespacex.tcl]]
+package ifneeded namespacex 0.1 [list source [file join $dir namespacex.tcl]]
