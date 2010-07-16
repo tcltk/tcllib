3dedf1a09d48e0bc7aa4c06534d7c978001e3d3e
exgerhardr <exgerhardr>
2010-07-16 14:42:11 +0000

    Updated version number in pkgIndex.

--- modules/imap4/pkgIndex.tcl
+++ modules/imap4/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded report 0.2 [list source [file join $dir imap4.tcl]]
+package ifneeded report 0.3 [list source [file join $dir imap4.tcl]]
