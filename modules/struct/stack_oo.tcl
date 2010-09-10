fc1316c9e7e522f335672b683ad522d1569b4d96
Andreas Kupries <akupries@shaw.ca>
2010-09-10 17:31:04 +0000

    * queue_oo.tcl: Established minimum requirements for TclOO 	* stack_oo.tcl: to support the two classes. Bug 3062782 was 	  a very old TclOO (0.3) interfering in the test.

--- modules/struct/stack_oo.tcl
+++ modules/struct/stack_oo.tcl
@@ -7,7 +7,9 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: stack_oo.tcl,v 1.3 2010/03/25 13:56:30 andreas_kupries Exp $
+# RCS: @(#) $Id: stack_oo.tcl,v 1.4 2010/09/10 17:31:04 andreas_kupries Exp $
+
+package require TclOO 0.6.1
 
 # Cleanup first
 catch {namespace delete ::struct::stack::stack_oo}
