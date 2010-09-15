e9c699c480bfb60db2c291da85cbf3370f29328a
Andreas Kupries <akupries@shaw.ca>
2010-09-15 19:48:33 +0000

    * comm.tcl (::comm::commIncoming): [Bug 3066872]: Replaced 	blocking gets call to read line of offered protocols by 	non-blocking gets and additional event handling. The procedure 	"commIncoming" is split into two.
    	* comm.tcl (::comm::Word0): [Bug 2972571]: Fixed misdetection
    	* comm.man: of quoted brace due to not handling \\ on its
    	* comm.test: own. Extended testsuite. Updated docs.
    
    	* pkgIndex.tcl: Bumped to version 4.6.2.

--- modules/comm/pkgIndex.tcl
+++ modules/comm/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.3]} {return}
-package ifneeded comm 4.6.1 [list source [file join $dir comm.tcl]]
+package ifneeded comm 4.6.2 [list source [file join $dir comm.tcl]]
