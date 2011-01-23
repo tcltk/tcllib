d74c0c7fc860c496b36a5f56ca187edf31143e48
Andreas Kupries <andreas.kupries@gmail.com>
2011-01-23 01:23:20 +0000

    * pop3.tcl: Extended package with STARTTLS support provided by 	* pop3.man: Pascal Scheffers. This switches a regular connection 	* pkgIndex.tcl: over to SSL/TLS. Version bumped to 1.8.

--- modules/pop3/pkgIndex.tcl
+++ modules/pop3/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded pop3 1.7 [list source [file join $dir pop3.tcl]]
+package ifneeded pop3 1.8 [list source [file join $dir pop3.tcl]]
