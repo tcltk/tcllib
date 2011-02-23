07475298b4c62cd6b996588f92295b8eaf5c4bd8
Andreas Kupries <andreas.kupries@gmail.com>
2011-02-23 22:11:59 +0000

    * mpformats/fmt.nroff: Moved the handling of the list nesting 	* mpformats/_nroff.tcl: level a bit around to be consistent at 	* checker.tcl: both begin and end.  Fixed the long-standing 	  indentation bug where the rendered text got unindented after a 	  nested list or example. We simply had to start indented 	  paragraphs, via .IP after them, and for each paragraph in a list 	  element. This needed the consistent nesting level to correctly 	  know when to emit the commands. And while this may generate 	  empty paragraphs, these can be removed easily in the 	  post-processing. 	* doctools.tcl: Bumped version to 1.4.13. 	* doctools.man: 	* pkgIndex.tcl:

--- modules/doctools/pkgIndex.tcl
+++ modules/doctools/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded doctools            1.4.12 [list source [file join $dir doctools.tcl]]
+package ifneeded doctools            1.4.13 [list source [file join $dir doctools.tcl]]
 package ifneeded doctools::toc       1.1.3  [list source [file join $dir doctoc.tcl]]
 package ifneeded doctools::idx       1.0.4  [list source [file join $dir docidx.tcl]]
 package ifneeded doctools::cvs       1      [list source [file join $dir cvs.tcl]]
