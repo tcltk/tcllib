b9bcdf833d6897124710ce4d4ece45dda48fc82e
Andreas Kupries <akupries@shaw.ca>
2011-05-31 16:07:35 +0000

    * cat.man: New base channels. Concatenation of channels, 	* cat.tcl: standard channel combining stdin and stdout, 	* facade.man: and a facade for wrapping around other 	* facade.tcl: channels. 	* std.man: 	* std.tcl: 	* pkgIndex.tcl:

--- modules/virtchannel_base/pkgIndex.tcl
+++ modules/virtchannel_base/pkgIndex.tcl
@@ -1,5 +1,7 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
 
+package ifneeded tcl::chan::cat 1          [list source [file join $dir cat.tcl]]
+package ifneeded tcl::chan::facade 1       [list source [file join $dir facade.tcl]]
 package ifneeded tcl::chan::fifo 1         [list source [file join $dir fifo.tcl]]
 package ifneeded tcl::chan::fifo2 1        [list source [file join $dir fifo2.tcl]]
 package ifneeded tcl::chan::halfpipe 1     [list source [file join $dir halfpipe.tcl]]
@@ -7,6 +9,7 @@ package ifneeded tcl::chan::memchan 1.0.1  [list source [file join $dir memchan.
 package ifneeded tcl::chan::null 1         [list source [file join $dir null.tcl]]
 package ifneeded tcl::chan::nullzero 1     [list source [file join $dir nullzero.tcl]]
 package ifneeded tcl::chan::random 1       [list source [file join $dir random.tcl]]
+package ifneeded tcl::chan::std 1          [list source [file join $dir std.tcl]]
 package ifneeded tcl::chan::string 1.0.1   [list source [file join $dir string.tcl]]
 package ifneeded tcl::chan::textwindow 1   [list source [file join $dir textwindow.tcl]]
 package ifneeded tcl::chan::variable 1.0.1 [list source [file join $dir variable.tcl]]
