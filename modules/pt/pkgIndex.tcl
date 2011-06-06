2f1c62968d2db6a72c2f8d10860f7e5272717273
Andreas Kupries <akupries@shaw.ca>
2011-06-06 17:27:18 +0000

    * pt_tclparam_config_tcloo.tcl: Fixed typo, OO is TclOO. 	* pkgIndex.tcl: Bumped version to 1.0.2.

--- modules/pt/pkgIndex.tcl
+++ modules/pt/pkgIndex.tcl
@@ -54,7 +54,7 @@ package ifneeded pt::peg::container::peg 1 [list source [file join $dir pt_peg_c
 # */PARAM support (canned configurations).
 package ifneeded pt::cparam::configuration::critcl  1.0.1 [list source [file join $dir pt_cparam_config_critcl.tcl]]
 package ifneeded pt::tclparam::configuration::snit  1.0.1 [list source [file join $dir pt_tclparam_config_snit.tcl]]
-package ifneeded pt::tclparam::configuration::tcloo 1.0.1 [list source [file join $dir pt_tclparam_config_tcloo.tcl]]
+package ifneeded pt::tclparam::configuration::tcloo 1.0.2 [list source [file join $dir pt_tclparam_config_tcloo.tcl]]
 
 # Parser generator core.
 package ifneeded pt::pgen 1.0.1 [list source [file join $dir pt_pgen.tcl]]
