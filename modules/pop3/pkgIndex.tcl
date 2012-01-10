6a0aa6e9801b0c5bc2a7f62da185d04b0452891f
Andreas Kupries <andreas.kupries@gmail.com>
2012-01-10 20:02:22 +0000

    * pop3.tcl: [Bug 3471474]: Fixed bug where the socketcmd was not 	* pop3.man: treated as cmdprefix as documented, but as command 	* pkgIndex.tcl: name. Dropped supported for 8.2 and moved forward 	  to require Tcl 8.4. Keep the 8.4-ism of 'eq'. Bumped the version 	  to 1.9.

--- modules/pop3/pkgIndex.tcl
+++ modules/pop3/pkgIndex.tcl
@@ -1,2 +1,2 @@
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded pop3 1.8 [list source [file join $dir pop3.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.4]} {return}
+package ifneeded pop3 1.9 [list source [file join $dir pop3.tcl]]
