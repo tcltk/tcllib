a1f7cbcc8a3b8ecc94b9f7d8e61c9feb565d1d17
Andreas Kupries <andreas.kupries@gmail.com>
2012-06-06 17:31:26 +0000

    * iban.tcl: [Patch 3532308]: Applied patch by Max Jarek to 	* iban.test: update the code to the IBAN Registry version 35. 	* iban.man: Bumped package version to 1.1. 	* pkgIndex.tcl:

--- modules/valtype/iban.tcl
+++ modules/valtype/iban.tcl
@@ -33,7 +33,7 @@ snit::type ::valtype::iban {
 	FR 27 GF 27 GP 27 MQ 27 RE 27 PF 27 TF 27 YT 27 NC 27 PM 27 WF 27 GE 22 DE 22 GI 23 GR 27 HU 28
 	IS 26 IE 22 IL 23 IT 27 KZ 20 KW 30 LV 21 LB 28 LI 21 LT 20 LU 20 MK 19 MT 31 MR 27 MU 30 MC 27
 	ME 22 NL 18 NO 15 PL 28 PT 25 RO 24 SM 27 SA 24 RS 22 SK 24 SI 19 ES 24 SE 24 CH 21 TN 24 TR 26
-	AE 23 GB 22
+	AE 23 GB 22 AZ 28 MD 24 PK 24 VG 24 GT 28
     }
 
     typevariable charmap {
@@ -88,4 +88,4 @@ snit::type ::valtype::iban {
 # # ## ### ##### ######## ############# ######################
 ## Ready
 
-package provide valtype::iban 1
+package provide valtype::iban 1.1
