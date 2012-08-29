57e571509d81f6700a48d749edaad20d110d9951
Andreas Kupries <akupries@shaw.ca>
2012-08-29 20:42:19 +0000

    * traverse.tcl (Init): Fixed a bug where a symlink to the 	* traverse.man: base directory was not handled correctly. 	* traverse.test: It got expanded and should not have been. 	* pkgIndex.tcl: Cause was missing initialization of the _known set 	* find.setup: with the normalized base path. Added test for this. 	  Bumped version to 0.4.3.

--- modules/fileutil/pkgIndex.tcl
+++ modules/fileutil/pkgIndex.tcl
@@ -2,7 +2,7 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded fileutil 1.14.4 [list source [file join $dir fileutil.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.3]} {return}
-package ifneeded fileutil::traverse 0.4.2 [list source [file join $dir traverse.tcl]]
+package ifneeded fileutil::traverse 0.4.3 [list source [file join $dir traverse.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded fileutil::multi     0.1   [list source [file join $dir multi.tcl]]
