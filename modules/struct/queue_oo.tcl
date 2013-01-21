a8688d7f04c7de5ca131162c658277b4cc5a8eb0
Andreas Kupries <akupries@shaw.ca>
2013-01-21 20:51:37 +0000

    Rewritten, simplified. The 8.5 requisite means that we have extended requirement syntax allowing us to jump the major version barrier without fuss.

--- modules/struct/queue_oo.tcl
+++ modules/struct/queue_oo.tcl
@@ -10,9 +10,8 @@
 # 
 # RCS: @(#) $Id: queue_oo.tcl,v 1.2 2010/09/10 17:31:04 andreas_kupries Exp $
 
-if {[catch {package require TclOO 0.6.1}]} {
-    package require TclOO 1
-}
+package require Tcl   8.5
+package require TclOO 0.6.1- ; # This includes 1 and higher.
 
 # Cleanup first
 catch {namespace delete ::struct::queue::queue_oo}
