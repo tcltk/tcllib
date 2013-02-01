24ceddea8f3ab4451dea3edfd5d76fa11e4130cd
Andreas Kupries <akupries@shaw.ca>
2013-02-01 08:10:32 +0000

    json, writing: Dropping the quoting of / as \/.

--- modules/json/pkgIndex.tcl
+++ modules/json/pkgIndex.tcl
@@ -4,4 +4,4 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded json 1.1.2 [list source [file join $dir json.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded json::write 1.0.1 [list source [file join $dir json_write.tcl]]
+package ifneeded json::write 1.0.2 [list source [file join $dir json_write.tcl]]
