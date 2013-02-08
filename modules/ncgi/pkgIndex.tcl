1c6b8c88a18b9e2bc61138a05a52fc311362c9ef
Andreas Kupries <akupries@shaw.ca>
2013-02-08 19:14:54 +0000

    (::ncgi::DecodeHex): [Bug 3603593]: Fixed bad scoping of DecodeHex, now in the ncgi namespace instead of polluting the global. Bumped version to 1.4.2.

--- modules/ncgi/pkgIndex.tcl
+++ modules/ncgi/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded ncgi 1.4.1 [list source [file join $dir ncgi.tcl]]
+package ifneeded ncgi 1.4.2 [list source [file join $dir ncgi.tcl]]
