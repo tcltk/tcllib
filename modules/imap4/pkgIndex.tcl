4a332fdac4777beb13d13f2bb8f789e39628edc6
Andreas Kupries <akupries@shaw.ca>
2013-02-12 21:24:50 +0000

    [Bug 3604129] Applied contribution by Magnatune. Additional command (copy). Bumped version to 0.5.

--- modules/imap4/pkgIndex.tcl
+++ modules/imap4/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded imap4 0.4 [list source [file join $dir imap4.tcl]]
+package ifneeded imap4 0.5 [list source [file join $dir imap4.tcl]]
