c526619d533fd336df2a47a045addf7f9006e049
Andreas Kupries <akupries@shaw.ca>
2013-02-15 07:31:06 +0000

    Lots of bugfixes, completed testsuite. First functional state. Still missing documentation.

--- modules/string/token.tcl
+++ modules/string/token.tcl
@@ -10,13 +10,21 @@ package require fileutil ;# cat
 # # ## ### ##### ######## ############# #####################
 ## API setup
 #
-## NOTE: We are here placing the 'token' ensemble command into the
-##       Tcl core's builtin 'string' ensemble.
 
 namespace eval ::string::token {
-    namespace export text file
+    namespace export chomp file text
     namespace ensemble create
 }
+
+## NOTE: We are placing the 'token' ensemble command into the Tcl
+##       core's builtin 'string' ensemble.
+
+apply {{} {
+    set map [namespace ensemble configure ::string -map]
+    dict set map token ::string::token
+    namespace ensemble configure ::string -map $map
+    return
+}}
 
 # # ## ### ##### ######## ############# #####################
 ## API
@@ -34,22 +42,31 @@ proc ::string::token::text {map text} {
     #   token = list(label start-index end-index)
 
     set start  0
-    set result 0
-    while {[Chomp $map start $text result]} continue
+    set result {}
+    while {[chomp $map start $text result]} continue
     return $result
 }
 
 # # ## ### ##### ######## ############# #####################
 ## Internal, helpers.
 
-proc ::string::token::Chomp {map sv text rv} {
+proc ::string::token::chomp {map sv text rv} {
     upvar 1 $sv start $rv result
+
+    # Stop on empty or eof.
+    if {$text eq {}} {return 0}
+    if {$start >= [string length $text]} {return 0}
+
+    #puts |$start||[string range $text $start end]||$result|
 
     foreach {pattern label} $map {
 	if {![regexp -start $start -indices -- \\A($pattern) $text -> range]} continue
 
 	lappend result [list $label {*}$range]
 	lassign $range a e
+
+	#puts MATCH|$pattern|[string range $text $a $e]|
+
 	set start $e
 	incr start
 	return 1
