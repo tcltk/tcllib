32cd88549428b692b6d51ffb3db22feb5152ff20
Andreas Kupries <akupries@shaw.ca>
2013-02-22 23:54:12 +0000

    Moved dtplite to version 1.0.5 with the split. Updated its documentation. Added missing ChangeLog. Regenerated embedded.

--- modules/dtplite/pkgIndex.tcl
+++ modules/dtplite/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded dtplite 1.0.4 [list source [file join $dir dtplite.tcl]]
+package ifneeded dtplite 1.0.5 [list source [file join $dir dtplite.tcl]]
