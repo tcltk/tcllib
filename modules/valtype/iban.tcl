8764145bb5475e5933c1ba0428b0a09a15f16b33
Andreas Kupries <akupries@shaw.ca>
2013-02-27 06:36:39 +0000

    [Patch 3606105]: Applied patch by Max Jarek to update the code to the IBAN Registry version 43.  Bumped package version to 1.2.

--- modules/valtype/iban.tcl
+++ modules/valtype/iban.tcl
@@ -29,10 +29,10 @@ snit::type ::valtype::iban {
     # Type Methods
 
     typevariable cclen -array {
-	AL 28 AD 24 AT 20 BH 22 BE 16 BA 20 BG 22 CR 21 HR 21 CY 28 CZ 24 DK 18 FO 18 GL 18 DO 28 EE 20 FI 18
+	AL 28 AD 24 AT 20 BH 22 BE 16 BA 20 BG 22 BR 29 CR 21 HR 21 CY 28 CZ 24 DK 18 FO 18 GL 18 DO 28 EE 20 FI 18
 	FR 27 GF 27 GP 27 MQ 27 RE 27 PF 27 TF 27 YT 27 NC 27 PM 27 WF 27 GE 22 DE 22 GI 23 GR 27 HU 28
 	IS 26 IE 22 IL 23 IT 27 KZ 20 KW 30 LV 21 LB 28 LI 21 LT 20 LU 20 MK 19 MT 31 MR 27 MU 30 MC 27
-	ME 22 NL 18 NO 15 PL 28 PT 25 RO 24 SM 27 SA 24 RS 22 SK 24 SI 19 ES 24 SE 24 CH 21 TN 24 TR 26
+	ME 22 NL 18 NO 15 PL 28 PS 29 PT 25 RO 24 SM 27 SA 24 RS 22 SK 24 SI 19 ES 24 SE 24 CH 21 TN 24 TR 26
 	AE 23 GB 22 AZ 28 MD 24 PK 24 VG 24 GT 28
     }
 
@@ -88,4 +88,4 @@ snit::type ::valtype::iban {
 # # ## ### ##### ######## ############# ######################
 ## Ready
 
-package provide valtype::iban 1.1
+package provide valtype::iban 1.2
