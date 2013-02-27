8a4558b49d602ff1833b1d97f20e505f916072e0
Andreas Kupries <andreas.kupries@gmail.com>
2013-02-27 20:00:57 +0000

    (::oo::Helpers::link): New helper command.  Makes instance methods available without 'my'. Bumped version to 1.2.

--- modules/ooutil/pkgIndex.tcl
+++ modules/ooutil/pkgIndex.tcl
@@ -4,4 +4,4 @@ if {![package vsatisfies [package provide Tcl] 8.5]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded oo::util 1.1 [list source [file join $dir ooutil.tcl]]
+package ifneeded oo::util 1.2 [list source [file join $dir ooutil.tcl]]
