1adac9caef022d46b3332a564047ec432a751d19
Andreas Kupries <akupries@shaw.ca>
2013-03-13 00:18:05 +0000

    Fixed package index, forgot to update for string::token::shell

--- modules/string/pkgIndex.tcl
+++ modules/string/pkgIndex.tcl
@@ -2,5 +2,5 @@ if {![package vsatisfies [package provide Tcl] 8.5]} {
     # FRINK: nocheck
     return
 }
-package ifneeded string::token        1 [list source [file join $dir token.tcl]]
-package ifneeded string::token::shell 1 [list source [file join $dir token_shell.tcl]]
+package ifneeded string::token        1   [list source [file join $dir token.tcl]]
+package ifneeded string::token::shell 1.1 [list source [file join $dir token_shell.tcl]]
