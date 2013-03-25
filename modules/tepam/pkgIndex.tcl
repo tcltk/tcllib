da809c3db553254fb1ce20626af4b62428b673f6
Andreas Drollinger <Andreas Drollinger>
2013-03-25 22:12:02 +0000

    Update to TEPAM 0.4.1: * Correction of bug 3608952: Help text is incorrectly generated if procedures are part   of the non-default namespaces * Correction of bug 3608951: Help text shows incorrect argument order if procedure   is defined in the "unnamed arguments first, named arguments later" calling style.

--- modules/tepam/pkgIndex.tcl
+++ modules/tepam/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.3]} {return}
-package ifneeded tepam   0.4.0 [list source [file join $dir tepam.tcl]]
+package ifneeded tepam   0.4.1 [list source [file join $dir tepam.tcl]]
