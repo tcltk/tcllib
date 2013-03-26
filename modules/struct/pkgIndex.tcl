108a984609b578319e23f34577b5ff03d974798a
Andreas Kupries <akupries@shaw.ca>
2013-03-26 17:08:39 +0000

    Fixed package index, was missing struct::queue version bump causing mismatch.

--- modules/struct/pkgIndex.tcl
+++ modules/struct/pkgIndex.tcl
@@ -2,7 +2,7 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded struct            2.1   [list source [file join $dir struct.tcl]]
 package ifneeded struct            1.4   [list source [file join $dir struct1.tcl]]
 
-package ifneeded struct::queue     1.4.4 [list source [file join $dir queue.tcl]]
+package ifneeded struct::queue     1.4.5 [list source [file join $dir queue.tcl]]
 package ifneeded struct::stack     1.5.3 [list source [file join $dir stack.tcl]]
 package ifneeded struct::tree      2.1.2 [list source [file join $dir tree.tcl]]
 package ifneeded struct::matrix    2.0.2 [list source [file join $dir matrix.tcl]]
