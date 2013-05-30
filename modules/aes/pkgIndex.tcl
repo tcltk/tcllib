707ff14492666f2b749bebc277b14df1e9d61722
Andreas Kupries <akupries@shaw.ca>
2013-05-30 21:22:01 +0000

    [Bug 3612645][Allura 1366]: Fix handling of last block read, it may be empty. In that case we must not pad, nor try to decrypt it into garbage. Extended testsuite with cases for this. Version bumped to 1.1.1.

--- modules/aes/pkgIndex.tcl
+++ modules/aes/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded aes 1.1 [list source [file join $dir aes.tcl]]
+package ifneeded aes 1.1.1 [list source [file join $dir aes.tcl]]
