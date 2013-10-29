9d2b8aeda1bbca1c823fba3fbf1952118062cc2d
Andreas Kupries <akupries@shaw.ca>
2013-10-29 07:03:09 +0000

    Ticket [1d2b62d10d]: Fixed unwanted double-close of channel when accessing a non-existing thumbnail in a file. Introduced by the refactoring. Bumped version to 0.5.

--- modules/jpeg/pkgIndex.tcl
+++ modules/jpeg/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
-package ifneeded jpeg 0.4.0 [list source [file join $dir jpeg.tcl]]
+package ifneeded jpeg 0.5 [list source [file join $dir jpeg.tcl]]
