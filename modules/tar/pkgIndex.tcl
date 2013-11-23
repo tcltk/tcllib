2243686f546f108394e93f1616cec7ef45d874d7
Andreas Kupries <akupries@shaw.ca>
2013-11-23 00:14:44 +0000

    Bumped package version, updated logs, ready to merge. Fixed ticket [6b7aa0aecc].

--- modules/tar/pkgIndex.tcl
+++ modules/tar/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded tar 0.8 [list source [file join $dir tar.tcl]]
+package ifneeded tar 0.9 [list source [file join $dir tar.tcl]]
