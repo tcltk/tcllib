7d57ea44de849e57fe447bb2feb974eca6db8378
Andreas Kupries <andreas.kupries@gmail.com>
2013-12-06 20:14:40 +0000

    Ticket [b8f35b9883]. Added references for SCRAM mechanism and package. Fixed missing cleanup of test variable.  New package SASL::SCRAM and testsuite, provided by <sgolovan@nes.ru>.

--- modules/sasl/pkgIndex.tcl
+++ modules/sasl/pkgIndex.tcl
@@ -8,3 +8,4 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
 package ifneeded SASL 1.3.2 [list source [file join $dir sasl.tcl]]
 package ifneeded SASL::NTLM 1.1.1 [list source [file join $dir ntlm.tcl]]
 package ifneeded SASL::XGoogleToken 1.0.1 [list source [file join $dir gtoken.tcl]]
+package ifneeded SASL::SCRAM 0.1 [list source [file join $dir scram.tcl]]
