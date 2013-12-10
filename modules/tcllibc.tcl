8d61d483a760cbc34f453eba0e3109807de1bfdd
Andreas Kupries <akupries@shaw.ca>
2013-12-10 00:50:27 +0000

    Initial work on a critcl json parser based on json-c. Issues: many-json2dict has no C equivalent, nor the *2dict commands. Boolean is 0/1 vs true/false in C vs. Tcl, this should be fixable in the testsuite. Basics are working.

--- modules/tcllibc.tcl
+++ modules/tcllibc.tcl
@@ -2,7 +2,7 @@
 # found in Tcllib in one shared library.
 
 package require critcl
-package provide tcllibc 0.3.10
+package provide tcllibc 0.3.11
 
 namespace eval ::tcllib {
     variable tcllibc_rcsid {$Id: tcllibc.tcl,v 1.13 2010/05/25 19:26:17 andreas_kupries Exp $}
