92c9e7e9521313ff80c7e8b64e69361adfc7352c
Andreas Kupries <akupries@shaw.ca>
2014-01-21 17:56:15 +0000

    Fixes to various testsuites and code, in preparation for 1.16 release. Release Candidate Branch opened now.

--- modules/grammar_fa/fa.tcl
+++ modules/grammar_fa/fa.tcl
@@ -13,8 +13,21 @@
 # ### ### ### ######### ######### #########
 ## Requisites
 
+package require Tcl 8.4
+if {[package vcompare [package present Tcl] 8.5] >= 0} {
+    # Tcl 8.5+, extended package version numbers.
+    # Require 1.3 and beyond, regardless of major version number.
+    package require snit 1.3- ; # OO system in use (Using hierarchical methods)
+} else {
+    # Tcl 8.4, emulate, ask for 2.x first, then 1.3+.
+    if {[catch {
+	package require snit 2   ; # OO system in use (Using hierarchical methods)
+    }]} {
+	package require snit 1.3 ; # OO system in use (Using hierarchical methods)
+    }
+}
+
 package require grammar::fa::op ; # Heavy FA operations.
-package require snit 1.3-       ; # OO system in use (Using hierarchical methods)
 package require struct::list    ; # Extended list operations.
 package require struct::set     ; # Extended set operations.
 
