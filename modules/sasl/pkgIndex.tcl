aa7d292472465857bbba01b12121deacc25d7b50
Andreas Kupries <akupries@shaw.ca>
2014-01-21 17:28:38 +0000

    Ticket [861f53ff24]. Added proper initialization to CreateNonce when falling back to md5 (no dev/urandom). Bumped version to 1.3.3.

--- modules/sasl/pkgIndex.tcl
+++ modules/sasl/pkgIndex.tcl
@@ -5,7 +5,7 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded SASL 1.3.2 [list source [file join $dir sasl.tcl]]
-package ifneeded SASL::NTLM 1.1.2 [list source [file join $dir ntlm.tcl]]
+package ifneeded SASL               1.3.3 [list source [file join $dir sasl.tcl]]
+package ifneeded SASL::NTLM         1.1.2 [list source [file join $dir ntlm.tcl]]
 package ifneeded SASL::XGoogleToken 1.0.1 [list source [file join $dir gtoken.tcl]]
-package ifneeded SASL::SCRAM 0.1 [list source [file join $dir scram.tcl]]
+package ifneeded SASL::SCRAM        0.1   [list source [file join $dir scram.tcl]]
