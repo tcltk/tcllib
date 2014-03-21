202f36a4a47dbee7f7ded75cce6b3dae5a85b763
Andreas Kupries <akupries@shaw.ca>
2014-03-21 19:09:02 +0000

    debug - Added command "hexl" for generation of hex-dumps. Bumped to version 1.0.5.

--- modules/debug/pkgIndex.tcl
+++ modules/debug/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package require Tcl] 8.5]} return
-package ifneeded debug            1.0.4 [list source [file join $dir debug.tcl]]
+package ifneeded debug            1.0.5 [list source [file join $dir debug.tcl]]
 package ifneeded debug::heartbeat 1     [list source [file join $dir heartbeat.tcl]]
 package ifneeded debug::timestamp 1     [list source [file join $dir timestamp.tcl]]
 package ifneeded debug::caller    1     [list source [file join $dir caller.tcl]]
