a66e6ffbaeb7586122f215c377005d7a457752d5
Andreas Kupries <akupries@shaw.ca>
2014-05-12 18:24:43 +0000

    Fix version mismatch code/index in commit [82431192f8], bump to 0.6.1

--- modules/zip/pkgIndex.tcl
+++ modules/zip/pkgIndex.tcl
@@ -1,3 +1,3 @@
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded zipfile::encode 0.3 [list source [file join $dir encode.tcl]]
-package ifneeded zipfile::decode 0.5 [list source [file join $dir decode.tcl]]
+package ifneeded zipfile::decode 0.6.1 [list source [file join $dir decode.tcl]]
