2a1d4f791b0315c5c974b850f92b1fc48cbaafe2
Andreas Kupries <andreas.kupries@gmail.com>
2014-05-16 17:30:38 +0000

    comm - Fixed version mismatch introduced by commit [6303673b03]. Bumped to 4.6.3.1

--- modules/comm/pkgIndex.tcl
+++ modules/comm/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.3]} {return}
-package ifneeded comm 4.6.3 [list source [file join $dir comm.tcl]]
+package ifneeded comm 4.6.3.1 [list source [file join $dir comm.tcl]]
