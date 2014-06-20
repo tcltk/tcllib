526d9a3298232030072b401ffa54171bfe1cc0fa
Andreas Kupries <akupries@shaw.ca>
2014-06-20 22:00:11 +0000

    json :: [3d91afbcf2] Fixed the C implementation. It now properly rejects irregular escape sequences. Also tweaked it to not show the plain 'syntax eror' from the parser if a more detailed/nicer message from the lexer is available. Updated the expected messages in the testsuite.

--- modules/json/tests/support.tcl
+++ modules/json/tests/support.tcl
@@ -101,27 +101,27 @@ foreach f [TestFilesGlob tests/*.err] {
 
 set FAIL(escape1)        {"\%"}
 set  ERR(escape1-tcl)    {unexpected token "END" at position 0; expecting VALUE}
-set  ERR(escape1-critcl) {syntax error 0 bytes before end, around ``%''}
+set  ERR(escape1-critcl) {bad escape 3 bytes before end, around ``%''}
 
 set FAIL(escape2)        {"\."}
 set  ERR(escape2-tcl)    {unexpected token "END" at position 0; expecting VALUE}
-set  ERR(escape2-critcl) {syntax error 0 bytes before end, around ``.''}
+set  ERR(escape2-critcl) {bad escape 3 bytes before end, around ``.''}
 
 set FAIL(escape3)        {["\%"]}
 set  ERR(escape3-tcl)    {unexpected token "END" at position 0; expecting VALUE}
-set  ERR(escape3-critcl) {syntax error 0 bytes before end, around ``%''}
+set  ERR(escape3-critcl) {bad escape 4 bytes before end, around ``%''}
 
 set FAIL(escape4)        {["\."]}
 set  ERR(escape4-tcl)    {unexpected token "END" at position 0; expecting VALUE}
-set  ERR(escape4-critcl) {syntax error 0 bytes before end, around ``.''}
+set  ERR(escape4-critcl) {bad escape 4 bytes before end, around ``.''}
 
 set FAIL(escape5)        {{"a":"\%"}}
 set  ERR(escape5-tcl)    "unexpected token \"}\" at position 4; expecting VALUE"
-set  ERR(escape5-critcl) {syntax error 0 bytes before end, around ``%''}
+set  ERR(escape5-critcl) {bad escape 4 bytes before end, around ``%''}
 
 set FAIL(escape6)        {{"a":"\."}}
 set  ERR(escape6-tcl)    "unexpected token \"}\" at position 4; expecting VALUE"
-set  ERR(escape6-critcl) {syntax error 0 bytes before end, around ``.''}
+set  ERR(escape6-critcl) {bad escape 4 bytes before end, around ``.''}
 
 
 
