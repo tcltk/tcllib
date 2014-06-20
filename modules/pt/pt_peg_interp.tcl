2193ef19c10bbca99509ae86c7e51f36110781bf
Andreas Kupries <andreas.kupries@gmail.com>
2014-06-20 00:02:27 +0000

    Collective update of the version numbers in all the modified packages. Lone exception is pt::parse::peg which uses generated code.
    Action item: Fix the generator packages to allow the specification of
    version information, then update pt::parse::peg as well.

--- modules/pt/pt_peg_interp.tcl
+++ modules/pt/pt_peg_interp.tcl
@@ -1,6 +1,6 @@
 # -*- tcl -*-
 #
-# Copyright (c) 2009 by Andreas Kupries <andreas_kupries@users.sourceforge.net>
+# Copyright (c) 2009-2014 by Andreas Kupries <andreas_kupries@users.sourceforge.net>
 
 # Interpreter for parsing expression grammars. In essence a recursive
 # descent parser configurable with a parsing expression grammar.
@@ -382,4 +382,4 @@ snit::type ::pt::peg::interp {
 # ### ### ### ######### ######### #########
 ## Package Management
 
-package provide pt::peg::interp 1
+package provide pt::peg::interp 1.0.1
