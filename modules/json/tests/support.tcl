7db580926d7aabdeead6ffbd2f23ffade044a588
Andreas Kupries <andreas.kupries@gmail.com>
2014-06-23 21:46:00 +0000

    json :: [3d91afbcf2]. Fixed handling of illegal escape sequences in both Tcl and C implementations of the parser. Version bumped to 1.3.3.

--- modules/json/tests/support.tcl
+++ modules/json/tests/support.tcl
@@ -100,27 +100,27 @@ foreach f [TestFilesGlob tests/*.err] {
 ## C   uses Tcl_UtfBacklash, and allows lots of irregular escapes.
 
 set FAIL(escape1)        {"\%"}
-set  ERR(escape1-tcl)    {unexpected token "END" at position 0; expecting VALUE}
+set  ERR(escape1-tcl)    {unexpected token ""\%"" at position 0; expecting STRING}
 set  ERR(escape1-critcl) {bad escape 3 bytes before end, around ``%''}
 
 set FAIL(escape2)        {"\."}
-set  ERR(escape2-tcl)    {unexpected token "END" at position 0; expecting VALUE}
+set  ERR(escape2-tcl)    {unexpected token ""\."" at position 0; expecting STRING}
 set  ERR(escape2-critcl) {bad escape 3 bytes before end, around ``.''}
 
 set FAIL(escape3)        {["\%"]}
-set  ERR(escape3-tcl)    {unexpected token "END" at position 0; expecting VALUE}
+set  ERR(escape3-tcl)    {unexpected token ""\%"" at position 1; expecting STRING}
 set  ERR(escape3-critcl) {bad escape 4 bytes before end, around ``%''}
 
 set FAIL(escape4)        {["\."]}
-set  ERR(escape4-tcl)    {unexpected token "END" at position 0; expecting VALUE}
+set  ERR(escape4-tcl)    {unexpected token ""\."" at position 1; expecting STRING}
 set  ERR(escape4-critcl) {bad escape 4 bytes before end, around ``.''}
 
 set FAIL(escape5)        {{"a":"\%"}}
-set  ERR(escape5-tcl)    "unexpected token \"}\" at position 4; expecting VALUE"
+set  ERR(escape5-tcl)    {unexpected token ""\%"" at position 3; expecting STRING}
 set  ERR(escape5-critcl) {bad escape 4 bytes before end, around ``%''}
 
 set FAIL(escape6)        {{"a":"\."}}
-set  ERR(escape6-tcl)    "unexpected token \"}\" at position 4; expecting VALUE"
+set  ERR(escape6-tcl)    {unexpected token ""\."" at position 3; expecting STRING}
 set  ERR(escape6-critcl) {bad escape 4 bytes before end, around ``.''}
 
 
