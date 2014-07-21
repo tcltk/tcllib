f50c68678bf0b470bee9e3978b8bb8c2eb3aa6ff
Andreas Kupries <andreas.kupries@gmail.com>
2014-07-21 19:57:28 +0000

    Ticket [3612465] :: inifile - Allow and ignore leading whitespace for all lines of a .ini file. Extended testsuite. Version bumped to 0.2.6.

--- modules/inifile/pkgIndex.tcl
+++ modules/inifile/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if { ![package vsatisfies [package provide Tcl] 8.2] } { return }
-package ifneeded inifile 0.2.5 [list source [file join $dir ini.tcl]]
+package ifneeded inifile 0.2.6 [list source [file join $dir ini.tcl]]
