dfcc212bb751ef84a5c4afc075b0558d78cf25d0
Andreas Kupries <akupries@shaw.ca>
2014-08-25 18:48:22 +0000

    string::token::shell - Added option "--". Prevents bad interpretation of input string starting with a dash as option. Version bumped to 1.2.

--- modules/string/pkgIndex.tcl
+++ modules/string/pkgIndex.tcl
@@ -3,4 +3,4 @@ if {![package vsatisfies [package provide Tcl] 8.5]} {
     return
 }
 package ifneeded string::token        1   [list source [file join $dir token.tcl]]
-package ifneeded string::token::shell 1.1 [list source [file join $dir token_shell.tcl]]
+package ifneeded string::token::shell 1.2 [list source [file join $dir token_shell.tcl]]
