57fcb136a434a259dea0d6c06cb43dd9e10dc5f3
Andreas Kupries <akupries@shaw.ca>
2015-04-15 20:59:43 +0000

    ip - Ticket [510c9fce1b] - Added distance and nextIp commands provided by Martin Heinrich. Extended testsuite, docs. Version bumped to 1.3.

--- modules/dns/pkgIndex.tcl
+++ modules/dns/pkgIndex.tcl
@@ -5,5 +5,5 @@
 if {![package vsatisfies [package provide Tcl] 8.2]} {return}
 package ifneeded dns    1.3.5 [list source [file join $dir dns.tcl]]
 package ifneeded resolv 1.0.3 [list source [file join $dir resolv.tcl]]
-package ifneeded ip     1.2.2 [list source [file join $dir ip.tcl]]
+package ifneeded ip     1.3   [list source [file join $dir ip.tcl]]
 package ifneeded spf    1.1.1 [list source [file join $dir spf.tcl]]
