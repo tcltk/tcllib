336870a335d9d605bcb7e19edffba13c646d0014
Andreas Kupries <andreas.kupries@gmail.com>
2015-05-26 20:52:31 +0000

    Fixed more trailing whitespace, and indentation (use 4 spaces per level). Fixed two missing closing brackets in huddle::compile (bool type). Updated package index to note requirement for 8.5+ for all packages.

--- modules/yaml/pkgIndex.tcl
+++ modules/yaml/pkgIndex.tcl
@@ -1,7 +1,6 @@
 
-package ifneeded yaml         0.3.8 [list source [file join $dir yaml.tcl]]
-package ifneeded huddle       0.1.7 [list source [file join $dir huddle.tcl]]
-
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
 
+package ifneeded yaml         0.3.8 [list source [file join $dir yaml.tcl]]
+package ifneeded huddle       0.1.7 [list source [file join $dir huddle.tcl]]
 package ifneeded huddle::json 0.1   [list source [file join $dir json2huddle.tcl]]
