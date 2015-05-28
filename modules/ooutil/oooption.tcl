6e7c42138b1b4960182adc918ab2b0c645c82a9d
Sean Woods <yoda@etoyoc.com>
2015-05-28 15:35:54 +0000

    Split the oo::property package into oo::meta and oo::option
    oo::meta now gracefully handles redefining the properties of a class.
    
    The "property" method is now "meta"
    
    The oo::meta::property method is now oo::meta::info
    
    The meta property for objects now combines the local "config" dict with
    the class meta dict to answer questions about the entire corpus. (Eliminating the need
    for configure/cget unless you truely do want to mimic Tk)

---
+++ modules/ooutil/oooption.tcl
@@ -0,0 +1,167 @@
+###
+# Option handling for TclOO
+###
+package require oo::meta
+
+oo::define oo::object {
+  
+  ###
+  # topic: 3c4893b65a1c79b2549b9ee88f23c9e3
+  # description:
+  #    Provide a default value for all options and
+  #    publically declared variables, and locks the
+  #    pipeline mutex to prevent signal processing
+  #    while the contructor is still running.
+  #    Note, by default an odie object will ignore
+  #    signals until a later call to <i>my lock remove pipeline</i>
+  ###
+  method InitializePublic {} {
+    my variable config
+    if {![info exists config]} {
+      set config {}
+    }
+    set dat [my meta getnull option]
+    foreach {var info} $dat {
+      if {[dict exists $info set-command]} {
+        if {[catch {my cget $var} value]} {
+          dict set config $var [my cget $var default]
+        } else {
+          if { $value eq {} } {
+            dict set config $var [my cget $var default]
+          }
+        }
+      }
+      if {![dict exists $config $var]} {
+        dict set config $var [my cget $var default]
+      }
+    }
+    foreach {var info} [my meta getnull variable] {
+      if { $var eq "config" } continue
+      my variable $var
+      if {![info exists $var]} {
+        if {[dict exists $info default]} {
+          set $var [dict get $info default]
+        } else {
+          set $var {}
+        }
+      }
+    }
+    foreach {var info} [my meta getnull array] {
+      if { $var eq "config" } continue
+      my variable $var
+      if {![info exists $var]} {
+        if {[dict exists $info default]} {
+          array set $var [dict get $info default]
+        } else {
+          array set $var {}
+        }
+      }
+    }
+  }
+
+  ###
+  # topic: 86a1b968cea8d439df87585afdbdaadb
+  ###
+  method cget {field {default {}}} {
+    my variable config
+    set field [string trimleft $field -]
+    set dat [my meta getnull option]
+  
+    if {[my meta is true options_strict] && ![dict exists $dat $field]} {
+      error "Invalid option -$field. Valid: [dict keys $dat]"
+    }
+    set info [dict getnull $dat $field]    
+    if {$default eq "default"} {
+      set getcmd [dict getnull $info default-command]
+      if {$getcmd ne {}} {
+        return [{*}[string map [list %field% $field %self% [namespace which my]] $getcmd]]
+      } else {
+        return [dict getnull $info default]
+      }
+    }
+    if {[dict exists $dat $field]} {
+      set getcmd [dict getnull $info get-command]
+      if {$getcmd ne {}} {
+        return [{*}[string map [list %field% $field %self% [namespace which my]] $getcmd]]
+      }
+      if {![dict exists $config $field]} {
+        set getcmd [dict getnull $info default-command]
+        if {$getcmd ne {}} {
+          dict set config $field [{*}[string map [list %field% $field %self% [namespace which my]] $getcmd]]
+        } else {
+          dict set config $field [dict getnull $info default]
+        }
+      }
+      if {$default eq "varname"} {
+        set varname [my varname visconfig]
+        set ${varname}($field) [dict get $config $field]
+        return "${varname}($field)"
+      }
+      return [dict get $config $field]
+    }
+    if {[dict exists $config $field]} {
+      return [dict get $config $field]
+    }
+    return [my meta get $field]
+  }
+  
+  ###
+  # topic: 73e2566466b836cc4535f1a437c391b0
+  ###
+  method configure args {
+    # Will be removed at the end of "configurelist_triggers"
+    set dictargs [::oo::meta::args_to_options {*}$args]
+    if {[llength $dictargs] == 1} {
+      return [my cget [lindex $dictargs 0]]
+    }
+    my configurelist $dictargs
+    my configurelist_triggers $dictargs
+  }
+
+  ###
+  # topic: dc9fba12ec23a3ad000c66aea17135a5
+  ###
+  method configurelist dictargs {
+    my variable config
+    set dat [my meta getnull option]
+    if {[my meta is true options_strict]} {
+      foreach {field val} $dictargs {
+        if {![dict exists $dat $field]} {
+          error "Invalid option $field. Valid: [dict keys $dat]"
+        }
+      }
+    }
+    ###
+    # Validate all inputs
+    ###
+    foreach {field val} $dictargs {
+      set script [dict getnull $dat $field validate-command]
+      if {$script ne {}} {
+        {*}[string map [list %field% [list $field] %value% [list $val] %self% [namespace which my]] $script]
+      }
+    }
+    ###
+    # Apply all inputs with special rules
+    ###
+    foreach {field val} $dictargs {
+      dict set config $field $val
+    }
+  }
+
+  ###
+  # topic: 543c936485189593f0b9ed79b5d5f2c0
+  ###
+  method configurelist_triggers dictargs {
+    set dat [my meta getnull option]
+    ###
+    # Apply all inputs with special rules
+    ###
+    foreach {field val} $dictargs {
+      set script [dict getnull $dat $field set-command]
+      if {$script ne {}} {
+        {*}[string map [list %field% [list $field] %value% [list $val] %self% [namespace which my]] $script]
+      }
+    }
+  }
+}
+package provide oo::option 0.1
