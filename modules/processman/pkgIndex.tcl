f878d4405da642fe85ad08269681b4367034fc52
Sean Woods <yoda@etoyoc.com>
2015-06-08 14:45:40 +0000

    Added a backward compatible alias to odie::processman from processman.tcl

--- modules/processman/pkgIndex.tcl
+++ modules/processman/pkgIndex.tcl
@@ -8,4 +8,5 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
+package ifneeded odie::processman 0.3 [list source [file join $dir processman.tcl]]
 package ifneeded processman 0.3 [list source [file join $dir processman.tcl]]
