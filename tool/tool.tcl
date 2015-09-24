01b1f17adc5b39e6bc06ff1022a275e5800e3fd3
Sean Woods <yoda@etoyoc.com>
2015-09-24 19:29:51 +0000

    Moved the tool.shed manifest to the new customary spot.
    Added a makefile directive to rebuild the makefile

--- tool/tool.tcl
+++ tool/tool.tcl
@@ -1,147 +1,37 @@
-set SCRIPT [file normalize [info script]]
-set TOOL_ROOT [file dirname [file dirname $SCRIPT]]
-set cwd [file dirname $SCRIPT]
-if {[catch {package require shed}]} {
-  puts "ADDING TOOL TO PKG PATH"
-  lappend auto_path [file join $cwd .. .. shed modules]
-  package require tool
-  package require shed
-}
-
-tool begin tcllib
 ###
 # Build basic description of this tool
 ###
-tool description: {
-The Tcl Standard Library
-}
-tool distribution: fossil
-tool class: sak
 
 ###
 # List of mirrors
 ###
-tool mirror http://core.tcl.tk/tcllib 
-tool mirror http://fossil.etoyoc.com/fossil/tcllib
-
+#tool mirror http://core.tcl.tk/tcllib 
+#tool mirror http://fossil.etoyoc.com/fossil/tcllib
+my variable tool_root
 ###
 # Populate the branches
 ###
-tool branch trunk {
-  version: 1.18b
-  release: beta
+my release add trunk {
+  distribution: official
+  checkout: trunk
 }
-tool branch tcllib-1-17 {
-  version: 1.17
-  release: final
-}
-tool branch tcllib-1-16 {
-  version: 1.16
-  release: final
-}
-tool branch tcllib-1-15 {
-  version: 1.15
-  release: final
-}
-tool branch tcllib-1-14 {
-  version: 1.14
-  release: final
-}
-tool branch tcllib-1-13 {
-  version: 1.13
-  release: final
-}
-tool branch tcllib-1-12 {
-  version: 1.12
-  release: final
-}
-tool branch tcllib-1-11-1 {
-  version: 1.11.1
-  release: final
-}
-tool branch tcllib-1-11 {
-  version: 1.11
-  release: final
-}
-tool branch tcllib-1-10 {
-  version: 1.10
-  release: final
-}
-tool branch tcllib-1-9 {
-  version: 1.9
-  release: final
-}
-tool branch tcllib-1-8 {
-  version: 1.8
-  release: final
-}
-tool branch tcllib-1-7 {
-  version: 1.7
-  release: final
-}
-tool branch tcllib-1-6-1 {
-  version: 1.6.1
-  release: final
-}
-tool branch tcllib-1-6 {
-  version: 1.6
-  release: final
-}
-tool branch tcllib-1-4 {
-  version: 1.4
-  release: final
-}
-tool branch tcllib-1-3 {
-  version: 1.3
-  release: final
-}
-tool branch tcllib-1-2-0 {
-  version: 1.2.0
-  release: final
-}
-tool branch tcllib-1-2 {
-  version: 1.2
-  release: final
-}
-tool branch tcllib-1-1 {
-  version: 1.1
-  release: final
-}
-tool branch tcllib-1-0 {
-  version: 1.0
-  release: final
-}
-tool branch tcllib-0-8 {
-  version: 0.8
-  release: final
-}
-tool branch tcllib-0-6-1 {
-  version: 0.6.1
-  release: final
-}
-tool branch tcllib-0-6 {
-  version: 0.6
-  release: final
-}
-tool branch tcllib-0-5 {
-  version: 0.5
-  release: final
-}
-tool branch tcllib-0-4 {
-  version: 0.4
-  release: final
+foreach release {
+  1.17 1.16 1.15 1.14 1.13 1.12 1.11.1 1.11 1.10
+  1.9 1.8 1.7 1.6.1 1.4 1.3 1.2.0 1.2 1.1 1.0
+  0.8 0.6.1 0.6 0.5 0.4
+} {
+  set checkout tcllib-[join [split $release .] -]
+  my release add $checkout [list version: $release checkout: $checkout distribution: official]
 }
 
-
-foreach file [glob [file join $TOOL_ROOT apps *]] {
+foreach file [glob [file join $::TOOL_ROOT apps *]] {
   if {[file extension $file] ne {}} continue
-  application_scan $file
+  my application scan $file
 }
 
 ###
 # Build the module section
 ###
-foreach path [glob [file join $TOOL_ROOT modules *]] {
-  tool module_path [file tail $path] $path
+foreach path [glob [file join $::TOOL_ROOT modules *]] {
+  my module scan [file tail $path] $path
 }
-tool write [file join [file dirname $SCRIPT] tool.shed]
