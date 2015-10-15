aa0d373d838d3417ac95a4af874552b94185b298
Sean Woods <yoda@etoyoc.com>
2015-10-15 15:06:11 +0000

    Updates to the shed spec, bug fixes for dicttool, Added a new class of object "organelle" to the TOOL

--- modules/tool/organ.tcl
+++ modules/tool/organ.tcl
@@ -40,3 +40,33 @@ oo::define oo::object {
     return [dict getnull $organs $stub]
   }
 }
+
+###
+# A special class of objects that
+# stores no meta data of its own
+# Instead it vampires off of the master object
+###
+oo::class create ::tool::organelle {
+
+  constructor {master} {
+    my entangle $master
+    set final_class [my select]
+    if {[info commands $final_class] ne {}} {
+      # Safe to switch class here, we haven't initialized anything
+      oo::objdefine [self] class $final_class
+    }
+    my initialize
+  }
+
+  method entangle {master} {
+    my graft master $master
+    my forward meta $master meta
+    foreach {stub organ} [$master organ] {
+      my graft $stub $organ
+    }
+  }
+  
+  method select {} {
+    return {}
+  }
+}
