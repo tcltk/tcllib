4f3a3b5759a82eb17e5c4e5787507e4c0a84f37f
Andreas Kupries <andreas.kupries@gmail.com>
2015-11-28 05:27:58 +0000

    tie - Fixed command quoting issue in tie::rarray aka remotearray breaking the transmission of values containing spaces :(
    Bumped version to 1.0.1. Extended testsuite. Updated docs.
    
    With thanks to Mike Collins <mocallins@yahoo.com> for alerting me to
    the problem.

--- modules/tie/pkgIndex.tcl
+++ modules/tie/pkgIndex.tcl
@@ -4,6 +4,6 @@ package ifneeded tie::std::file     1.0.4 [list source [file join $dir tie_file.
 package ifneeded tie::std::growfile 1.0   [list source [file join $dir tie_growfile.tcl]]
 package ifneeded tie::std::log      1.0   [list source [file join $dir tie_log.tcl]]
 package ifneeded tie::std::array    1.0   [list source [file join $dir tie_array.tcl]]
-package ifneeded tie::std::rarray   1.0   [list source [file join $dir tie_rarray.tcl]]
+package ifneeded tie::std::rarray   1.0.1 [list source [file join $dir tie_rarray.tcl]]
 package ifneeded tie::std::dsource  1.0   [list source [file join $dir tie_dsource.tcl]]
 
