f83ca90cad2861c693aa374774972541d7e70af0
Sean Woods <yoda@etoyoc.com>
2015-12-03 00:48:17 +0000

    httpd module:
    * Fixed the test suite for httpd
    * Added tests for the httpd module
    * httpd now utilizes non-blocking I/O for bulk transfers
    * Brought the SCGI application module up to date with API changes
    * Added a validate IP check
    * Condensed form handling into the base httpd::reply class
    * Combined the code from scgi-server.tcl into the scgi.test file
    
    oometa:
    Removed the non-functioning oometaclass package
    
    oodialect:
    * The XXX::define procedure will now generate a class if one was
    not already present. This is needed for projects like IRM which
    may need to reload entire libraries of code (without destroying the 
    objects implementing them)
    
    tool:
    * Fixed notification handling
    * Created a notification on object destruction
    * Added support for method ensembles
    * Added support for inheritable class methods
    * Added a "package require sha1" to the main TOOL bootloader

--- modules/httpd/scgi-app.tcl
+++ modules/httpd/scgi-app.tcl
@@ -13,64 +13,27 @@ namespace eval ::scgi {}
 tool::class create ::scgi::reply {  
   superclass ::httpd::reply
   
-  property socket buffersize   32768
-  property socket blocking     0
-  property socket translation  {binary binary}
-
-  method RequestRead {} {    
-    my variable chan
-    my variable data
-    my variable inbuffer
-    set rawdata [read $chan]
-    append inbuffer $rawdata
-    if {[eof $chan]} {
-      my destroy
-      return
-    }
-    if {$data(state) == "start"} {
-      set colonIdx [string first : $inbuffer]
-      if {$colonIdx == -1} {
-        # we don't have the headers length yet
-        return
-      } else {
-        set length [string range $inbuffer 0 $colonIdx-1]
-        set inbuffer [string range $inbuffer $colonIdx+1 end]
-        set data(state) headers
-        set data(length) $length
-      }
-    }
-    if {$data(state) == "headers" } {
-      if {[string length $inbuffer] < $data(length)+1} {
-        # we don't have the complete headers yet, wait for more
-        return
-      }
-      set headers [string range $inbuffer 0 $data(length)-1]
-      set headers [lrange [split $headers \0] 0 end-1]
-      my variable query_body
-      set inbuffer [string range $inbuffer $data(length)+1 end]
-      set data(content_length) [dict get $headers CONTENT_LENGTH]
-      my query_headers merge $headers
-      set data(state) body
-    }
-    
-    if {[string length $inbuffer] < $data(content_length)} {
-      return
-    }
-    my variable query_body
-    set query_body $inbuffer
-
-    # Dispatch to the URL implementation.
-    if [catch {
-      set code [my <server> dispatch [self]]
-      if {$code eq 200} {
-        my content
-      }
-    } err] {
+  ###
+  # A modified dispatch method from a standard HTTP reply
+  # Unlike in HTTP, our headers were spoon fed to use from
+  # the server
+  ###
+  method dispatch {newsock datastate} {
+    my query_headers replace $datastate
+    my variable chan rawrequest dipatched_time
+    set chan $newsock
+    chan event $chan readable {}
+    chan configure $chan -translation {auto crlf} -buffering line
+    set dispatched_time [clock seconds]
+    try {
+      # Dispatch to the URL implementation.
+      my content
+    } on error {err info} {
+      puts stderr $::errorInfo
       my error 500 $err
-    } else {
+    } finally {
       my output
     }
-    return
   }
   
   method EncodeStatus {status} {
@@ -81,8 +44,92 @@ tool::class create ::scgi::reply {
 tool::class create scgi::app {
   superclass ::httpd::server
 
+  property socket buffersize   32768
+  property socket blocking     0
+  property socket translation  {binary binary}
+  
   property reply_class ::scgi::reply
   
+  method connect {sock ip port} {
+    ###
+    # If an IP address is blocked
+    # send a "go to hell" message
+    ###
+    if {[my validation Blocked_IP $sock $ip]} {
+      catch {close $sock}
+      return
+    }
+    set query {
+      REQUEST_URI {NOT_POPULATED}
+    }
+    try {
+      chan configure $sock \
+        -blocking 1 \
+        -translation {binary binary} \
+        -buffersize 4096 \
+        -buffering none
+    
+      # Read the SCGI request on byte at a time until we reach a ":"
+      set size {}
+      while 1 {
+        set char [read $sock 1]
+        if {[chan eof $sock]} {
+          catch {close $sock}
+          return
+        }
+        if {$char eq ":"} break
+        append size $char
+      }
+      # With length in hand, read the netstring encoded headers
+      set inbuffer [read $sock [expr $size+1]]
+      chan configure $sock -blocking 0 -buffersize 4096 -buffering full
+      set query [lrange [split [string range $inbuffer 0 end-1] \0] 0 end-1]
+      set reply [my dispatch $query]
+      dict with query {}
+      if {[llength $reply]} {
+        if {[dict exists $reply class]} {
+          set class [dict get $reply class]          
+        } else {
+          set class [my cget reply_class]
+        }  
+        set pageobj [$class create [namespace current]::reply::[::tool::uuid_short] [self]]
+        if {[dict exists $reply mixin]} {
+          oo::objdefine $pageobj mixin [dict get $reply mixin]
+        }
+        $pageobj dispatch $sock $reply
+        my log HttpAccess $REQUEST_URI
+      } else {
+        try {
+          my log HttpMissing $REQUEST_URI
+          puts $sock "Status: 404 NOT FOUND"
+          dict with query {}
+          set body [subst [my template notfound]]
+          puts $sock "Content-length: [string length $body]"
+          puts $sock
+          puts $sock $body
+        } on error {err errdat} {
+          puts stderr "FAILED ON 404: $err"
+        } finally {
+          catch {close $sock}
+        }
+      }
+    } on error {err errdat} {
+      try {
+        puts stderr $::errorInfo
+        puts $sock "Status: 505 INTERNAL ERROR"
+        dict with query {}
+        set body [subst [my template internal_error]]
+        puts $sock "Content-length: [string length $body]"
+        puts $sock
+        puts $sock $body
+        my log HttpError $REQUEST_URI
+      } on error {err errdat} {
+        puts stderr "FAILED ON 505: $err $::errorInfo"
+      } finally {
+        catch {close $sock}
+      }
+    }
+  }
 }
 
 package provide scgi::app 0.1
---
+++ modules/tool/procs.tcl
@@ -0,0 +1,4 @@
+###
+# Fundimental utilities for TOOL that need to be loaded before we create
+# our metaclass
+###
