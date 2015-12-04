ef284f4a76fa5124a6d1ef2d8001e5e77cf50619
Sean Woods <yoda@etoyoc.com>
2015-12-04 18:59:11 +0000

    Rearanged the code in the TOOL module. No functional changes. Started work on a .man file for TOOL

--- modules/tool/metaclass.tcl
+++ modules/tool/metaclass.tcl
@@ -11,12 +11,15 @@
 #-------------------------------------------------------------------------
 
 namespace eval ::tool {}
-namespace eval ::tool::define {}
 
+###
+# New OO Keywords for TOOL
+###
+namespace eval ::tool::define {}
 proc ::tool::define::array {name {values {}}} {
   set class [current_class]
   set name [string trimright $name :]:
-  if {![::oo::meta::info $class exists $array $name]} {
+  if {![::oo::meta::info $class exists array $name]} {
     ::oo::meta::info $class set array $name {}
   }
   foreach {var val} $values {
@@ -54,6 +57,15 @@ my lock remove constructor
 }
 
 ###
+# topic: 7a5c7e04989704eef117ff3c9dd88823
+# title: Specify the a method for the class object itself, instead of for objects of the class
+###
+proc ::tool::define::class_method {name arglist body} {
+  set class [current_class]
+  ::oo::meta::info $class set class_typemethod $name: [list $arglist $body]
+}
+
+###
 # topic: 4cb3696bf06d1e372107795de7fe1545
 # title: Specify the destructor for a class
 ###
@@ -65,17 +77,194 @@ proc ::tool::define::destructor rawbody {
   ::oo::define [current_class] destructor $body
 }
 
-proc ::tool::define::option {name branchinfo} {
+###
+# topic: 8bcae430f1eda4ccdb96daedeeea3bd409c6bb7a
+# description: Add properties and option handling
+###
+proc ::tool::define::property args {
   set class [current_class]
-  # NEXT, save the option data
-  ::oo::meta::info $class branchset option $name $branchinfo
+  switch [llength $args] {
+    2 {
+      set type const
+      set property [string trimleft [lindex $args 0] :]
+      set value [lindex $args 1]
+      ::oo::meta::info $class set $type $property: $value
+      return
+    }
+    3 {
+      set type     [lindex $args 0]
+      set property [string trimleft [lindex $args 1] :]
+      set value    [lindex $args 2]
+      ::oo::meta::info $class set $type $property: $value
+      return
+    }
+    default {
+      error "Usage:
+property name type valuedict
+OR property name value"
+    }
+  }
+  ::oo::meta::info $class set {*}$args
 }
 
+###
+# topic: 615b7c43b863b0d8d1f9107a8d126b21
+# title: Specify a variable which should be initialized in the constructor
+# description:
+#    This keyword can also be expressed:
+#    [example {property variable NAME {default DEFAULT}}]
+#    [para]
+#    Variables registered in the variable property are also initialized
+#    (if missing) when the object changes class via the [emph morph] method.
+###
 proc ::tool::define::variable {name {default {}}} {
   set class [current_class]
   set name [string trimright $name :]
   ::oo::meta::info $class set variable $name: $default
   ::oo::define $class variable $name
+}
+
+###
+# Utility Procedures
+###
+
+# topic: 643efabec4303b20b66b760a1ad279bf
+###
+proc ::tool::args_to_dict args {
+  if {[llength $args]==1} {
+    return [lindex $args 0]
+  }
+  return $args
+}
+
+###
+# topic: b40970b0d9a2525990b9105ec8c96d3d
+###
+proc ::tool::args_to_options args {
+  set result {}
+  foreach {var val} [args_to_dict {*}$args] {
+    lappend result [string trimleft $var -] $val
+  }
+  return $result
+}
+
+###
+# topic: a92cd258900010f656f4c6e7dbffae57
+###
+proc ::tool::dynamic_methods class {
+  set metadata [::oo::meta::metadata $class]
+  foreach command [info commands ::tool::dynamic_methods_*] {
+    $command $class $metadata
+  }
+}
+
+###
+# topic: 4969d897a83d91a230a17f166dbcaede
+###
+proc ::tool::dynamic_arguments {arglist args} {
+  set idx 0
+  set len [llength $args]
+  if {$len > [llength $arglist]} {
+    ###
+    # Catch if the user supplies too many arguments
+    ###
+    set dargs 0
+    if {[lindex $arglist end] ni {args dictargs}} {
+      set string [dynamic_wrongargs_message $arglist]
+      error $string
+    }
+  }
+  foreach argdef $arglist {
+    if {$argdef eq "args"} {
+      ###
+      # Perform args processing in the style of tcl
+      ###
+      uplevel 1 [list set args [lrange $args $idx end]]
+      break
+    }
+    if {$argdef eq "dictargs"} {
+      ###
+      # Perform args processing in the style of tcl
+      ###
+      uplevel 1 [list set args [lrange $args $idx end]]
+      ###
+      # Perform args processing in the style of tool
+      ###
+      set dictargs [::tool::args_to_options {*}[lrange $args $idx end]]
+      uplevel 1 [list set dictargs $dictargs]
+      break
+    }
+    if {$idx > $len} {
+      ###
+      # Catch if the user supplies too few arguments
+      ###
+      if {[llength $argdef]==1} {
+        set string [dynamic_wrongargs_message $arglist]
+        error $string
+      } else {
+        uplevel 1 [list set [lindex $argdef 0] [lindex $argdef 1]]
+      }
+    } else {
+      uplevel 1 [list set [lindex $argdef 0] [lindex $args $idx]]
+    }
+    incr idx
+  }
+}
+
+###
+# topic: b88add196bb63abccc44639db5e5eae1
+###
+proc ::tool::dynamic_methods_class {thisclass metadata} {
+  foreach {method info} [dict getnull $metadata class_typemethod] {
+    lassign $info arglist body
+    set method [string trimright $method :]
+    ::oo::objdefine $thisclass method $method $arglist $body
+  }
+}
+
+###
+# topic: 53ab28ac5c6ee601fe1fe07b073be88e
+###
+proc ::tool::dynamic_wrongargs_message arglist {
+  set result "Wrong # args: should be:"
+  set dargs 0
+  foreach argdef $arglist {
+    if {$argdef in {args dictargs}} {
+      set dargs 1
+      break
+    }
+    if {[llength $argdef]==1} {
+      append result " $argdef"
+    } else {
+      append result " ?[lindex $argdef 0]?"
+    }
+  }
+  if { $dargs } {
+    append result " ?option value?..."
+  }
+  return $result
+}
+
+proc ::tool::object_create objname {
+  foreach varname {
+    object_info
+    object_subscribe
+  } {
+    variable $varname
+    set ${varname}($objname) {}
+  }
+  set object_info($objname) [list class [info object class $objname]]
+}
+
+proc ::tool::object_destroy objname {
+  ::tool::event::generate $objname object_destroy [list objname $objname]
+  foreach varname {
+    object_info
+    object_subscribe
+  } {
+    variable $varname
+    unset ${varname}($objname)
+  }
 }
 
 #-------------------------------------------------------------------------
@@ -97,92 +286,124 @@ proc ::tool::define::variable {name {default {}}} {
   
   destructor {}
   
-  method configure {args} {
-    my variable options
-    switch [llength $args] {
-      0 {
-        return [array get options]
-      }
-      1 {
-        set field [lindex $args 0]
-        if {![my meta exists option $field]} {
-          error "Invalid option $field. Valid: [my meta keys option]"
-        }
-        return $options($field)
-      }
-      default {
-        my configurelist $args
-      }
-    }
-  }
-  
-  method cget {option} {
-    my variable options
-    if {![my meta exists option $option]} {
-      error "Invalid option $option. Valid: [my meta keys option]"
-    }
-    return $options($option)
-  }
-
-  # Called after all options and public variables are initialized
-  method initialize {} {}
-  
-  method option_info args {
-    set option_info [my meta getnull option]
-    switch [llength $args] {
-      0 { return $option_info }
-      1 {
-        set field [lindex $args 0]
-        if {$field eq "list" } { 
-          return [dict keys $option_info]
-        }
-        if {![dict exists $option_info $field]} {
-          error "Invalid option $field. Valid [dict keys $option_info]"
-        }
-        return [dict get $option_info $field]
-      }
-      default {
-        return [dict get $option_info {*}$args]
-      }
-    }
-  }
-
-  method configurelist values {
-    my variable options
-    set option_info [my meta getnull option]
-    # Run all validation checks
-    foreach {field value} $values {
-      if {[dict exists $option_info $field validate-command]} {
-        if {[catch [string map [list %self% [self] %field% $field %value% $value] [dict get $option_info $field validate-command]] res opts]} {
-          return {*}$opts $res
-        }
-      }
-    }
-    # Ensure all options are valid
-    foreach {field value} $values {
-      if {![dict exists $option_info $field]} {
-        error "Bad option $field. Valid: [dict keys $option_info]"
-      }
-    }
-    # Set the values and apply them
-    foreach {field value} $values {
-      if {[dict exists $option_info $field map-command]} {
-        set options($field) [eval [string map [list %self% [self] %field% $field %value% $value] [dict get $option_info $field map-command]]]
-      } else {
-        set options($field) $value
-      }
-      if {[dict exists $option_info $field set-command]} {
-        eval [string map [list %self% [self] %field% $field %value% $value] [dict get $option_info $field set-command]]
-      }
-    }
-  }
-  
   method ancestors {{reverse 0}} {
     set result [::oo::meta::ancestors [info object class [self]]]
     if {$reverse} {
       return [lreverse $result]
     }
     return $result
+  }
+  
+  ###
+  # title: Forward a method
+  ###
+  method forward {method args} {
+    oo::objdefine [self] forward $method {*}$args
+  }
+  
+  ###
+  # title: Direct a series of sub-functions to a seperate object
+  ###
+  method graft args {
+    my variable organs
+    if {[llength $args] == 1} {
+      error "Need two arguments"
+    }
+    set object {}
+    foreach {stub object} $args {
+      dict set organs $stub $object
+      oo::objdefine [self] forward <${stub}> $object
+      oo::objdefine [self] export <${stub}>
+    }
+    return $object
+  }
+  
+  # Called after all options and public variables are initialized
+  method initialize {} {}
+  
+  ###
+  # topic: 3c4893b65a1c79b2549b9ee88f23c9e3
+  # description:
+  #    Provide a default value for all options and
+  #    publically declared variables, and locks the
+  #    pipeline mutex to prevent signal processing
+  #    while the contructor is still running.
+  #    Note, by default an odie object will ignore
+  #    signals until a later call to <i>my lock remove pipeline</i>
+  ###
+  method InitializePublic {} {
+    my variable config meta
+    if {![info exists meta]} {
+      set meta {}
+    }
+    foreach {var value} [my meta branchget variable] {
+      if { $var in "meta" } continue
+      my variable $var
+      if {![info exists $var]} {
+        set $var $value
+      }
+    }
+    foreach {var value} [my meta branchget array] {
+      if { $var eq "meta" } continue
+      my variable $var
+      foreach {f v} $value {
+        if {![array exists ${var}($f)]} {
+          set ${var}($f) $v
+        }
+      }
+    }
+  }
+  
+  method morph newclass {
+    if {$newclass eq {}} return
+    set class [string trimleft [info object class [self]]]
+    set newclass [string trimleft $newclass :]
+    if {[info command ::$newclass] eq {}} {
+      error "Class $newclass does not exist"
+    }
+    if { $class ne $newclass } {
+      my Morph_leave
+      oo::objdefine [self] class ::${newclass}
+      my InitializePublic
+      my Morph_enter
+    }
+  }
+
+  ###
+  # Commands to perform as this object transitions out of the present class
+  ###
+  method Morph_leave {} {}
+  ###
+  # Commands to perform as this object transitions into this class as a new class
+  ###
+  method Morph_enter {} {}
+  
+  ###
+  # title: List which objects are forwarded as organs
+  ###
+  method organ {{stub all}} {
+    my variable organs
+    if {![info exists organs]} {
+      return {}
+    }
+    if { $stub eq "all" } {
+      return $organs
+    }
+    return [dict getnull $organs $stub]
+  }
+  
+  method property args {
+    if {[my meta exists {*}$args]} {
+      return [my meta get {*}$args]
+    }
+    set field [string trimright [lindex $args end] :]:
+    if {[my meta exists {*}[lrange $args 0 end-1] $field]} {
+      return [my meta get {*}[lrange $args 0 end-1] $field]
+    }
+    if {[my meta exists const {*}[lrange $args 0 end-1] $field]} {
+      return [my meta get const {*}[lrange $args 0 end-1] $field]
+    }
+    return {}
   }
 }
 
