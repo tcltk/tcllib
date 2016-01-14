37aa5b24be36b89d857bcb54ddbf262a2503c177
Andreas Kupries <akupries@shaw.ca>
2016-01-14 06:01:31 +0000

    uuid - Bumped min required Tcl to 8.5 (expand operator used for nettool integration).

--- modules/uuid/pkgIndex.tcl
+++ modules/uuid/pkgIndex.tcl
@@ -4,5 +4,5 @@
 #
 # $Id: pkgIndex.tcl,v 1.3 2012/11/19 19:28:24 andreas_kupries Exp $
 
-if {![package vsatisfies [package provide Tcl] 8.2]} {return}
+if {![package vsatisfies [package provide Tcl] 8.5]} {return}
 package ifneeded uuid 1.0.5 [list source [file join $dir uuid.tcl]]
