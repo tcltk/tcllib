2031ad01da8e19adabd476bc49e95b289a31f884
tne <tne>
2016-06-09 20:12:28 +0000

    Revamped the tool::main command to prevent nested invocations and to prevent a vwait or update in an idle coroutine from derailing the entire event system.

--- modules/tool/pipeline.tcl
+++ modules/tool/pipeline.tcl
@@ -33,6 +33,8 @@ proc ::tool::do_events {} {
   # Process coroutines
   variable all_coroutines
   variable coroutine_object
+  variable last_event
+  set last_event [clock seconds]
   set count 0
   foreach coro $all_coroutines {
     if {[info command $coro] eq {}} {
@@ -76,8 +78,38 @@ proc ::tool::do_events {} {
   return $count
 }
 
+proc ::tool::Main_Service {} {
+  # Cap cron delays at 1 minute
+  if {[info command ::CRON] eq {}} {
+    coroutine ::CRON ::cron::runTasksCoro
+  }
+  set cron_delay [::CRON]
+  set tool_running [::tool::do_events]
+  if {$cron_delay==0 || $tool_running>0} {
+    incr ::tool::loops(active)
+    update
+    set ::tool::wake_up 1
+  } else {
+    incr ::tool::loops(idle)
+  }
+}
+
+
 proc ::tool::main {} {
   package require cron 1.2
+  variable event_loops
+  variable last_event
+  if {[info exists ::tool::main($event_loops)]} {
+    if {$::tool::main($event_loops)} {
+      set last_event -1
+      set ::tool::wake_up 1
+      update
+      if {$last_event>0} {
+        puts "Avoiding re-entrance into ::tool::main"
+        return
+      }
+    }
+  }
   ###
   # Have the cron::wake procedure wake up an idle loop instead
   # of it's normal run commands in the background
@@ -85,23 +117,26 @@ proc ::tool::main {} {
   proc ::cron::wake {} {
     set ::tool::wake_up 1
   }
+  # Signal for all other MAIN loops to terminate
+  for {set x 0} {$x < $event_loops} {incr x} {
+    set ::tool::main($x) 0
+  }
+  set ::tool::wake_up -1
+  update
+  set this [incr event_loops]
+  set ::tool::main($this) 1
+  set ::tool::wake_up 0
 
-  set ::forever 1
-  while {$::forever} {
+  while {$::tool::main($this)} {
+    set panic [after 120000 {puts "Warning: Tool event loop has not responded in 2 minutes"}]
     incr ::tool::loops(all)
-    if {[info command ::CRON] eq {}} {
-      coroutine ::CRON ::cron::runTasksCoro
-    }
-    set cron_delay [::CRON]
-    set tool_running [::tool::do_events]
-    if {$cron_delay==0 || $tool_running>0} {
-      incr ::tool::loops(active)
-      update
-    } else {
-      incr ::tool::loops(idle)
-      set ::tool::wake_up 0
-      after [expr {$cron_delay*1000}] {set ::tool::wake_up 1}
-      vwait ::tool::wake_up
+    after idle ::tool::Main_Service
+    set next [after [expr {60000}] {set ::tool::wake_up 1}]
+    vwait ::tool::wake_up
+    after cancel $panic
+    after cancel $next
+    if {$::tool::wake_up < 0} {
+      break
     }
   }
 }
@@ -164,6 +199,10 @@ proc ::tool::object_destroy objname {
 
 namespace eval ::tool {
   variable trace 0
+  variable event_loops
+  if {![info exists event_loops]} {
+    set event_loops 0
+  }
   variable all_coroutines
   if {![info exists all_coroutines]} {
     set all_coroutines {}
