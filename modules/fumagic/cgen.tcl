6cda24157afd3f03a8953b0eba243d8220a80fb2
Poor Yorick <pooryorick@users.sf.net>
2016-08-04 09:09:47 +0000

    fumagic: Fix versions and version requirements.

--- modules/fumagic/cgen.tcl
+++ modules/fumagic/cgen.tcl
@@ -25,11 +25,11 @@
 # ### ### ### ######### ######### #########
 ## Requirements
 
-package require Tcl 8.6
+package require Tcl 8.4
 package require fileutil::magic::rt ; # Runtime core, for Access to the typemap
 package require struct::list        ; # Our data structures.
 
-package provide fileutil::magic::cgen 1.2
+package provide fileutil::magic::cgen 1.2.0
 
 # ### ### ### ######### ######### #########
 ## Implementation
