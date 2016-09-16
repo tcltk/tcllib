e1490bd45e1bdaacf1f408fdb4300627cbc72001
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-09-16 21:38:33 +0000

    Name changes, comments cleaned up

--- modules/pt/pt_rdengine_nx.tcl
+++ modules/pt/pt_rdengine_nx.tcl
@@ -21,8 +21,8 @@ namespace eval ::pt::rde {
     ## a given TclOO class.
     ##
     
-    nx::Class create TclClass -superclass nx::Class {
-	:property clone:required
+    nx::Class create ClassFactory -superclass nx::Class {
+	:property prototype:required
 	:method init {args} {
 	    :alias instvar ::nsf::methods::object::instvar
 	    namespace eval [namespace qualifier [self]] {
@@ -30,17 +30,17 @@ namespace eval ::pt::rde {
 	    }
 
 	    ## clone constructor
-	    lassign [info class constructor ${:clone}] ctorParams ctorBody
+	    lassign [info class constructor ${:prototype}] ctorParams ctorBody
 	    :method init $ctorParams [:injectVars $ctorBody]
 	    ## clone all methods
-	    foreach m [info class methods ${:clone} -private] {
-		lassign [info class definition ${:clone} $m] params body
+	    foreach m [info class methods ${:prototype} -private] {
+		lassign [info class definition ${:prototype} $m] params body
 		:method $m $params [:injectVars $body]
 	    }
 	}
 	:method injectVars {body} {
 	    if {![info exists :vars]} {
-		set :vars [info class variables ${:clone}]
+		set :vars [info class variables ${:prototype}]
 	    }
 	    if {[llength ${:vars}]} {
 		append tmp [list :instvar {*}${:vars}] "\n" $body;
@@ -52,11 +52,14 @@ namespace eval ::pt::rde {
     }
 
     ##
-    ## ::pt::rde::nx: The NX derivative of ::pt::rde::oo, to be inherited
-    ## by the generated grammar class. Future content of pt_rdengine_oo.tcl?
+    ## ::pt::rde::nx:
+    ##
+    ## The NX derivative of ::pt::rde::oo, to be inherited
+    ## by the generated grammar class.
     ##
     
-    TclClass create nx -clone ::pt::rde::oo
+    ClassFactory create nx -prototype ::pt::rde::oo
+    
 }
 
 package provide pt::rde::nx [package req pt::rde::oo]
