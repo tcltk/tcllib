87947a1c9e926f15c3d66528222f4ab821ccac30
Sean Woods <yoda@etoyoc.com>
2016-12-13 18:35:38 +0000

    Added a standalone sleep event
    Added some procs needed for tao/tool backward compaditbility

--- modules/tool/pipeline.tcl
+++ modules/tool/pipeline.tcl
@@ -1,9 +1,27 @@
 ::namespace eval ::tool::signal {}
+::namespace eval ::tao {}
 
-# Provide a backward compadible hook
+# Provide a backward compatible hook
 proc ::tool::main {} {
   ::cron::main
 }
+
+proc ::tool::do_events {} {
+  ::cron::do_events
+}
+
+proc ::tool::main {} {
+  ::cron::main
+}
+
+proc ::tao::do_events {} {
+  ::cron::do_events
+}
+
+proc ::tao::main {} {
+  ::cron::main
+}
+
 
 package provide tool::pipeline 0.1
 
