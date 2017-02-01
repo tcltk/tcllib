423c4dd74034ca5b6b32791ce714bdd882e3dcb7
tne <tne>
2017-02-01 22:27:12 +0000

    Fixing a math error in units

--- modules/units/pkgIndex.tcl
+++ modules/units/pkgIndex.tcl
@@ -1,4 +1,4 @@
 # Tcl package index file, version 1.1
 
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded units 2.2 [list source [file join $dir units.tcl]]
+package ifneeded units 2.2.1 [list source [file join $dir units.tcl]]
