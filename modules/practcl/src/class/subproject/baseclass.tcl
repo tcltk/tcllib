9f7cf6d054efda92a68fe514326489d06f60e238
tne <tne>
2017-11-04 09:50:10 +0000

    Practcl: Reinstated the update method for projects

--- modules/practcl/src/class/subproject/baseclass.tcl
+++ modules/practcl/src/class/subproject/baseclass.tcl
@@ -97,6 +97,10 @@ oo::class create ::practcl::subproject {
 
   method sources {} {}
 
+  method update {} {
+    my ScmUpdate
+  }
+  
   method unpack {} {
     ::practcl::distribution select [self]
     my Unpack
