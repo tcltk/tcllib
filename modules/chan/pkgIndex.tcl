e7eae67b0fead5650a1f4ce97bb62ee77faf5081
Poor Yorick <pooryorick@users.sf.net>
2018-11-20 15:54:57 +0000

    new module: {chan coroutine}:  specializes channel commands to use the current coroutine for non-blocking I/O management.

--- modules/chan/pkgIndex.tcl
+++ modules/chan/pkgIndex.tcl
@@ -20,3 +20,13 @@ package ifneeded {chan base} 0.1 [list ::apply {dir {
     }
     package provide {chan base} 0.1
 }} $dir]
+
+
+package ifneeded {chan coroutine} 0.1 [list ::apply {dir {
+    package require ego
+    namespace eval ::tcllib::chan::coroutine [list ::source $dir/coroutine.tcl]
+    package provide {chan coroutine} 0.1
+    namespace eval ::tcllib::chan {
+	namespace export coroutine
+    }
+}} $dir]
