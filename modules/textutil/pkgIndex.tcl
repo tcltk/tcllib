ce210aee08a44c034e16d58aa1cc785dc03dceb7
Sean Woods <yoda@etoyoc.com>
2018-12-03 15:15:40 +0000

    Added wcswidth calculations to the textutil modules. This mechanism allows screen rendering agents to know how many columns to reserve for certain (predominately Eastern) characters.
    Per: https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms

--- modules/textutil/pkgIndex.tcl
+++ modules/textutil/pkgIndex.tcl
@@ -2,7 +2,7 @@ if {![package vsatisfies [package provide Tcl] 8.2]} {
     # FRINK: nocheck
     return
 }
-package ifneeded textutil           0.8   [list source [file join $dir textutil.tcl]]
+package ifneeded textutil           0.9   [list source [file join $dir textutil.tcl]]
 package ifneeded textutil::adjust   0.7.3 [list source [file join $dir adjust.tcl]]
 package ifneeded textutil::split    0.8   [list source [file join $dir split.tcl]]
 package ifneeded textutil::trim     0.7   [list source [file join $dir trim.tcl]]
@@ -10,3 +10,4 @@ package ifneeded textutil::tabify   0.7   [list source [file join $dir tabify.tc
 package ifneeded textutil::repeat   0.7   [list source [file join $dir repeat.tcl]]
 package ifneeded textutil::string   0.8   [list source [file join $dir string.tcl]]
 package ifneeded textutil::expander 1.3.1 [list source [file join $dir expander.tcl]]
+package ifneeded textutil::wcswidth 0.1   [list source [file join $dir wcswidth.tcl]]
