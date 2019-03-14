4f3a5be7e8ca63292b64f9d283bcaadd1a31f372
Andreas Kupries <andreas.kupries@gmail.com>
2019-03-14 07:27:54 +0000

    doctools:
    - `text`: Sliced and diced internals for easier (re)comprehension of
      the parts. Going towards reuse for a new `markdown` format.
    
      Fixed various mis-formattings:
    
      - Missing newline at end of output
    
      - Superflous newlines at beginning of example sections.
    
    - Updated various formatters to not pass initial leading whitespace
      into the output (See testsuite change, added comments)
    
    - `latex`: Fixed handling of paragraph breaks, [nl].
    
    - Extended testsuite, more manpage examples.
      Also added comments at top with short description.
    
    doctoc:
    
    - Syntax checker fix, items are allowed before toc end, after the last
      division.
    
    - `text`: Updated to make use of and match the reworked engine.
    
    - `html`: Fixed fixed mismanagement of items before first and after
      last division. Completely new state for tracking levels, items.
    
    - `nroff`: Fixed missing indenting of division title
    
    - `tmml`: Fixed variable name typo
    
    - Extended testsuite with examples for all the generators.
    
    docidx:
    
    - `nroff`: Fixed unsorted output
    
    - Extended testsuite with examples for all the generators.

---
+++ modules/doctools/mpformats/_text_utils.tcl
@@ -0,0 +1,56 @@
+# -*- tcl -*-
+#
+# Copyright (c) 2019 Andreas Kupries <andreas_kupries@sourceforge.net>
+# Freely redistributable.
+#
+# _text_utils.tcl -- Text formatting utilities
+
+# # ## ### ##### ########
+## API
+
+proc Compose {lb} {
+    upvar 1 $lb linebuffer
+    return [string trimright [join $linebuffer \n]]
+}
+
+proc ReHead {line prefix} {
+    set n [string length $prefix]
+    incr n -1
+    string replace $line 0 $n $prefix
+}
+
+proc MaxLen {v s} {
+    upvar 1 $v max
+    set n [string length $s]
+    if {$n <= $max} return
+    set max $n
+}
+
+proc BlankMargin {} { global lmarginIncrement ; Blank $lmarginIncrement }
+
+proc Repeat  {char n}      { textutil::repeat::strRepeat $char $n }
+proc Blank   {n}           { textutil::repeat::blank $n }
+proc RepeatM {char text}   { Repeat $char [string length $text] }
+proc BlankM  {text}        { Blank        [string length $text] }
+proc Undent  {text}        { textutil::adjust::undent $text }
+proc Reflow  {text maxlen} { textutil::adjust::adjust $text -length $maxlen }
+proc Indent  {text prefix} { textutil::adjust::indent $text $prefix }
+proc Indent1 {text p1 p}   { return "${p1}[textutil::adjust::indent $text $p 1]" }
+proc InFlow {text maxlen prefix1 prefix} {
+    # Reformats the paragraph `text` to keep line length under
+    # `maxlen` and then indents the result using `prefix1` and
+    # `prefix`.  `prefix1` is applied to the first line, and `prefix`
+    # to the remainder. The caller is responsible for ensuring that
+    # both prefixes have the same length.
+    Indent1 [Reflow $text $maxlen] $prefix1 $prefix
+}
+
+proc Provenance {} {
+    textutil::string::uncap [c_provenance]
+}
+
+# # ## ### ##### ########
+# Internals
+
+# # ## ### ##### ########
+return
