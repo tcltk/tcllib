c775d431e5d369896502f6fe03420c57602ab70f
Arjen Markus <arjen.markus@deltares.nl>
2019-10-29 20:00:11 +0000

    Correct the proc interp-linear for several corner cases (empty string as y-value, only integer values for x)

--- modules/math/pkgIndex.tcl
+++ modules/math/pkgIndex.tcl
@@ -12,7 +12,7 @@ package ifneeded math::roman             1.0   [list source [file join $dir roma
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded math::optimize          1.0.1 [list source [file join $dir optimize.tcl]]
-package ifneeded math::interpolate       1.1.1 [list source [file join $dir interpolate.tcl]]
+package ifneeded math::interpolate       1.1.2 [list source [file join $dir interpolate.tcl]]
 package ifneeded math::bignum            3.1.1 [list source [file join $dir bignum.tcl]]
 package ifneeded math::bigfloat          1.2.2 [list source [file join $dir bigfloat.tcl]]
 package ifneeded math::machineparameters 0.1   [list source [file join $dir machineparameters.tcl]]
