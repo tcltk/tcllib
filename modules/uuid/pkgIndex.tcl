735302630422c855b7149e95010d1b9dbb0a8dd5
Sean Woods <yoda@etoyoc.com>
2020-01-28 15:58:03 +0000

    Added a binary encode to the uuid's random string generator to avoid embedded nulls terminating strings prematurely

--- modules/uuid/pkgIndex.tcl
+++ modules/uuid/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded uuid 1.0.7 [list source [file join $dir uuid.tcl]]
+package ifneeded uuid 1.0.6 [list source [file join $dir uuid.tcl]]
