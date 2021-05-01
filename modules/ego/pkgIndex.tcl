97d22c0443b3a68522ff8d12a708cce66c6c4589
Poor Yorick <pooryorick@users.sf.net>
2021-05-01 13:04:24 +0000

    drop ego

--- modules/ego/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env tclsh
-
-if {![package vsatisfies [package provide Tcl] 8.6]} {return}
-
-package ifneeded ego 0.1 [list ::apply {dir {
-    namespace eval ::tcllib::ego [list ::source $dir/ego.tcl]
-    package provide ego 0.1
-}} $dir]
