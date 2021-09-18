235102d4e8c1d712dac0a26dae4f6c3f2d41068b
Andreas Kupries <andreas.kupries@gmail.com>
2021-09-18 09:04:00 +0000

    markdown - B - Ticket [5b8e3a9e9b] Fix mishandling of no empty line after atx header.
    Extended testsuite. Bumped to version 1.2.1.
    Fixed a single typo in the original patches.
    
    With thanks to Torsten Berg <berg@typoscriptics.de> for the patch to the report.

--- modules/markdown/pkgIndex.tcl
+++ modules/markdown/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded Markdown 1.2 [list source [file join $dir markdown.tcl]]
+package ifneeded Markdown 1.2.1 [list source [file join $dir markdown.tcl]]
