d71f062529340af23ee63665c33cbea4f4e7e244
Poor Yorick <pooryorick@users.sf.net>
2022-03-06 16:07:52 +0000

    Add a routine to load an accelerated version.

--- modules/aes/pkgIndex.tcl
+++ modules/aes/pkgIndex.tcl
@@ -2,4 +2,5 @@ if {![package vsatisfies [package provide Tcl] 8.5]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded aes 1.2.1 [list source [file join $dir aes.tcl]]
+package ifneeded aes 1.2.2 [list source [file join $dir aes.tcl]]
+package ifneeded aesc 0.0.1 [list source [file join $dir aesc.tcl]]
