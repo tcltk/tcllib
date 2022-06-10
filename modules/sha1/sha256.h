854065837b5aa7dee1e6b527109c89f4b8432b83
apnadkarni <apnmbx-wits@yahoo.com>
2022-06-10 12:35:11 +0000

    Fix for 935ac3b65b. Conflicting link definition for sha256 with MS C

--- modules/sha1/sha256.h
+++ modules/sha1/sha256.h
@@ -39,6 +39,35 @@
 #endif
 */
 # include <stdint.h>
+# include <tcl.h>
+
+/*
+ * These macros are used to control whether functions are being declared for
+ * import or export. If a function is being declared while it is being built
+ * to be included in a shared library, then it should have the DLLEXPORT
+ * storage class. If is being declared for use by a module that is going to
+ * link against the shared library, then it should have the DLLIMPORT storage
+ * class. If the symbol is beind declared for a static build or for use from a
+ * stub library, then the storage class should be empty.
+ *
+ * The convention is that a macro called BUILD_xxxx, where xxxx is the name of
+ * a library we are building, is set on the compile line for sources that are
+ * to be placed in the library. When this macro is set, the storage class will
+ * be set to DLLEXPORT. At the end of the header file, the storage class will
+ * be reset to DLLIMPORT.
+ */
+
+#undef TCL_STORAGE_CLASS
+#ifdef BUILD_sha256c
+#   define TCL_STORAGE_CLASS DLLEXPORT
+#else
+#   ifdef USE_SHA256C_STUBS
+#      define TCL_STORAGE_CLASS
+#   else
+#      define TCL_STORAGE_CLASS DLLIMPORT
+#   endif
+#endif
+
 
 #define SHA256_HASH_SIZE 32
 
@@ -75,9 +104,9 @@ typedef struct _SHA256Context SHA256_CTX;
 extern "C" {
 #endif
 
-void SHA256Init (SHA256Context *sc);
-void SHA256Update (SHA256Context *sc, const void *data, uint32_t len);
-void SHA256Final (SHA256Context *sc, uint8_t hash[SHA256_HASH_SIZE]);
+EXTERN void SHA256Init (SHA256Context *sc);
+EXTERN void SHA256Update (SHA256Context *sc, const void *data, uint32_t len);
+EXTERN void SHA256Final (SHA256Context *sc, uint8_t hash[SHA256_HASH_SIZE]);
 
 void SHA224Init (SHA256Context *sc);
 
