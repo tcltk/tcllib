18d322be063c305d229e658d93770db0bc9c20f2
Andreas Kupries <andreas.kupries@gmail.com>
2023-01-16 21:33:29 +0000

    comm - Ticket [cb9a7f0bfc] - Fixed regression in hook eval handling.
    Introduced by the simplification of commit [c2d2a4b7e60916fe].
    Bumped version to 4.7.1.

--- modules/comm/pkgIndex.tcl
+++ modules/comm/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded comm 4.7 [list source [file join $dir comm.tcl]]
+package ifneeded comm 4.7.1 [list source [file join $dir comm.tcl]]
