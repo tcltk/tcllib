d57767eec2810f7d3a6436dab20e688ffb010017
Andreas Kupries <andreas.kupries@gmail.com>
2024-09-13 20:46:34 +0000

    Fixup version typo

--- modules/struct/pkgIndex.tcl
+++ modules/struct/pkgIndex.tcl
@@ -14,7 +14,7 @@ package ifneeded struct::skiplist  1.4   [list source [file join $dir skiplist.t
 package ifneeded struct::graph     1.2.2 [list source [file join $dir graph1.tcl]]
 package ifneeded struct::tree      1.2.3 [list source [file join $dir tree1.tcl]]
 
-package ifneeded struct::list       1.89   [list source [file join $dir list.tcl]]
+package ifneeded struct::list       1.9    [list source [file join $dir list.tcl]]
 package ifneeded struct::list::test 1.8.5  [list source [file join $dir list.test.tcl]]
 package ifneeded struct::graph      2.4.4  [list source [file join $dir graph.tcl]]
 package ifneeded struct::map        1.1    [list source [file join $dir map.tcl]]
