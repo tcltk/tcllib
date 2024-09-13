2292a8f4dfcc4f46bc8a0d808fb20e91224cc8ca
Andreas Kupries <andreas.kupries@gmail.com>
2024-09-13 18:51:59 +0000

    snit2 - B - Tcl 9 portability fixes. Bumped to version 2.3.4 snit  - B - Tcl 9 portability fixes. Bumped to version 1.4.3

--- modules/snit/pkgIndex.tcl
+++ modules/snit/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {[package vsatisfies [package provide Tcl] 8.5 9]} {
-    package ifneeded snit 2.3.3 \
+    package ifneeded snit 2.3.4 \
         [list source [file join $dir snit2.tcl]]
 }
 
-package ifneeded snit 1.4.2 [list source [file join $dir snit.tcl]]
+package ifneeded snit 1.4.3 [list source [file join $dir snit.tcl]]
