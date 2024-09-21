4628a39533252a617ccc7105a647602e3a99b1f7
Andreas Kupries <andreas.kupries@gmail.com>
2024-09-21 09:18:37 +0000

    tar - Ticket [65767447de] - EF - Added gzip support based on patch by Christian Werner, with thanks. Internal refactoring to be more clear wrt semantics. Docs added. Docs regenerated. Bumped to version 0.13.

--- modules/tar/pkgIndex.tcl
+++ modules/tar/pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.5 9]} { return }
-package ifneeded tar 0.12.1 [list source [file join $dir tar.tcl]]
+package ifneeded tar 0.13 [list source [file join $dir tar.tcl]]
