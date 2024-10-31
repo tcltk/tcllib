22cf12c3dc6e76272c04efda06c0836dec48bd5c
Poor Yorick <pooryorick@users.sf.net>
2024-10-31 12:59:55 +0000

    Fix mime failing tests. Add GPL license.

--- modules/mime/pkgIndex.tcl
+++ modules/mime/pkgIndex.tcl
@@ -1,4 +1,4 @@
-if {![package vsatisfies [package provide Tcl] 8.6.9]} return
+if {![package vsatisfies [package provide Tcl] 8.6.9 9]} return
 package ifneeded smtp 1.5.2 [list source [file join $dir smtp.tcl]]
 
 package ifneeded mime 3.0.0 [list source [file join $dir mime.tcl]]
