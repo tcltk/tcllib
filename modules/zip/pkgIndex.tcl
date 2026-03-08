9c1078a87277b83346eb815b6e8a77b2b46bbd4b
Keith Nash <k.j.nash@usa.net>
2026-03-08 21:03:26 +0000

    Add Paul Obermeier's code for zipfile::decode::filetime, resolving ticket 4a404d7bdd.  Add documentation and tests.  Bump package zipfile::decode to 0.11.0.

--- modules/zip/pkgIndex.tcl
+++ modules/zip/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {![package vsatisfies [package provide Tcl] 8.5 9]} {return}
 
-package ifneeded zipfile::decode 0.10.1 [list source [file join $dir decode.tcl]]
+package ifneeded zipfile::decode 0.11.0 [list source [file join $dir decode.tcl]]
 package ifneeded zipfile::encode 0.5.2 [list source [file join $dir encode.tcl]]
 
 if {![package vsatisfies [package provide Tcl] 8.6 9]} {return}
